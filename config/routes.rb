e2f66fc9fb32a1ab5823c4a6e5cc95e22386a932
Michael Ly <michael.ly@bitzesty.com>
2014-11-18 15:33:57 +0000

    Sliced the eligibility failure page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/eligibility' => "content_only#eligibility", as: 'eligibility'
+  get '/eligibility'         => "content_only#eligibility",         as: 'eligibility'
   get '/eligibility_success' => "content_only#eligibility_success", as: 'eligibility_success'
+  get '/eligibility_failure' => "content_only#eligibility_failure", as: 'eligibility_failure'
 
   root to: 'content_only#home'
 end
