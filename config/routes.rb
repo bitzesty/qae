2bf07d01d6514db8391ae549a78d3d9c9443088c
Michael Ly <michael.ly@bitzesty.com>
2014-11-18 17:28:40 +0000

    Sliced the innovation award application page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get '/eligibility'         => "content_only#eligibility",         as: 'eligibility'
-  get '/eligibility_success' => "content_only#eligibility_success", as: 'eligibility_success'
-  get '/eligibility_failure' => "content_only#eligibility_failure", as: 'eligibility_failure'
-  get '/eligibility_check'   => "content_only#eligibility_check",   as: 'eligibility_check'
+  get '/eligibility'            => "content_only#eligibility",            as: 'eligibility'
+  get '/eligibility_success'    => "content_only#eligibility_success",    as: 'eligibility_success'
+  get '/eligibility_failure'    => "content_only#eligibility_failure",    as: 'eligibility_failure'
+  get '/eligibility_check'      => "content_only#eligibility_check",      as: 'eligibility_check'
+  get '/apply_innovation_award' => "content_only#apply_innovation_award", as: 'apply_innovation_award'
 
   root to: 'content_only#home'
 end
