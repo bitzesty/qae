51db545151dbfec48577eda961e10fce794ca246
Michael Ly <michael.ly@bitzesty.com>
2014-11-18 15:30:05 +0000

    Sliced eligibility success page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/eligibility' => "content_only#eligibility", as: 'eligibility'
+  get '/eligibility_success' => "content_only#eligibility_success", as: 'eligibility_success'
 
   root to: 'content_only#home'
 end
