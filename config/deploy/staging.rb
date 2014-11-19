74443bb529da3266e5ef28e33273fdbbfd296433
Vasili Kachalko <amarant.st@gmail.com>
2014-11-19 20:54:06 +0300

    remove unused propertiy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :default_env, { 'RAILS_ENV' => 'production' }
 set :rails_env, 'production'
 
-server '95.138.174.112', user: 'qae', roles: %w{web app}, my_property: :my_value
+server '95.138.174.112', user: 'qae', roles: %w{web app}
