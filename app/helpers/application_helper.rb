0bbd5562a8788f093ce08e04458a894a5099953b
Michael Ly <michael.ly@bitzesty.com>
2014-11-21 12:12:26 +0000

    Partialised the page steps progress and made helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
 module ApplicationHelper
+  def step_link_to(name, url, opts = {})
+    index_step_text = "<span class='step-number'>#{opts[:index]}.</span> #{name}".html_safe
+
+    opts[:class] ||= ""
+
+    if opts.has_key?(:index) && opts[:index].is_a?(Integer)
+      if opts.has_key?(:active) && opts[:active].is_a?(Integer)
+        if opts[:index] == opts[:active]
+          opts[:class] += " step-current"
+
+          content_tag :li, opts do
+            content_tag :span do
+              index_step_text
+            end
+          end
+        else
+          if opts[:index] < opts[:active]
+            opts[:class] += " step-past"
+          end
+
+          content_tag :li, opts do
+            link_to url do
+              index_step_text
+            end
+          end
+        end
+      else
+        content_tag :li, opts do
+          link_to url do
+            index_step_text
+          end
+        end
+      end
+    else
+      content_tag :li, opts do
+        link_to name, url
+      end
+    end
+  end
 end
