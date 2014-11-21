8a5d1bcc0e2b1c0a1562d808f186179b1beb258a
Michael Ly <michael.ly@bitzesty.com>
2014-11-21 12:24:05 +0000

    DRYed up the step_link_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,40 +1,37 @@
 module ApplicationHelper
   def step_link_to(name, url, opts = {})
-    index_step_text = "<span class='step-number'>#{opts[:index]}.</span> #{name}".html_safe
-
     opts[:class] ||= ""
+    step_status = ""
+
+    if opts.has_key?(:index) && opts[:index].is_a?(Integer)
+      index_step_text = "<span class='step-number'>#{opts[:index]}.</span> #{name}".html_safe
+    else
+      index_step_text = name
+    end
 
     if opts.has_key?(:index) && opts[:index].is_a?(Integer)
       if opts.has_key?(:active) && opts[:active].is_a?(Integer)
         if opts[:index] == opts[:active]
+          step_status  = "current"
           opts[:class] += " step-current"
-
-          content_tag :li, opts do
-            content_tag :span do
-              index_step_text
-            end
-          end
         else
           if opts[:index] < opts[:active]
+            step_status  = "past"
             opts[:class] += " step-past"
-          end
-
-          content_tag :li, opts do
-            link_to url do
-              index_step_text
-            end
-          end
-        end
-      else
-        content_tag :li, opts do
-          link_to url do
-            index_step_text
           end
         end
       end
-    else
-      content_tag :li, opts do
-        link_to name, url
+    end
+
+    content_tag :li, opts do
+      if step_status == "current"
+        content_tag :span do
+          index_step_text
+        end
+      else
+        link_to url do
+          index_step_text
+        end
       end
     end
   end
