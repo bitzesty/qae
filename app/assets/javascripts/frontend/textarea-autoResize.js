ae6716f63365f187fd5b768a384a4bc23f78bc94
Michael Ly <michael.ly@bitzesty.com>
2014-11-21 16:22:57 +0000

    Firefox fix for auto resize and do it on load

--- app/assets/javascripts/frontend/textarea-autoResize.js
+++ app/assets/javascripts/frontend/textarea-autoResize.js
@@ -10,25 +10,33 @@ txt.each(function() {
   // default/initial heights for all textareas so
   // that resizing doesn't get shorter than this
   var textarea = $(this);
-  textarea.css('min-height', textarea.height());
-  textarea.attr("data-height", textarea.height())
+  textarea.css('min-height', textarea.outerHeight());
+  textarea.attr("data-height", textarea.outerHeight())
 });
 
 $('body').append(hiddenDiv);
 
-txt.on('keyup', function () {
-  content = $(this).val();
+resizeTextarea = function (textInput) {
+  content = textInput.val();
 
   content = content.replace(/\n/g, '<br>');
   hiddenDiv.html(content + '<br class="lbr">');
 
-  if (hiddenDiv.height() >= $(this).attr("data-height")) {
+  if (hiddenDiv.height() >= textInput.attr("data-height")) {
     // resize when there's more text than the text container
-    $(this).css("height", hiddenDiv.height());
+    textInput.css("height", hiddenDiv.height()+1);
   } else if (content == "") {
     // resize when no text
-    $(this).css("height", $(this).attr("data-height"));
+    textInput.css("height", textInput.attr("data-height"));
   }
+}
+
+txt.each(function() {
+  resizeTextarea($(this));
+});
+
+txt.on('keyup', function () {
+  resizeTextarea($(this));
 });
 
 txt.on('mouseup', function () {
