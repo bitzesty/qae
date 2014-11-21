cf90ab6eba2d8f46dfcfad1386be2c1e176b3a09
Michael Ly <michael.ly@bitzesty.com>
2014-11-21 12:28:20 +0000

    removed colon from controller

--- app/controllers/content_only_controller.rb
+++ app/controllers/content_only_controller.rb
@@ -3,34 +3,34 @@ class ContentOnlyController < ApplicationController
   end
 
   def eligibility_check_1
-    @active_step = 1;
+    @active_step = 1
   end
 
   def eligibility_check_2
-    @active_step = 2;
+    @active_step = 2
   end
 
   def eligibility_check_3
-    @active_step = 3;
+    @active_step = 3
   end
 
   def innovation_award_form_1
-    @active_step = 1;
+    @active_step = 1
   end
 
   def innovation_award_form_2
-    @active_step = 2;
+    @active_step = 2
   end
 
   def innovation_award_form_3
-    @active_step = 3;
+    @active_step = 3
   end
 
   def innovation_award_form_4
-    @active_step = 4;
+    @active_step = 4
   end
 
   def innovation_award_form_5
-    @active_step = 5;
+    @active_step = 5
   end
 end
