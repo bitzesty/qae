9441e84ed051fa682770f4167021e3904e952c25
Michael Ly <michael.ly@bitzesty.com>
2014-11-25 16:50:21 +0000

    Eligibility page for award form

--- app/controllers/content_only_controller.rb
+++ app/controllers/content_only_controller.rb
@@ -14,23 +14,27 @@ class ContentOnlyController < ApplicationController
     @active_step = 3
   end
 
-  def innovation_award_form_1
+  def innovation_award_eligible
     @active_step = 1
   end
 
-  def innovation_award_form_2
+  def innovation_award_form_1
     @active_step = 2
   end
 
-  def innovation_award_form_3
+  def innovation_award_form_2
     @active_step = 3
   end
 
-  def innovation_award_form_4
+  def innovation_award_form_3
     @active_step = 4
   end
 
-  def innovation_award_form_5
+  def innovation_award_form_4
     @active_step = 5
+  end
+
+  def innovation_award_form_5
+    @active_step = 6
   end
 end
