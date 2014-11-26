fb51f6caa305e5e6097af5ed845a31001a36e1f9
Michael Ly <michael.ly@bitzesty.com>
2014-11-26 13:35:58 +0000

    better authenticate user for placeholder pages

--- app/controllers/content_only_controller.rb
+++ app/controllers/content_only_controller.rb
@@ -1,28 +1,5 @@
 class ContentOnlyController < ApplicationController
-  before_filter :authenticate_user!,
-    :only => [
-      :dashboard,
-      :eligibility_1,
-      :eligibility_2,
-      :eligibility_3,
-      :eligibility_4,
-      :eligibility_5,
-      :eligibility_6,
-      :eligibility_success,
-      :eligibility_failure,
-      :eligibility_check_1,
-      :eligibility_check_2,
-      :eligibility_check_3,
-      :apply_innovation_award,
-      :innovation_award_eligible,
-      :innovation_award_form_1,
-      :innovation_award_form_2,
-      :innovation_award_form_3,
-      :innovation_award_form_4,
-      :innovation_award_form_5,
-      :innovation_award_confirm,
-      :account
-    ]
+  before_filter :authenticate_user!, except: [:home]
 
   def home
   end
