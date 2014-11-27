23d43992daa26303adef4daac78ce2f53c3c48ca
Michael Ly <michael.ly@bitzesty.com>
2014-11-27 11:13:26 +0000

    Added extra question to basic eligibility and notes on the logic

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/eligibility_4'             => "content_only#eligibility_4",             as: 'eligibility_4'
   get '/eligibility_5'             => "content_only#eligibility_5",             as: 'eligibility_5'
   get '/eligibility_6'             => "content_only#eligibility_6",             as: 'eligibility_6'
+  get '/eligibility_7'             => "content_only#eligibility_7",             as: 'eligibility_7'
   get '/eligibility_success'       => "content_only#eligibility_success",       as: 'eligibility_success'
   get '/eligibility_failure'       => "content_only#eligibility_failure",       as: 'eligibility_failure'
   get '/eligibility_check_1'       => "content_only#eligibility_check_1",       as: 'eligibility_check_1'
