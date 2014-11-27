d7aa5a8c9915ba7055a22964623099eaf13952d5
Ruslan Khamidullin <rusllonrails@gmail.com>
2014-11-27 14:22:08 +0400

    [FormAnswer] added model

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'database_cleaner'
+require 'shoulda/matchers'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| load f, true}
 
