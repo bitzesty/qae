5be0350de30b9d9836bcc7f006f2209924e19050
Vasili Kachalko <amarant.st@gmail.com>
2014-11-28 14:25:42 +0300

    refactor registration process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   self.responder = AppResponder
   respond_to :html
@@ -16,5 +17,11 @@ class ApplicationController < ActionController::Base
     if resource.is_a?(User)
       eligibility_1_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :agreed_with_privacy_policy) }
   end
 end
