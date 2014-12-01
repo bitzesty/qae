22dd8c952dce83bdb30e72b8a8eb6665bb0432d7
Michael Ly <michael.ly@bitzesty.com>
2014-12-01 16:30:24 +0000

    Added other award placeholder form pages

--- app/controllers/content_only_controller.rb
+++ app/controllers/content_only_controller.rb
@@ -52,4 +52,52 @@ class ContentOnlyController < ApplicationController
   def innovation_award_form_5
     @active_step = 6
   end
+
+  def international_trade_award_eligible
+    @active_step = 1
+  end
+
+  def international_trade_award_form_1
+    @active_step = 2
+  end
+
+  def international_trade_award_form_2
+    @active_step = 3
+  end
+
+  def international_trade_award_form_3
+    @active_step = 4
+  end
+
+  def international_trade_award_form_4
+    @active_step = 5
+  end
+
+  def international_trade_award_form_5
+    @active_step = 6
+  end
+
+  def sustainable_development_award_eligible
+    @active_step = 1
+  end
+
+  def sustainable_development_award_form_1
+    @active_step = 2
+  end
+
+  def sustainable_development_award_form_2
+    @active_step = 3
+  end
+
+  def sustainable_development_award_form_3
+    @active_step = 4
+  end
+
+  def sustainable_development_award_form_4
+    @active_step = 5
+  end
+
+  def sustainable_development_award_form_5
+    @active_step = 6
+  end
 end
