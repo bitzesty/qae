c084de255d275a5b58a5c454678e0200e62b4f3e
Victor Bilyk <victorbilyk@gmail.com>
2014-12-02 11:54:51 +0600

    moving qae2014 forms to app/forms

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Qae
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths += %W( #{config.root}/app/forms/ )
+ 
     config.assets.precompile += %w{
       application.css
       application-ie9.css
