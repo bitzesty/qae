3d4d1a2dcae066dc3779974002d894f2d61cc6d1
Vasili Kachalko <amarant.st@gmail.com>
2014-12-02 18:16:36 +0300

    remove placeholders

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,6 @@ Rails.application.routes.draw do
   }
 
   get '/dashboard'                 => "content_only#dashboard",                 as: 'dashboard'
-  get '/eligibility_1'             => "content_only#eligibility_1",             as: 'eligibility_1'
-  get '/eligibility_2'             => "content_only#eligibility_2",             as: 'eligibility_2'
-  get '/eligibility_3'             => "content_only#eligibility_3",             as: 'eligibility_3'
-  get '/eligibility_4'             => "content_only#eligibility_4",             as: 'eligibility_4'
-  get '/eligibility_5'             => "content_only#eligibility_5",             as: 'eligibility_5'
-  get '/eligibility_6'             => "content_only#eligibility_6",             as: 'eligibility_6'
-  get '/eligibility_7'             => "content_only#eligibility_7",             as: 'eligibility_7'
-  get '/eligibility_success'       => "content_only#eligibility_success",       as: 'eligibility_success'
-  get '/eligibility_failure'       => "content_only#eligibility_failure",       as: 'eligibility_failure'
   get '/eligibility_check_1'       => "content_only#eligibility_check_1",       as: 'eligibility_check_1'
   get '/eligibility_check_2'       => "content_only#eligibility_check_2",       as: 'eligibility_check_2'
   get '/eligibility_check_3'       => "content_only#eligibility_check_3",       as: 'eligibility_check_3'
