7d177ec8bdbdb87be9f0cc774d38dbbbbe92dfd1
Michael Ly <michael.ly@bitzesty.com>
2014-12-02 17:32:36 +0000

    tweak copy

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   enumerize :prefered_method_of_contact, in: %w(phone email)
   serialize :qae_info_source, Array
-  enumerize :qae_info_source, in: %w(govuk compeditor business_event national_press business_press online local_trade_body national_trade_body mail_from_qae word_of_mouth other), multiple: false
+  enumerize :qae_info_source, in: %w(govuk competitor business_event national_press business_press online local_trade_body national_trade_body mail_from_qae word_of_mouth other), multiple: false
 
   private
 
