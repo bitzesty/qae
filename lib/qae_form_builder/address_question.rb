749c15a0ac59500181741270e04c8a01aa79672a
Victor Bilyk <victorbilyk@gmail.com>
2014-12-04 19:19:25 +0600

    small fix for address question & more autosaved questions

--- lib/qae_form_builder/address_question.rb
+++ lib/qae_form_builder/address_question.rb
@@ -1,9 +1,13 @@
 class QAEFormBuilder
 
   class AddressQuestionBuilder < QuestionBuilder
+    def countries countries
+      @q.countries = countries
+    end
   end
 
   class AddressQuestion < Question
+    attr_accessor :countries
   end
 
 end
