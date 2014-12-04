affcf1a798b1de9eacd73403e5279bb08d3da59b
Vasili Kachalko <amarant.st@gmail.com>
2014-12-04 15:15:30 +0300

    fix user sign in specs

--- spec/acceptance/steps/user_sign_in_steps.rb
+++ spec/acceptance/steps/user_sign_in_steps.rb
@@ -4,7 +4,8 @@ end
 
 step "I sign in as user" do
   visit '/users/sign_in'
-  fill_in 'Email', with: @user.email
-  fill_in 'Password', with: 'password123'
+
+  fill_in 'user_email', with: @user.email
+  fill_in 'user_password', with: 'password123'
   click_button 'Log in'
 end
