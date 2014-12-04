062d423b6771dd9989adf85536fe06cb9a631e21
Michael Ly <michael.ly@bitzesty.com>
2014-12-04 17:43:33 +0000

    Cleaned up indentation

--- app/forms/qae_2014_forms/innovation.rb
+++ app/forms/qae_2014_forms/innovation.rb
@@ -6,23 +6,21 @@ require 'qae_2014_forms/innovation/innovation_step5'
 
 class QAE2014Forms
   class << self
+    def innovation
+      @innovation ||= QAEFormBuilder.build 'Apply for the Innovation Award' do
 
-  def innovation
-    @innovation ||= QAEFormBuilder.build 'Apply for the Innovation Award' do
+        step 'Company Information', &QAE2014Forms.innovation_step1
 
-      step 'Company Information', &QAE2014Forms.innovation_step1
+        step 'Commercial Performance', &QAE2014Forms.innovation_step2
 
-      step 'Commercial Performance', &QAE2014Forms.innovation_step2
+        step 'Description of Goods or Services', &QAE2014Forms.innovation_step3
 
-      step 'Description of Goods or Services', &QAE2014Forms.innovation_step3
+        step 'Declaration of Corporate Responsibility', &QAE2014Forms.innovation_step4
 
-      step 'Declaration of Corporate Responsibility', &QAE2014Forms.innovation_step4
+        step 'Authorisation/Monitoring', &QAE2014Forms.innovation_step5
 
-      step 'Authorisation/Monitoring', &QAE2014Forms.innovation_step5
-
+      end
     end
-  end
-
   end
 end
 
