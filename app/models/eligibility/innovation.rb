83784a7b769f5e80e4003cd0e23bc938826dbee7
Vasili Kachalko <amarant.st@gmail.com>
2014-12-05 17:52:18 +0300

    add eligibility model for development award

--- app/models/eligibility/innovation.rb
+++ app/models/eligibility/innovation.rb
@@ -1,10 +1,8 @@
 class Eligibility::Innovation < Eligibility
-  extend Enumerize
-
   property :innovative_product, boolean: true, label: "Do you have an innovative product/service/initiative?", accept: :true
   property :number_of_innovative_products, positive_integer: true, label: "How many?", accept: :not_nil
   property :was_on_market_for_two_years, boolean: true, label: "Has it been released to the market for at least two years?", accept: :true
-  property :had_impact_on_commercial_performace_over_two_years, boolean: true, lable: "Has the innovation had a measurable positive impact on your commercial performance over at least the last two years??", accept: :true
+  property :had_impact_on_commercial_performace_over_two_years, boolean: true, lable: "Has the innovation had a measurable positive impact on your commercial performance over at least the last two years?", accept: :true
 
   def eligible?
     answers && answers.any? && answers.all? do |question, answer|
---
+++ spec/models/eligibility/development_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+RSpec.describe Eligibility::Development, :type => :model do
+  let(:user) { FactoryGirl.create(:user) }
+
+  context 'answers storage' do
+    it 'saves and reads answers' do
+      eligibility = Eligibility::Development.new(user: user)
+      eligibility.sustainable_development = '1'
+
+      expect { eligibility.save! }.to change {
+        Eligibility::Development.count
+      }.by(1)
+
+      eligibility = Eligibility::Development.last
+
+      expect(eligibility.user).to eq(user)
+      expect(eligibility).to be_sustainable_development
+    end
+  end
+
+  describe '#eligible?' do
+    let(:eligibility) { Eligibility::Development.new(user: user) }
+
+    it 'is not eligible by default' do
+      expect(eligibility).not_to be_eligible
+    end
+
+    it 'is eligible when all questions are answered correctly' do
+      eligibility.sustainable_development = true
+      eligibility.development_contributed_to_commercial_success = true
+      eligibility.can_demonstrate_corporate_responsibility = true
+
+      expect(eligibility).to be_eligible
+    end
+
+    it 'is not eligible when not all answers are correct' do
+      eligibility.sustainable_development = false
+      eligibility.development_contributed_to_commercial_success = true
+      eligibility.can_demonstrate_corporate_responsibility = true
+
+
+      expect(eligibility).not_to be_eligible
+    end
+  end
+end
