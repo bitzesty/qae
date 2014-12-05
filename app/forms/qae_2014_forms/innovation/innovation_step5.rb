0756fba62c0c20a228f7ca1ee3ec72efb50f98c7
Michael Ly <michael.ly@bitzesty.com>
2014-12-05 13:41:11 +0000

    Updated questions for innovation step 5

--- app/forms/qae_2014_forms/innovation/innovation_step5.rb
+++ app/forms/qae_2014_forms/innovation/innovation_step5.rb
@@ -9,6 +9,7 @@ class QAE2014Forms
         end
 
         options :agree_to_be_contacted, %Q{We will contact you regarding your entry. We may also wish to contact you about other issues relating to The Queen's Award for Enterprise (eg. acting as a case study). Are you happy for us to do this?} do
+          ref 'E 2'
           yes_no
         end
 
@@ -18,17 +19,17 @@ class QAE2014Forms
         end
 
         text :contact_position, 'Position' do
-          ref 'E 3.1'
+          classes "sub-question"
           required
         end
 
         contact_email :contact_email, 'Email address' do
-          ref 'E 3.2'
+          classes "sub-question"
           required
         end
 
         text :contact_phone, 'Telephone number' do
-          ref 'E 3.3'
+          classes "sub-question"
           required
         end
 
@@ -36,27 +37,17 @@ class QAE2014Forms
           ref 'E 4'
           required
           text %Q{
-            By ticking this box, I certify that all the particulars given and those
-            in any accompanying statements are correct to the best of my knowledge
-            and belief and that no material information has been withheld.
-            I undertake to notify The Queen’s Awards Office of any changes to
-            the information I have provided in this entry form.
+            By ticking this box, I certify that all the particulars given and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld. I undertake to notify The Queen’s Awards Office of any changes to the information I have provided in this entry form.
             <br>
             <br>
-            I am not aware of any matter which might cast doubt upon the worthiness
-            of this business unit to receive a Queen’s Award. I consent to all
-            necessary enquiries being made by The Queen’s Awards Office in relation
-            to this entry. This includes enquiries made of Government Departments
-            and Agencies in discharging its responsibilities to vet any business
-            unit which might be granted a Queen’s Award to ensure the highest
-            standards of propriety. 
+            I am not aware of any matter which might cast doubt upon the worthiness of this business unit to receive a Queen’s Award. I consent to all necessary enquiries being made by The Queen’s Awards Office in relation to this entry. This includes enquiries made of Government Departments and Agencies in discharging its responsibilities to vet any business unit which might be granted a Queen’s Award to ensure the highest standards of propriety.
           }
         end
 
 
         submit "Submit application" do
           notice %Q{
-            Note that you can submit and come back to change or add any other info until the deadline.
+            <p>Note that you can submit and come back to change or add any other info until the deadline.</p>
           }
           style "large"
         end
