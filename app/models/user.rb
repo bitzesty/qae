3095613631030e21bced223114e292cc282425f5
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2014-12-05 13:37:38 -0200

    Finishing validations, and making messages nicer

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,20 +15,46 @@ class User < ActiveRecord::Base
   validates :role, presence: true
 
   # First step validations
-  validates :title, presence: true, :if => :is_first_step?
-  validates :first_name, presence: true, :if => :is_first_step?
-  validates :last_name, presence: true, :if => :is_first_step?
-  validates :job_title, presence: true, :if => :is_first_step?
-  validates :phone_number, presence: true, :if => :is_first_step?
+  validates :title, presence: {message: "Title can't be blank"}, :if => :is_first_step?
+  validates :first_name, presence: {message: "First name can't be blank"}, :if => :is_first_step?
+  validates :last_name, presence: {message: "Last name can't be blank"}, :if => :is_first_step?
+  validates :job_title, presence: {message: "Job title can't be blank"}, :if => :is_first_step?
+  validates :phone_number, presence: {message: "Telephone number can't be blank"}, :if => :is_first_step?
   
   # Second step validations
-  validates :company_name, presence: true, :if => :is_second_step?
-  validates :company_address_first, presence: true, :if => :is_second_step?
-  validates :company_address_second, presence: true, :if => :is_second_step?
-  validates :company_city, presence: true, :if => :is_second_step?
-  validates :company_country, presence: true, :if => :is_second_step?
-  validates :company_postcode, presence: true, :if => :is_second_step?
-  validates :company_phone_number, presence: true, :if => :is_second_step?
+  validates :company_name, presence: {message: "Company name can't be blank"}, :if => :is_second_step?
+  validates :company_address_first, presence: {message: "First address line can't be blank"}, :if => :is_second_step?
+  validates :company_address_second, presence: {message: "Second address line can't be blank"}, :if => :is_second_step?
+  validates :company_city, presence: {message: "City can't be blank"}, :if => :is_second_step?
+  validates :company_country, presence: {message: "Country can't be blank"}, :if => :is_second_step?
+  validates :company_postcode, presence: {message: "Postcode can't be blank"}, :if => :is_second_step?
+  validates :company_phone_number, presence: {message: "Telephone number can't be blank"}, :if => :is_second_step?
+
+  validates :company_postcode, length: {
+    minimum: 6,
+    maximum: 8,
+    :too_short => "Please enter a valid postcode.",
+    :too_long => "Please enter a valid postcode."
+  }, :if => :is_second_step?
+
+  validates :company_postcode, format: {
+    with: /[a-zA-Z]{1,2}[0-9][0-9a-zA-Z]?\s?[0-9][a-zA-Z]{2}/,
+    :message => "Please enter a valid postcode"
+  }, :if => :is_second_step?
+
+  validates :phone_number, length: {
+    minimum: 7,
+    maximum: 20,
+    :too_short => "Please enter  a valid telephone number",
+    :too_long => "Please enter a valid telephone number"
+  }, :if => :is_first_step?
+
+  validates :company_phone_number, length: {
+    minimum: 7,
+    maximum: 20,
+    :too_short => "Please enter  a valid telephone number",
+    :too_long => "Please enter a valid telephone number"
+  }, :if => :is_second_step?
 
   begin :associations
     has_many :form_answers, dependent: :destroy
