c90ca626412255a88650d6faaa2fc8e4927ffd04
Michael Ly <michael.ly@bitzesty.com>
2014-12-08 13:29:54 +0000

    Sliced the international trade 2 and fixed some questions partials

--- app/forms/qae_2014_forms/international_trade/international_trade_step2.rb
+++ app/forms/qae_2014_forms/international_trade/international_trade_step2.rb
@@ -2,23 +2,31 @@ class QAE2014Forms
   class << self
     def trade_step2
       @trade_step2 ||= Proc.new {
-        options :innovation_performance_years, %Q{For how long has the innovation had substantial impact on (ie. measurably improved) your organisation's performance?} do
+        # TODO Hide this when A5 "List the Queen's Award(s) you currently hold" has a value "International Trade (6 years)"
+        # Act as if this is preslected with "6 plus" for dependent questions below
+        options :trade_commercial_success, "For how long has your organisation seen growth and commercial success in its international trade?" do
           ref 'B 1'
           required
           context %Q{
-            <p>Your answer here will determine whether you are assessed for outstanding innovation (over two years) or continuous innovation (over five years).</p>
+            <p>Your answer here will determine whether you are assessed for outstanding (over  three years) or continuous (over six years) achievement in international trade.</p>
           }
-          option '2 to 4', '2-4 years'
-          option '5 plus', '5 years or more'
+          option '3 to 5', '3-5 years'
+          option '6 plus', '6 years or more'
         end
 
-        financial_year_dates :financial_year_dates, 'Please select your financial year end dates:' do
+        # TODO Show this when A5 "List the Queen's Award(s) you currently hold" has a value "International Trade (6 years)"
+        # Act as if the B1 above is preslected with "6 plus" for dependent questions below
+        #header :trade_commercial_success_preselected, "Because you are the current holder of a Queen's Award for Continuous Achievement in International Trade (6 years), you may only apply for the Outstanding Achievement Award (3 years) this year." do
+        #  ref 'B 1'
+        #end
+
+        trade_financial_year_dates :financial_year_dates, 'Please select your financial year end dates:' do
           ref 'B 2'
           required
           context %Q{
             <p>If you haven't reached/finalised your latest year-end yet, please enter it anyway and use financial estimates to complete your application.</p>
           }
-          conditional :innovation_performance_years, :true
+          conditional :trade_commercial_success, :true
         end
 
         number :employees, 'State the number of people employed by the company for each year of your entry.' do
@@ -29,199 +37,122 @@ class QAE2014Forms
           }
           style "small"
           min 2
-          conditional :innovation_performance_years, :true
-        end
-
-        options :innovation_part_of, 'My innovation is an integral part of' do
-          ref 'B 4'
-          required
-          option :entire_business, 'The entire business'
-          option :single_product_or_service, 'A single product or service'
+          conditional :trade_commercial_success, :true
         end
 
         header :company_financials, 'Company Financials' do
+          ref 'B 4'
+          conditional :trade_commercial_success, :true
+        end
+
+        trade_by_years :overseas_sales, 'Direct overseas sales' do
+          classes "sub-question"
+          required
           context %Q{
-            <p>These figures should be for your entire organisation.</p>
+            <p>guidance text for this question is carnage</p>
           }
-          ref 'B 5'
-          conditional :innovation_performance_years, :true
-        end
-
-        by_years :total_turnover, 'Total turnover' do
-          classes "sub-question"
-          required
-          conditional :innovation_performance_years, :true
+          conditional :trade_commercial_success, :true
           drop_conditional :drops_in_turnover
         end
 
-        by_years :exports, 'of which exports' do 
+        trade_by_years :total_turnover, 'Total turnover (home plus overseas)' do
           classes "sub-question"
           required
-          context %Q{<p>Please enter '0' if you had none.</p>}
-          conditional :innovation_performance_years, :true
+          conditional :trade_commercial_success, :true
           drop_conditional :drops_in_turnover
         end
 
-        by_years :net_profit, 'Net profit after tax but before dividends' do 
+        trade_by_years :net_profit, 'Net profit after tax but before dividends' do 
           classes "sub-question"
           required
-          conditional :innovation_performance_years, :true
+          conditional :trade_commercial_success, :true
           drop_conditional :drops_in_turnover
         end
 
-        by_years :total_net_assets, 'Total net assets' do 
-          classes "sub-question"
-          required
-          context %Q{<p>As per your balance sheet. Total assets (fixed and current), less liabilities (current and long-term).}
-          conditional :innovation_performance_years, :true
-          drop_conditional :drops_in_turnover
-        end
-
-        textarea :drops_in_turnover, "Explain any drops in turnover, export sales, total net assets and net profits, as well as any losses made." do
+        textarea :drops_in_turnover, "Explain any drops in turnover or net profit, and any losses made." do
           classes "sub-question js-conditional-drop-question"
           rows 5
-          words_max 200
-          conditional :innovation_performance_years, :true
+          words_max 500
+          conditional :trade_commercial_success, :true
         end
 
         options :company_estimated_figures, 'Are any of these figures estimated?' do
           classes "sub-question"
           yes_no
-          conditional :innovation_performance_years, :true
-          conditional :innovation_part_of, :entire_business
+          conditional :trade_commercial_success, :true
         end
 
         textarea :company_estimates_use, 'Explain the use of estimates, and how much of these are actual receipts or firm orders.' do
           classes "sub-question"
           rows 5
           words_max 200
-          conditional :innovation_performance_years, :true
+          conditional :trade_commercial_success, :true
           conditional :company_estimated_figures, :yes
         end
 
-        header :product_financials, 'Product/Service Financials' do
-          ref 'B 6'
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-        end
- 
-        by_years :units_sold, 'Number of units/contracts sold' do
-          classes "sub-question"
-          required
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-          drop_conditional :drops_in_sales
-        end
-
-        by_years :sales, 'Sales' do 
-          classes "sub-question"
-          required
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-          drop_conditional :drops_in_sales
-        end
-
-        by_years :sales_exports, 'of which exports' do
-          classes "sub-question"
-          context %Q{<p>Please enter '0' if you had none.</p>}
-          required
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-          drop_conditional :drops_in_sales
-        end
-
-        by_years :sales_royalties, 'of which royalties or licenses' do
-          classes "sub-question"
-          context %Q{<p>Please enter '0' if you had none.</p>}
-          required
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-          drop_conditional :drops_in_sales
-        end
-
-        textarea :drops_in_sales, "Explain any drop in sales" do
-          classes "sub-question js-conditional-drop-question"
-          required
-          rows 5
-          words_max 300
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-        end
-
-        options :product_estimated_figures, 'Are any of these figures estimated?' do
+        options :resale_overseas, 'Do you purchase your products/services (or any of their components) from overseas for resale overseas?' do
           classes "sub-question"
           yes_no
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
+          context %Q{
+            <p>Excluding raw materials and value added.</p>
+          }
+          conditional :trade_commercial_success, :true
         end
 
-        textarea :product_estimates_use, 'Explain the use of estimates, and how much of these are actual receipts or firm orders.' do
+        trade_by_years :total_imported_cost, 'Total cost of the imported products/services/components' do 
+          classes "sub-question"
+          required
+          conditional :trade_commercial_success, :true
+          conditional :resale_overseas, :true
+          drop_conditional :drops_in_turnover
+        end
+
+        options :manufacture_overseas, 'Do you manufacture overseas?' do
+          ref 'B 5'
+          required
+          yes_no
+        end
+
+        trade_by_years_percentage :overseas_yearly_percentage, 'Indicate the yearly percentage of your goods produced overseas.' do 
+          classes "sub-question"
+          required
+          conditional :trade_commercial_success, :true
+          conditional :manufacture_overseas, :true
+        end
+
+        textarea :manufacture_model_benefits, 'Describe the benefits of this business model to the UK.' do
           classes "sub-question"
           rows 5
-          words_max 200
-          conditional :product_estimated_figures, :yes
-          conditional :innovation_performance_years, :true
+          words_max 400
+          conditional :manufacture_overseas, :true
         end
 
-        textarea :financial_comments, 'Additional comments (optional)' do
+        options :operate_overseas, 'Does you franchise your operations overseas?' do
+          ref 'B 6'
+          required
+          yes_no
+        end
+
+        textarea :operate_model_benefits, 'Describe the benefits of this business model to the UK.' do
           classes "sub-question"
+          required
           rows 5
-          words_max 100
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
+          words_max 500
+          conditional :operate_overseas, :true
         end
 
-        by_years :avg_unit_price, 'Average unit selling price/contract value' do 
+        options :received_grant, 'Have you received any grant funding to support this product/service?' do
           ref 'B 7'
           required
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
+          yes_no
         end
 
-        textarea :avg_unit_price_desc, 'Explain your unit selling prices/contract values, highlighting any changes over the above periods.' do
-          classes "sub-question"
-          required
-          rows 5
-          words_max 200
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-        end
-
-        by_years :avg_unit_cost_self, 'Cost, to you, of a single unit/contract' do 
-          ref 'B 8'
-          required
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-        end
-
-        textarea :costs_change_desc, 'Explain your unit/ contract costs, highlighting any changes over the above periods.' do
-          classes "sub-question"
-          required
-          rows 5
-          words_max 200
-          conditional :innovation_part_of, :single_product_or_service
-          conditional :innovation_performance_years, :true
-        end
-
-        textarea :innovation_performance, 'Describe how, when, and to what extent the innovation improved the commercial perfmormance of your business. Also explain any cost savings you made as a result of the innovation.' do
-          ref 'B 9'
-          required
-          rows 5
-          words_max 300
-        end 
-
-        textarea :investments_details, 'Please enter details of all the investments made in your innovation, and the years when they were made. Include investments made both during and prior to your entry period.' do
-          ref 'B 10'
-          required
-          rows 5
-          words_max 300
-        end
-
-        textarea :roi_details, 'How long did it take the investment indicated above to be repaid? When and how was this repayment achieved?' do
+        textarea :funding_details, 'Please give details of date(s), source(s) and level(s) of funding.' do
           classes "sub-question"
           required
           rows 5
           words_max 300
+          conditional :received_grant, :true
         end
       }
     end
---
+++ lib/qae_form_builder/trade_by_years_question.rb
@@ -0,0 +1,19 @@
+class QAEFormBuilder
+  class TradeFinancialYearDatesQuestionBuilder < QuestionBuilder
+  end
+
+  class TradeFinancialYearDatesQuestion < Question
+  end
+
+  class TradeByYearsQuestionBuilder < QuestionBuilder
+  end
+
+  class TradeByYearsQuestion < Question
+  end
+
+  class TradeByYearsPercentageQuestionBuilder < QuestionBuilder
+  end
+
+  class TradeByYearsPercentageQuestion < Question
+  end
+end
