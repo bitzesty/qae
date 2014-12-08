9d235d957e752f79a590c543c57d72e76e7d7157
Michael Ly <michael.ly@bitzesty.com>
2014-12-08 15:32:46 +0000

    Sustainable Development form

--- app/forms/qae_2014_forms.rb
+++ app/forms/qae_2014_forms.rb
@@ -1,6 +1,7 @@
 require 'qae_form_builder'
 require 'qae_2014_forms/innovation'
 require 'qae_2014_forms/international_trade'
+require 'qae_2014_forms/sustainable_development'
 require 'qae_2014_forms/countries'
 
 class QAE2014Forms
