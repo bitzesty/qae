3c2898fd44020174e153be856df785999f5301a5
Vasili Kachalko <amarant.st@gmail.com>
2014-12-08 17:31:42 +0300

    change condition

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -102,7 +102,7 @@ class AccountsController < ApplicationController
   private
 
   def check_eligibility
-    if !current_user.basic_eligibility || !current_user.basic_eligibility.passed?
+    if !(current_user.basic_eligibility && current_user.basic_eligibility.passed?)
       redirect_to eligibility_path
     end
   end
