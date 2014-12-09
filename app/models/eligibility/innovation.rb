351ffb14bec423c3c7c0b123cbd1b959a14b50c3
Michael Ly <michael.ly@bitzesty.com>
2014-12-09 17:05:51 +0000

    updated innovation copy

--- app/models/eligibility/innovation.rb
+++ app/models/eligibility/innovation.rb
@@ -6,5 +6,6 @@ class Eligibility::Innovation < Eligibility
   property :innovative_product, boolean: true, label: "Do you have an innovative product/service/initiative?", accept: :true
   property :number_of_innovative_products, positive_integer: true, label: "How many?", accept: :not_nil
   property :was_on_market_for_two_years, boolean: true, label: "Has it been released to the market for at least two years?", accept: :true
-  property :had_impact_on_commercial_performace_over_two_years, boolean: true, lable: "Has the innovation had a measurable positive impact on your commercial performance over at least the last two years?", accept: :true
+  property :innovation_recouped_investments, boolean: true, label: "Has the innovation recouped your initial investments in it?", accept: :true
+  property :had_impact_on_commercial_performace_over_two_years, boolean: true, label: "Has the innovation had a measurable positive impact on your commercial performance over at least the last two years?", accept: :true
 end
