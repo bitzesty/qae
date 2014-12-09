cb8e87662b8a0bfe63c9a233be055580f4d4eac2
Michael Ly <michael.ly@bitzesty.com>
2014-12-09 14:56:52 +0000

    Made the step titles consistent and use letter steps

--- app/forms/qae_2014_forms/international_trade.rb
+++ app/forms/qae_2014_forms/international_trade.rb
@@ -9,15 +9,15 @@ class QAE2014Forms
     def trade
       @trade ||= QAEFormBuilder.build 'Apply for the International Trade Award' do
 
-        step 'Company Information', &QAE2014Forms.trade_step1
+        step 'Company Information', 'Company Info', &QAE2014Forms.trade_step1
 
-        step 'Commercial Performance', &QAE2014Forms.trade_step2
+        step 'Commercial Performance', 'Commercial Performance', &QAE2014Forms.trade_step2
 
-        step 'Description of Goods or Services', &QAE2014Forms.trade_step3
+        step 'Description of Goods or Services', 'Goods or Services', &QAE2014Forms.trade_step3
 
-        step 'Declaration of Corporate Responsibility', &QAE2014Forms.trade_step4
+        step 'Declaration of Corporate Responsibility', 'Corporate Responsibility', &QAE2014Forms.trade_step4
 
-        step 'Authorisation/Monitoring', &QAE2014Forms.trade_step5
+        step 'Authorisation/Monitoring', 'Authorisation', &QAE2014Forms.trade_step5
 
       end
     end
