b3f270f542307e9e3c0b9565cf1eb48ae51d4245
Victor Bilyk <victorbilyk@gmail.com>
2014-12-09 11:11:15 +0600

    fixing travis build

--- travis.sh
+++ travis.sh
@@ -2,7 +2,9 @@
 
 set +e
 
-bundle exec rspec spec --tag ~skip_travis
+RAILS_ENV=test bundle exec rake db:create
+RAILS_ENV=test bundle exec rake db:migrate
+RAILS_ENV=test bundle exec rspec spec --tag ~skip_travis
 rv=$?
 
 exit $rv
