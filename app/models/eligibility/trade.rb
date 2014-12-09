3054b76ace963b25dde4f76b37d488eac7791db4
Michael Ly <michael.ly@bitzesty.com>
2014-12-09 17:11:15 +0000

    COPY updated award eligibilities

--- app/models/eligibility/trade.rb
+++ app/models/eligibility/trade.rb
@@ -4,7 +4,7 @@ class Eligibility::Trade < Eligibility
 
   validates :qae_for_trade_expiery_date, presence: true, if: :current_holder_of_qae_for_trade?
 
- property :sales_above_100_000_pounds, boolean: true, label: "Are your annual sales from export above £100,000?", accept: :true
+ property :sales_above_100_000_pounds, boolean: true, label: "Have you had £100,000 or more overseas sales in each of the last three years?", accept: :true
   property :any_dips_over_the_last_three_years, label: "Have you had any dips in your overseas sales over the last three years?", accept: :true, boolean: true
   property :current_holder_of_qae_for_trade, label: "Are you a current holder of a Queen's Award for International Trade?", boolean: true, accept: :all
   property :qae_for_trade_expiery_date, values: %w(2015 2016 2017 2018 2019), accept: :not_nil_if_current_holder_of_qae_for_trade, label: 'When does it expire?'
