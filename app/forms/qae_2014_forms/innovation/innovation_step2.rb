fc810dc6fe9032f93a99d05d1b53d132c2eae389
Michael Ly <michael.ly@bitzesty.com>
2014-12-15 16:20:03 +0000

    Switching sections B and C for award forms

---
+++ app/forms/qae_2014_forms/innovation/innovation_step2.rb
@@ -0,0 +1,157 @@
+class QAE2014Forms
+  class << self
+    def innovation_step2
+      @innovation_step2 ||= Proc.new {
+        context %Q{
+          <p>When answering these questions please avoid using technical jargon where possible. If you have more than one innovative product/service you will need to submit more than one application.</p>
+        }
+
+        textarea :innovation_desc_short, 'Briefly describe your product/service/intiative' do
+          ref 'B 1'
+          required
+          context %Q{
+            <p>e.g. 'innovation in the production of injectable general anaesthetic.'</p>
+          }
+          rows 2
+          words_max 15
+        end
+
+        textarea :innovation_desc_long, 'Describe your innovative product/service/initiative in detail' do
+          classes "sub-question"
+          required
+          context %Q{
+            <p>Describe the product/service/intitative itself, explain any aspect(s) you believe innovative, and why you believe it is innovative: consider its uniqeueness and the challenges you had to overcome. Also explain how it fits within the overall business e.g. is it your sole product.</p>
+          }
+          rows 5
+          words_max 800
+        end
+
+        textarea :innovation_context, 'Describe the context of your innovation, why and how it came about.' do
+          ref 'B 2'
+          required
+          context %Q{
+            <p>Outline the disadvantages, if any, of your own/competing products/services/intiatives prior to the innovation. Or otherwise, how you identified a gap in the market.</p>
+          }
+          rows 5
+          words_max 500
+        end
+
+        textarea :innovation_overcomes_issues, "Discuss the degree to which your innovation solves prior issues, and any special difficulties you overcame in achieving these solutions." do
+          ref 'B 3'
+          required
+          rows 5
+          words_max 800
+        end
+
+        textarea :innovation_befits_details, "Give details of benefits received by your customers and your business as a result of the innovation." do
+          ref 'B 4'
+          required
+          context %Q{
+            <p>e.g. increased efficiency, reduction in costs, design/production/marketing/distribution improvements, better after-sales support, reduced downtime or increased reliability. You can also include testimonials to support your claim.</p>
+          }
+          rows 5
+          words_max 800
+        end
+
+        textarea :innovation_competitors, "Identify and describe any products/services/initiatives made by other organisations that compete with your innovation, and explain how your innovation differs." do
+          ref 'B 5'
+          required
+          rows 5
+          words_max 300
+        end
+
+        options :innovation_any_contributors, 'Did any external organisation or individiual contribute to this product/service/initiative?' do
+          ref 'B 6'
+          required
+          context %Q{
+            <p>Excluding suppliers and any joint entrant(s) named in A7.</p>
+          }
+          yes_no
+        end
+
+        textarea :innovation_contributors, 'Please enter their name(s) and explain their contribution(s).' do
+          classes "sub-question"
+          required
+          conditional :innovation_any_contributors, :yes
+          rows 5
+          words_max 500
+        end
+
+        options :innovation_contributors_aware, 'Are they aware that you are applying for this award?' do
+          classes "sub-question"
+          required
+          conditional :innovation_any_contributors, :yes
+          yes_no
+          option :some, 'Some are aware'
+        end
+
+        options :innovation_under_license, 'Is the product/service/initiative under license from another organisation?' do
+          ref 'B 7'
+          yes_no
+        end
+
+        textarea :innovation_license_terms, 'Briefly describe the licensing arrangement.' do
+          classes "sub-question"
+          required
+          conditional :innovation_under_license, :yes
+          rows 5
+          words_max 100
+        end
+
+        options :innovations_grant_funding, 'Have you received any grant funding to support this innovation?' do
+          ref 'B 8'
+          required
+          yes_no
+        end
+
+        textarea :innovation_grant_funding_sources, "Please give details of date(s), source(s) and level(s) of funding." do
+          classes "sub-question"
+          required
+          conditional :innovations_grant_funding, :yes
+          rows 5
+          words_max 300
+        end
+
+        number :innovation_years, 'How long has it been since the product/service/initiative was released into the marketplace?' do
+          required
+          ref 'B 9'
+          max 100
+          unit ' years'
+          style "small inline"
+        end
+
+        options :innovation_released_by_applicant, "Was the product/service/intiative released by you?" do
+          classes "sub-question"
+          required
+          yes_no
+        end
+
+        number :innovation_years_by_applicant, 'How many years have you had it in the marketplace?' do
+          classes "regular-question inline-input-question"
+          required
+          conditional :innovation_released_by_applicant, :no
+          max 100
+          unit ' years'
+          style "small inline"
+        end
+
+        textarea :innovation_additional_comments, 'Additional comments' do
+          classes "sub-question"
+          rows 5
+          words_max 200
+        end
+
+        materials :innovation_materials, 'If there is additional material you feel would help us to assess your entry then you can add up to 4 files or links here.' do
+          ref 'B 10'
+          context %Q{
+            <p>We can't guarantee these will be reviewed, so inlcude any vital information within the form.</p>
+            <p>You may upload files of less than 5mb each in either MS Word Document, PDF, MS Excel Spreadsheet or MS Powerpoint Presentation formats. You may link to videos, websites or other media you feel relevant.</p>
+          } # TODO!
+          help 'Information we will not review', %Q{
+            <p>We will not consider business plans, annual accounts or company policy documents. Additional materials should not be used as a substitue for completing sections of the form.</p>
+          }
+        end
+      }
+    end
+  end
+end
---
+++ app/forms/qae_2014_forms/international_trade/international_trade_step2.rb
@@ -0,0 +1,97 @@
+
+class QAE2014Forms
+  class << self
+    def trade_step2
+      @trade_step2 ||= Proc.new {
+        textarea :trade_desc_whole, 'Describe your business as a whole' do
+          ref 'B 1'
+          required
+          context %Q{
+            <p>Include a brief history, your overall growth strategy, corporate targets/direction/vision, and a full description of the products/services you export.</p>
+          }
+          rows 5
+          words_max 500
+        end
+
+        options :trade_goods_amount, 'How many types of goods/services make up your international trade?' do
+          ref 'B 2'
+          required
+          option '1', '1'
+          option '2', '2'
+          option '3', '3'
+          option '4', '4'
+          option '5', '5'
+        end
+
+        textarea :goods_desc_short, 'Brief description of the goods and/or services being traded overseas' do
+          classes "sub-question"
+          required
+          context %Q{
+            <p>e.g. 'design and manufacture of bespoke steel windows and doors'. If relevant, give details of material used or end use. </p>
+          }
+          rows 2
+          words_max 15
+        end
+
+        number :total_overseas_trade, '% of your total overseas trade' do
+          classes "sub-question"
+        end
+
+        textarea :trade_plans_desc, 'Describe your international and domestic trading strategies (plans), their vision/objectives for the future, their method of implementation, and how your actual performance compared to the plans set out.' do
+          ref 'B 3'
+          required
+          context %Q{
+            <p>Include, for example, comparisons between domestic and international strategies, treatment of different markets (linking to top performing markets), market research, market development, routes to market, after sales and technical advice, staff language training, export practices, overseas distributors, inward/outward trade missions, trade fairs and visits to existing/potential markets. Make sure you explain how your actual performance compares to your planned performance.</p>
+          }
+          rows 5
+          words_max 800
+        end
+
+        header :overseas_markets_header, "Overseas Markets" do
+        end
+
+        textarea :markets_geo_spread, "Describe the geographical spread of your overseas markets." do
+          ref 'B 4'
+          required
+          context %Q{
+            <p>Include evidence of how you segment and manage geographical regions. Please supply market share information. </p>
+          }
+          rows 5
+          words_max 500
+        end
+
+        textarea :top_overseas_sales, "State the percentage of total overseas sales made to each of your top 5 overseas countries during the final year of your entry." do
+          classes "sub-question"
+          required
+          rows 5
+          words_max 200
+        end
+
+        textarea :identify_new_overseas, "Identify new overseas markets established during your period of entry, and their contribution to total overseas sales." do
+          classes "sub-question"
+          required
+          rows 5
+          words_max 300
+        end
+
+        textarea :trade_factors, "Give details of any special challenges affecting your trade in goods or services, and how you overcame them." do
+          ref 'B 5'
+          required
+          rows 5
+          words_max 200
+        end
+
+        materials :trade_materials, 'If there is additional material you feel would help us to assess your entry then you can add up to 4 files or links here.' do
+          ref 'B 7'
+          context %Q{
+            <p>We can't guarantee these will be reviewed, so inlcude any vital information within the form.</p>
+            <p>You may upload files of less than 5mb each in either MS Word Document, PDF, MS Excel Spreadsheet or MS Powerpoint Presentation formats. Or MP4 (video) files of up to TODOmb.</p>
+          } # TODO!
+          help 'Information we will not review', %Q{
+            <p>We will not consider business plans, annual accounts or company policy documents. Additional materials should not be used as a substitue for completing sections of the form.</p>
+          }
+        end
+      }
+    end
+  end
+end
