b587c1d426727ee42c4a283297ace15bd465cd02
Michael Ly <michael.ly@bitzesty.com>
2014-12-17 12:15:56 +0000

    Updated the development eligibility questions

--- app/models/eligibility/development.rb
+++ app/models/eligibility/development.rb
@@ -1,6 +1,7 @@
 class Eligibility::Development < Eligibility
   AWARD_NAME = 'Sustainable Development'
 
-  property :sustainable_development, boolean: true, label: "Has your organisation had a substantial sustainable development iniative for at least the last two years?", accept: :true
-  property :development_contributed_to_commercial_success, boolean: true, label: "Can you demonstrate that your sustainable development is accompanied by - and contributes to - commercial success?", accept: :true, hint: "If figures such as profitability, turnover and market share have met/exceeded business plan targets, then you can demonstrate 'commercial success'."
+  property :sustainable_development, boolean: true, label: "Have you had a substantial sustainable development initiative for at least the last two years?", accept: :true
+  property :demonstrate_development_activities, boolean: true, label: "Can you provide quantitative data to demonstrate your sustainable development activities?", accept: :true, hint: "e.g. energy use per unit of production, % of waste recycled, % of materials sourced fair trade."
+  property :development_contributed_to_commercial_success, boolean: true, label: "Has your sustainable development had a positive impact on your commercial success over the last two years?", accept: :true, hint: "e.g. increased profitability/turnover, created new markets, improved market share."
 end
