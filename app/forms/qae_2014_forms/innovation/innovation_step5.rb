63bb23b2083987ce1b95d5f8c790081cf3038323
Michael Ly <michael.ly@bitzesty.com>
2014-12-17 15:55:27 +0000

    Updated innovation E

--- app/forms/qae_2014_forms/innovation/innovation_step5.rb
+++ app/forms/qae_2014_forms/innovation/innovation_step5.rb
@@ -2,25 +2,31 @@ class QAE2014Forms
   class << self
     def innovation_step5
       @innovation_step5 ||= Proc.new {
-        options :agree_to_be_contacted, %Q{We will contact you regarding your entry. We may also wish to contact you about other issues relating to The Queen's Awards for Enterprise (eg. acting as a case study). Are you happy for us to do this?} do
+        # TODO Pull in info for A13
+        confirm :confirmation_of_consent, 'Confirmation of consent' do
           ref 'E 1'
-          yes_no
+          required
+          text 'I confirm that I have the consent of the head of my organisation (as in A13) to submit this entry form.'
         end
 
-        confirm :confirmation_of_consent, 'Confirmation of consent' do
+        confirm :agree_to_be_contacted_by_qae, "Contact from Queen's Award for Enterprise" do
           ref 'E 2'
-          required
-          text 'I confirm that I have the consent of the Head of the applicant business (as identified in A11) to submit this entry form.'
+          text "I am happy to be contacted about Queen's Award for Enterprise issues not related to my application (e.g. acting as a case study, newsletters, other info)."
+        end
+
+        confirm :agree_to_be_contacted_by_dept, "Contact from Department of Business, Innovation and Skills." do
+          ref 'E 3'
+          text "I am happy to be contacted by the Department of Business, Innovation and Skills."
         end
 
         confirm :entry_confirmation, 'Confirmation of entry' do
-          ref 'E 3'
+          ref 'E 4'
           required
           text %Q{
             By ticking this box, I certify that all the particulars given and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld. I undertake to notify The Queen’s Awards Office of any changes to the information I have provided in this entry form.
             <br>
             <br>
-            I am not aware of any matter which might cast doubt upon the worthiness of this business unit to receive a Queen’s Award. I consent to all necessary enquiries being made by The Queen’s Awards Office in relation to this entry. This includes enquiries made of Government Departments and Agencies in discharging its responsibilities to vet any business unit which might be granted a Queen’s Award to ensure the highest standards of propriety.
+            I am not aware of any matter which might cast doubt upon the worthiness of this business unit to receive a Queen’s Award. I consent to all necessary enquiries being made by The Queen’s Awards Office in relation to this entry. This includes enquiries made of Government Departments and Agencies in discharging its responsibilities to vet any business unit which might be granted a Queen’s Award to ensure the highest standards of propriety. 
           }
         end
 
