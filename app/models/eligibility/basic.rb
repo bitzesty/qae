af471b9ae7a153e8d49994a6bfd2d96036664100
Vasili Kachalko <amarant.st@gmail.com>
2014-12-22 19:12:49 +0300

    move duplicating code into a method

--- app/models/eligibility/basic.rb
+++ app/models/eligibility/basic.rb
@@ -2,13 +2,17 @@ class Eligibility::Basic < Eligibility
   AWARD_NAME = 'General'
 
   property :kind, values: %w[application nomination], label: "Do you want to apply for an award for your organisation, or nominate an individual for the Enterprise Promotion award?", accept: :not_nil
-  property :based_in_uk, boolean: true, label: "Is your organisation based in the UK?", accept: :true, if: proc { kind_value.nil? || kind.application? }
-  property :has_management_and_two_employees, boolean: true, label: "Does it have two or more full-time UK employees?", accept: :true, if: proc { kind_value.nil? || kind.application? }
-  property :organization_kind, values: %w[business charity], label: "What kind of organisation is it?", accept: :not_nil, if: proc { kind_value.nil? || kind.application? }
-  property :industry, values: %w[product_business service_business either_business], label: "Is your business mainly a:", accept: :not_nil_or_charity, if: proc { (kind_value.nil? || kind.application?) && (!organization_kind_value || !organization_kind.charity?) }
-  property :registered, boolean: true, label: "Is it registered with the UK Government?", accept: :not_nil, if: proc { kind_value.nil? || kind.application? }
-  property :self_contained_enterprise, boolean: true, label: "Does it act as a self-contained operational unit?", accept: :true, if: proc { (kind_value.nil? || kind.application?) && !registered? }
-  property :current_holder, boolean: true, label: "Are you a current Queen's Award holder in any category?", accept: :not_nil, if: proc { kind_value.nil? || kind.application? }
+  property :based_in_uk, boolean: true, label: "Is your organisation based in the UK?", accept: :true, if: proc { application? }
+  property :has_management_and_two_employees, boolean: true, label: "Does it have two or more full-time UK employees?", accept: :true, if: proc { application? }
+  property :organization_kind, values: %w[business charity], label: "What kind of organisation is it?", accept: :not_nil, if: proc { application? }
+  property :industry, values: %w[product_business service_business either_business], label: "Is your business mainly a:", accept: :not_nil_or_charity, if: proc { application? && (!organization_kind_value || !organization_kind.charity?) }
+  property :registered, boolean: true, label: "Is it registered with the UK Government?", accept: :not_nil, if: proc { application? }
+  property :self_contained_enterprise, boolean: true, label: "Does it act as a self-contained operational unit?", accept: :true, if: proc { application? && !registered? }
+  property :current_holder, boolean: true, label: "Are you a current Queen's Award holder in any category?", accept: :not_nil, if: proc { application? }
+
+  def application?
+    kind_value.nil? || kind.application?
+  end
 
   def eligible?
     current_step_index = questions.index(current_step) || questions.size - 1
