be367b4fbbd87b11d09333ea6c5c79aa756e89e5
Vasili Kachalko <amarant.st@gmail.com>
2014-12-23 15:03:36 +0300

    authenticate user on account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,5 @@
 class AccountsController < ApplicationController
+  before_action :authenticate_user!
   before_action :check_basic_eligibility, :check_award_eligibility
   before_action :restrict_access_if_admin_in_read_only_mode!, only: [
     :new, :create, :update, :destroy,
