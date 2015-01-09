743a869da939265183ae240aa66a006763fa84e4
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-01-09 17:32:22 +0300

    [Devise] emails enabled

---
+++ db/migrate/20150109142716_add_confirmable_devise_fields_to_users.rb
@@ -0,0 +1,11 @@
+class AddConfirmableDeviseFieldsToUsers < ActiveRecord::Migration
+  def change
+    change_table(:users) do |t|
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+    end
+
+    add_index :users, :confirmation_token,   unique: true
+  end
+end
