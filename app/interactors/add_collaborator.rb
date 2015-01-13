90cd9170b8dc89651964de9f8efb0fe390168b21
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-01-13 19:37:32 +0300

    [QAE1019] Collaborators: send better collaboration email instead of default devise's

--- app/interactors/add_collaborator.rb
+++ app/interactors/add_collaborator.rb
@@ -1,5 +1,13 @@
 class AddCollaborator
-  attr_reader :current_user, :account, :params, :collaborator, :email, :success
+  attr_reader :current_user, 
+              :account, 
+              :params, 
+              :collaborator, 
+              :email, 
+              :success, 
+              :new_user, 
+              :generated_password,
+              :devise_confirmation_token
 
   def initialize(current_user, account, params)
     @current_user = current_user
@@ -25,23 +33,42 @@ class AddCollaborator
       return user if user.present? && user.account.blank?
     end
     
+    @new_user = true
     User.new(params)
   end
 
   def persist!
-    collaborator.agreed_with_privacy_policy = '1'
     collaborator.account = account
-    collaborator.password = Devise.friendly_token.first(8)
-    collaborator.skip_confirmation_notification!
+
+    if new_user?
+      collaborator.agreed_with_privacy_policy = '1'
+      collaborator.password = set_generated_password
+      collaborator.skip_confirmation_notification!
+      collaborator.send(:generate_confirmation_token!)
+      @devise_confirmation_token = @collaborator.instance_variable_get("@raw_confirmation_token")
+    end
 
     @success = collaborator.save
   end
 
   def send_collaboration_email!
-    Users::CollaborationMailer.delay.access_granted(current_user, collaborator)
+    Users::CollaborationMailer.delay.access_granted(
+      current_user, 
+      collaborator, 
+      new_user,
+      generated_password,
+      devise_confirmation_token)
+  end
+
+  def set_generated_password
+    @generated_password = SecureRandom.hex(6)
   end
 
   def success?
     @success
+  end
+
+  def new_user?
+    @new_user
   end
 end
