2823910084e23a688cd82488ba598657a694fb2e
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-01-18 15:17:27 +0300

    [QAE1019] fixed collaborators permissions specs

--- spec/features/account/collaborators_spec.rb
+++ spec/features/account/collaborators_spec.rb
@@ -54,19 +54,33 @@ So that they can collaborate form answers
                               role: "regular"
   end
 
-  before do
-    login_as account_admin
-    visit account_collaborators_path
-  end
+  describe "Permissions" do
+    describe "I'm logged in as account admin and can see the List of Collaborators" do
+      before do
+        login_as account_admin
+        visit account_collaborators_path
+      end
 
-  describe "List of Collaborators" do
-    it "Should see list of account collaborators, excluding his self" do
-      expect_to_see "Collaborators"
+      it "Should see list of account collaborators, excluding his self" do
+        expect_to_see "Collaborators"
 
-      within(".js-collaborators-list") do
-        expect_to_see another_account_admin.decorate.full_name
-        expect_to_see regular_admin.decorate.full_name
-        expect_to_see_no account_admin.decorate.full_name
+        within(".js-collaborators-list") do
+          expect_to_see another_account_admin.decorate.full_name
+          expect_to_see regular_admin.decorate.full_name
+          expect_to_see_no account_admin.decorate.full_name
+        end
+      end
+    end
+
+    describe "I'm logged in as regular admin and I can't access the Collaborators section" do
+      before do
+        login_as regular_admin
+        visit account_collaborators_path
+      end
+
+      it "Should restrict access for non account admins" do
+        expect_to_see "Access denied!"
+        expect(current_path).to be_eql(root_path)
       end
     end
   end
