a280f05838e24d569215bb85f8ef9d5f6cf2e393
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-01-19 13:04:00 +0300

    [QAE1019] Fixed disabled submission conditions

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,8 +1,8 @@
 module FormHelper
-  def possible_read_only_ops
+  def possible_read_only_ops(submission=nil)
     ops = {}
 
-    if admin_in_read_only_mode?
+    if admin_in_read_only_mode? || (submission && !current_user.account_admin?)
       ops[:disabled] = 'disabled'  
       ops[:class] = 'read_only'
     end
