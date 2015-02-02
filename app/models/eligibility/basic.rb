7e1f74615eb92a28c478d8afd08fde97bf315722
Leszek <andrukanisleszek@gmail.com>
2015-02-02 14:13:29 +0100

    Improve formatting of Eligibility models

--- app/models/eligibility/basic.rb
+++ app/models/eligibility/basic.rb
@@ -1,13 +1,46 @@
 class Eligibility::Basic < Eligibility
   AWARD_NAME = 'General'
 
-  property :kind, values: %w[application nomination], label: "Do you want to apply for an award for your organisation, or nominate an individual for the Enterprise Promotion award?", accept: :not_nil
-  property :based_in_uk, boolean: true, label: "Is your organisation based in the UK?", accept: :true, if: proc { application? }
-  property :has_management_and_two_employees, boolean: true, label: "Does it have two or more full-time UK employees?", accept: :true, if: proc { application? }
-  property :organization_kind, values: %w[business charity], label: "What kind of organisation is it?", accept: :not_nil, if: proc { application? }
-  property :industry, values: %w[product_business service_business either_business], label: "Is your business mainly a:", accept: :not_nil_or_charity, if: proc { application? && (!organization_kind_value || !organization_kind.charity?) }
-  property :self_contained_enterprise, boolean: true, label: "Is your organisation a self-contained operational unit?", accept: :true, if: proc { application? }
-  property :current_holder, boolean: true, label: "Are you a current Queen's Award holder in any category?", accept: :not_nil, if: proc { application? }
+  property :kind,
+            values: %w[application nomination],
+            label: "Do you want to apply for an award for your organisation, or nominate an individual for the Enterprise Promotion award?",
+            accept: :not_nil
+
+  property :based_in_uk,
+            boolean: true,
+            label: "Is your organisation based in the UK?",
+            accept: :true,
+            if: proc { application? }
+
+  property :has_management_and_two_employees,
+            boolean: true,
+            label: "Does it have two or more full-time UK employees?",
+            accept: :true,
+            if: proc { application? }
+
+  property :organization_kind,
+            values: %w[business charity],
+            label: "What kind of organisation is it?",
+            accept: :not_nil,
+            if: proc { application? }
+
+  property :industry,
+            values: %w[product_business service_business either_business],
+            label: "Is your business mainly a:",
+            accept: :not_nil_or_charity,
+            if: proc { application? && (!organization_kind_value || !organization_kind.charity?) }
+
+  property :self_contained_enterprise,
+            boolean: true,
+            label: "Is your organisation a self-contained operational unit?",
+            accept: :true,
+            if: proc { application? }
+
+  property :current_holder,
+            boolean: true,
+            label: "Are you a current Queen's Award holder in any category?",
+            accept: :not_nil,
+            if: proc { application? }
 
   def application?
     kind_value.nil? || kind.application?
