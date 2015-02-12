d720a92bba52738651f953e946842e8949cbfc84
Vasili Kachalko <amarant.st@gmail.com>
2015-02-12 18:31:39 +0300

    add 4th EP step (no wireframes yet)

--- app/forms/qae_2014_forms/enterprise_promotion.rb
+++ app/forms/qae_2014_forms/enterprise_promotion.rb
@@ -1,16 +1,16 @@
 require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step1'
 require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step2'
 require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step3'
+require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step4'
 
 class QAE2014Forms
   class << self
     def promotion
       @promotion ||= QAEFormBuilder.build 'Apply for the Enterprise Promotion Award' do
-
         step 'Nominee', 'Nominee', &QAE2014Forms.promotion_step1
         step 'Nominee background', 'Nominee background', &QAE2014Forms.promotion_step2
         step 'Recommendation', 'Recommendation', &QAE2014Forms.promotion_step3
-
+        step 'Letters of support', 'Letters of support', &QAE2014Forms.promotion_step4
       end
     end
   end
---
+++ app/forms/qae_2014_forms/enterprise_promotion/enterprise_promotion_step4.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+class QAE2014Forms
+  class << self
+    def promotion_step4
+      @promotion_step4 ||= Proc.new {
+        header :letters_of_support_header, 'To be implemented: waiting for wireframes' do
+          ref 'D 1'
+        end
+      }
+    end
+  end
+end
