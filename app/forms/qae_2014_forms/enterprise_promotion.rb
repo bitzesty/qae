5178baba1acd2a350507e41703700655235d803f
Vasili Kachalko <amarant.st@gmail.com>
2015-02-13 13:44:34 +0300

    added more fieds to ep form, fixed some bugs

--- app/forms/qae_2014_forms/enterprise_promotion.rb
+++ app/forms/qae_2014_forms/enterprise_promotion.rb
@@ -2,6 +2,7 @@ require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step1'
 require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step2'
 require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step3'
 require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step4'
+require 'qae_2014_forms/enterprise_promotion/enterprise_promotion_step5'
 
 class QAE2014Forms
   class << self
@@ -11,6 +12,7 @@ class QAE2014Forms
         step 'Nominee background', 'Nominee background', &QAE2014Forms.promotion_step2
         step 'Recommendation', 'Recommendation', &QAE2014Forms.promotion_step3
         step 'Letters of support', 'Letters of support', &QAE2014Forms.promotion_step4
+        step 'Authorisation and monitoring', 'Authorisation and monitoring', &QAE2014Forms.promotion_step5
       end
     end
   end
