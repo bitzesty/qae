4a863e50cf372c505daacf8bba919b47ec4e006c
Matthew Ford <matt@bitzesty.com>
2015-02-18 17:08:21 +0000

    seeds.rb Users not valid - commented out for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,18 @@ unless Admin.exists?
   Admin.create!(email: 'admin@example.com', password: 'admin123').tap {|admin| admin.confirm!}
 end
 
-unless User.exists?
-  5.times do |i|
-    User.create!(
-      email: "user#{i}@example.com",
-      password: 'password',
-      agreed_with_privacy_policy: "1"
-    )
-  end
-end
+# unless User.exists?
+#   5.times do |i|
+#     User.create!(
+#       email: "user#{i}@example.com",
+#       password: 'password',
+#       agreed_with_privacy_policy: "1"
+#     )
+#   end
+# end
 
-unless Comment.exists?
-  FormAnswer.first(3).each do |app|
-    app.comments.create(body: "Random comment", author: Admin.last)
-  end
-end
+# unless Comment.exists?
+#   FormAnswer.first(3).each do |app|
+#     app.comments.create(body: "Random comment", author: Admin.last)
+#   end
+# end
