f7c2447b6e26f2cd7ade9d73bc526719f38e2612
Leszek <andrukanisleszek@gmail.com>
2015-02-25 13:20:20 +0100

    Add SIC codes

---
+++ lib/sic_code.rb
@@ -0,0 +1,33 @@
+require 'csv'
+
+class SICCode < ActiveYaml::Base
+  set_root_path Rails.root
+  set_filename "sic_codes"
+
+  def self.load_csv(csv_filename = "#{Rails.root}/sic_codes.csv")
+    csv = CSV.parse File.open(csv_filename).read
+
+    headers = {
+      0 => 'code',
+      1 => 'description',
+      2 => 'year1',
+      3 => 'year2',
+      4 => 'year3',
+      5 => 'year4',
+      6 => 'year5',
+      7 => 'year6'
+    }
+
+    res = csv[1..-1].map do |row|
+      out = {}
+      headers.each do |index, attr_name|
+        out[attr_name] = row[index]
+      end
+      out
+    end
+
+    file = File.open("#{Rails.root}/sic_codes.yml", "w+")
+    file.write(res.to_yaml)
+    file.close
+  end
+end
