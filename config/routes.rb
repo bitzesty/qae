4c20d248cda87611fbb8c48ef4f691392c86cc00
Leszek <andrukanisleszek@gmail.com>
2015-03-01 22:47:25 +0100

    Extract the separate namespace for Assessor.
    Decided to separate the Assessor as separate namespace with maximal reusing the common structures.

--- config/routes.rb
+++ config/routes.rb
@@ -74,6 +74,11 @@ Rails.application.routes.draw do
     resources :dashboard, only: [:index]
   end
 
+  namespace :assessor do
+    root to: "dashboard#index"
+    resources :dashboard, only: [:index]
+  end
+
   namespace :admin do
     root to: "dashboard#index"
     resources :dashboard, only: [:index]
