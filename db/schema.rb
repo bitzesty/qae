2d57ba1528b02399381e98ec6fd48e585c8c505c
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-02 17:24:22 +0300

    [DB Scheme] updated with corrected audit_certificates table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227135432) do
+ActiveRecord::Schema.define(version: 20150228145247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,15 +48,6 @@ ActiveRecord::Schema.define(version: 20150227135432) do
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
-  create_table "audit_certificates", force: :cascade do |t|
-    t.integer  "form_answer_id"
-    t.string   "attachment"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "audit_certificates", ["form_answer_id"], name: "index_audit_certificates_on_form_answer_id", using: :btree
-
   create_table "assessors", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -81,6 +72,15 @@ ActiveRecord::Schema.define(version: 20150227135432) do
   add_index "assessors", ["confirmation_token"], name: "index_assessors_on_confirmation_token", unique: true, using: :btree
   add_index "assessors", ["email"], name: "index_assessors_on_email", unique: true, using: :btree
   add_index "assessors", ["reset_password_token"], name: "index_assessors_on_reset_password_token", unique: true, using: :btree
+
+  create_table "audit_certificates", force: :cascade do |t|
+    t.integer  "form_answer_id", null: false
+    t.string   "attachment"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "audit_certificates", ["form_answer_id"], name: "index_audit_certificates_on_form_answer_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.integer  "commentable_id",   null: false
