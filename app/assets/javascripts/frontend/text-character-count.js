c9bcf49ee48558636d2ada5c7c520959e1c1f4b3
Michael Ly <michael.ly@bitzesty.com>
2015-03-11 17:52:41 +0000

    Fix char count position from label to input

--- app/assets/javascripts/frontend/text-character-count.js
+++ app/assets/javascripts/frontend/text-character-count.js
@@ -1,4 +1,11 @@
 $.fn.charcount = function() {
+  // Adds class to parent so that we can position the question and input closer
+  var prev_el = this.closest(".question-group").prev();
+  prev_el.addClass("char-spacing");
+  if (prev_el.hasClass("errors-container")) {
+    prev_el.prev().append("<span class='char-space'></div>");
+  }
+
   // Creates the character count elements
   this.wrap("<div class='char-count'></div>");
   this.after("<div class='char-text'>Word count: <span class='current-count'>0</span></div>");
