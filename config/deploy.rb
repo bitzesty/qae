b009f03ad24e8c81aa2ec0560ae33949f2991f30
Vasili Kachalko <amarant.st@gmail.com>
2015-03-13 20:37:55 +0300

    trying to update crontab after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,8 @@ set :linked_files, %w(config/database.yml config/secrets.yml .env)
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
+set :whenever_command, "bundle exec whenever"
+
 set :ssh_options, {
   forward_agent: true
 }
