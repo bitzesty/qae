ea8e250ddafca6e1f758bc227da8ea6deccb01e8
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-17 21:16:12 +0300

    Small correct with @user_award_forms_submitted

--- app/controllers/content_only_controller.rb
+++ app/controllers/content_only_controller.rb
@@ -43,7 +43,9 @@ class ContentOnlyController < ApplicationController
     @user_award_forms_innovation = @user_award_forms.where(award_type: "innovation")
     @user_award_forms_development = @user_award_forms.where(award_type: "development")
     @user_award_forms_promotion = @user_award_forms.where(award_type: "promotion")
-    @user_award_forms_submitted = current_user.account.form_answers.where.not(urn: nil).order("award_type")
+    @user_award_forms_submitted = current_user.account.form_answers
+                                                      .where(submitted: true)
+                                                      .order("award_type")
   end
 
   def get_current_form
