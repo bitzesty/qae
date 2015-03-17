272eead20b03d89053267706421c21870c136464
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-17 19:14:51 +0300

    [Capistrano] fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :default_env, { 'RAILS_ENV' => 'production' }
 set :rails_env, 'production'
-set :branch, ENV["BRANCH"] || 'production'
+set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
 server '52.17.4.150', user: 'qae', roles: %w{web app}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :default_env, { 'RAILS_ENV' => 'staging' }
 set :rails_env, 'staging'
-set :branch, ENV["BRANCH"] || 'staging'
+set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
 server '52.17.8.167', user: 'qae', roles: %w{web app}
