a7926fd644ef5a5b6c8ebdcf6f3ab54244fa0964
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-17 19:28:34 +0300

    [OLD SERVERS] added support of dev and demo servers to configs

--- config/initializers/active_job_shoryuken.rb
+++ config/initializers/active_job_shoryuken.rb
@@ -1,12 +1,14 @@
-# TODO: remove else condition - once old servers (dev and demo) will be terminated
-if (Rails.env.staging? || Rails.env.production?) && ENV["AWS_ACCESS_KEY_ID"]
-  Rails.application.configure do
-    config.active_job.queue_adapter = :shoryuken
-    config.active_job.queue_name_prefix = Rails.env
-    config.active_job.queue_name_delimiter = "_"
-  end
-else
-  Rails.application.configure do
-    config.active_job.queue_adapter = :inline
+# TODO: remove ENV condition - once old servers (dev and demo) will be terminated
+if Rails.env.staging? || Rails.env.production?
+  if ENV["AWS_ACCESS_KEY_ID"]
+    Rails.application.configure do
+      config.active_job.queue_adapter = :shoryuken
+      config.active_job.queue_name_prefix = Rails.env
+      config.active_job.queue_name_delimiter = "_"
+    end
+  else
+    Rails.application.configure do
+      config.active_job.queue_adapter = :inline
+    end
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,18 @@
-# TODO: remove else condition - once old servers (dev and demo) will be terminated
+# TODO: remove ENV condition - once old servers (dev and demo) will be terminated
 CarrierWave.configure do |config|
-  if (Rails.env.staging? || Rails.env.production?) && ENV["AWS_ACCESS_KEY_ID"]
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_REGION"]
-    }
-    config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
-    config.storage = :fog
-    config.fog_public = false
-  else
-    config.storage = :file
+  if Rails.env.staging? || Rails.env.production?
+    if ENV["AWS_ACCESS_KEY_ID"]
+      config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region: ENV["AWS_REGION"]
+      }
+      config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
+      config.storage = :fog
+      config.fog_public = false
+    else
+      config.storage = :file
+    end
   end
 end
