2d89f8f2c059d230494412b21837a2aec64bff4d
Michael Ly <michael.ly@bitzesty.com>
2015-03-18 17:54:58 +0000

    Restyle the settings page

--- app/inputs/separated_date_time_input.rb
+++ app/inputs/separated_date_time_input.rb
@@ -2,8 +2,8 @@ class SeparatedDateTimeInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options)
     out = ''.html_safe
 
-    out << @builder.text_field("formatted_#{attribute_name}_date", input_html_options.merge(class: 'datepicker'))
-    out << @builder.text_field("formatted_#{attribute_name}_time", input_html_options.merge(class: 'timepicker'))
+    out << @builder.text_field("formatted_#{attribute_name}_date", input_html_options.merge(class: "form-control datepicker", placeholder: "dd/mm/yyyy"))
+    out << @builder.text_field("formatted_#{attribute_name}_time", input_html_options.merge(class: "form-control timepicker", placeholder: "hh:mm"))
 
     out
   end
