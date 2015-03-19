c2e91907e31c2ea1f023f2ebc923f292ff7d9d88
Michael Ly <michael.ly@bitzesty.com>
2015-03-19 17:18:31 +0000

    Helper method for application_deadline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -113,4 +113,10 @@ class ApplicationController < ActionController::Base
     @submission_deadline ||= settings.deadlines.where(kind: "submission_end").first
   end
   helper_method :submission_deadline
+
+  def application_deadline(kind)
+    settings.deadlines.where(kind: kind).first.decorate.formatted_trigger_time
+  end
+  helper_method :application_deadline
+
 end
