fa95c4cf6bd71d1c45c04523d9ccd2edef743b2c
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-20 15:50:27 +0300

    [Capistrano] corrected production IPs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ set :rails_env, 'production'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.16.232.176', user: 'qae', roles: %w{web app}
-server '52.16.127.209', user: 'qae', roles: %w{web app}
+server '52.17.44.47', user: 'qae', roles: %w{web app}
+server '52.17.44.59', user: 'qae', roles: %w{web app}
