af32d65083a25e235fb2255d858a77a6f922d3e0
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-20 20:39:31 +0300

    [Capistrano] updated production IPS

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ set :rails_env, 'production'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.17.44.47', user: 'qae', roles: %w{web app}
-server '52.17.44.59', user: 'qae', roles: %w{web app}
+server '52.16.161.206', user: 'qae', roles: %w{web app}
+server '52.17.14.164', user: 'qae', roles: %w{web app}
