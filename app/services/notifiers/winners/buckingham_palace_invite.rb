0e5de2d541ff26aad45bd8773e2b02fbc5cbaa79
Vasili Kachalko <amarant.st@gmail.com>
2015-03-25 17:17:17 +0300

    change Shoryuken queue settings
    [ci skip]

--- app/services/notifiers/winners/buckingham_palace_invite.rb
+++ app/services/notifiers/winners/buckingham_palace_invite.rb
@@ -1,7 +1,7 @@
 class Notifiers::Winners::BuckinghamPalaceInvite
   include Shoryuken::Worker
 
-  shoryuken_options queue: "default", auto_delete: true
+  shoryuken_options queue: "#{Rails.env}_default", auto_delete: true
 
   def perform(sqs_msg, email)
     invite = PalaceInvite.where(email: email).first_or_create
