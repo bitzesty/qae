f55db1af907fe694d110af1e634ca10d4eef2edd
Matthew Ford <matt@bitzesty.com>
2015-03-26 16:51:32 +0000

    Update devise.rb
    change stretches in tests to 1 else 100

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -59,7 +59,7 @@ Devise.setup do |config|
   # ==> Configuration for :database_authenticatable
   # For bcrypt, this is the cost for hashing the password and defaults to 10. If
   # using other encryptors, it sets how many times you want the password re-encrypted.
-  config.stretches = 10
+  config.stretches = Rails.env.test? ? 1 : 100
 
   # Setup a pepper to generate the encrypted password.
   config.pepper = ENV['DEVISE_PEPPER']
