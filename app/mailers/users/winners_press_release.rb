2121abfc173194723d0710cc7ce6c0f9dc95bbfd
Vasili Kachalko <amarant.st@gmail.com>
2015-03-27 18:57:10 +0300

    use nomenee email for press comments request

--- app/mailers/users/winners_press_release.rb
+++ app/mailers/users/winners_press_release.rb
@@ -2,9 +2,16 @@ class Users::WinnersPressRelease < ApplicationMailer
   def notify(form_answer_id)
     @form_answer = FormAnswer.find(form_answer_id).decorate
     @user = @form_answer.user.decorate
+
+    email = if @form_answer.promotion?
+      @form_answer.document["nominee_email"]
+    else
+      @user.email
+    end
+
     @token = @form_answer.press_summary.token
     @subject = "[Queen's Awards for Enterprise] Press Comment"
 
-    mail to: @user.email, subject: @subject
+    mail to: email, subject: @subject
   end
 end
