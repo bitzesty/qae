d9f451b597c2e0595e215ed236e0e1121856dee4
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-30 13:55:07 +0300

    [Capistrano] added ability to run custom tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,4 +59,14 @@ namespace :deploy do
   after "deploy:reverted", "whenever:update_crontab"
 end
 
+# Invoke any rake task via capistrano
+# Examples:
+#   cap staging cap_rake:invoke task="school:regenerate_letters school_id=1"
+namespace :cap_rake do
+  desc "Invoke rake task"
+  task :invoke, roles: :backend do
+    run "cd #{current_path} && bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"
+  end
+end
+
 
