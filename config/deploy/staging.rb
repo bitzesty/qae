8de492865158127bfaa762d474c20a9ba71c6b4c
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-03-31 11:48:32 +0300

    [Capistrano] updated staging IPS

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ set :rails_env, 'staging'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.17.133.157', user: 'qae', roles: %w{web app}
-server '52.17.133.151', user: 'qae', roles: %w{web app}
+server '52.17.139.74', user: 'qae', roles: %w{web app}
+server '52.17.143.230', user: 'qae', roles: %w{web app}
