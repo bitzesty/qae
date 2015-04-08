03817b40772e662fcf8a161edeeb29dc8e52ca08
Leszek <andrukanisleszek@gmail.com>
2015-04-08 19:01:29 +0200

    Applications report table

--- app/models/form_answer_statistics/picker.rb
+++ app/models/form_answer_statistics/picker.rb
@@ -7,6 +7,35 @@ class FormAnswerStatistics::Picker
     out
   end
 
+  def applications_table
+    out = {}
+    registered_users = []
+    registered_users << User.where(created_at: (Time.now - 24.hours)..Time.now).count
+    registered_users << User.where(created_at: (Time.now - 7.days)..Time.now).count
+    registered_users << User.count
+    out[:registered_users] = { name: "Registered Users", counters: registered_users }
+
+
+    not_eligible = []
+    not_eligible << not_eligible(DateTime.now - 1.day)
+    not_eligible << not_eligible(DateTime.now - 7.days)
+    not_eligible << klass.where(state: "not_eligible").count
+    out[:applications_not_eligible] = { name: "Applications not eligible", counters: not_eligible }
+
+    in_progress = []
+    in_progress << application_in_progress(Time.now - 1.days)
+    in_progress << application_in_progress(Time.now - 7.days)
+    in_progress << klass.where(state: "application_in_progress").count
+    out[:applications_in_progress] = { name: "Applications in progress", counters: in_progress }
+
+    submitted = collect_submission_ranges(klass.all)
+    out[:applications_submitted] = {
+      name: "Applications Submitted",
+      counters: submitted
+    }
+    out
+  end
+
   def applications_completions
     # TODO: SCOPE WITH YEAR
     out = {}
@@ -31,8 +60,25 @@ class FormAnswerStatistics::Picker
 
   private
 
+  def application_in_progress(time_range)
+    klass.joins(:form_answer_transitions)
+      .where(state: "application_in_progress")
+      .where("form_answer_transitions.to_state = ? AND
+        form_answer_transitions.created_at > ?", "application_in_progress", time_range)
+      .uniq.count
+  end
+
+  def not_eligible(time_range)
+    klass.joins(:form_answer_transitions)
+      .where(state: "not_eligible")
+      .where("form_answer_transitions.to_state = ? AND
+        form_answer_transitions.created_at > ?", "not_eligible", time_range)
+      .uniq.count
+  end
+
   def submissions_query(scope, time_range)
     out = scope.joins(:form_answer_transitions)
+    out = out.where("form_answers.state = ?", "submitted")
     out = out.where("form_answer_transitions.to_state = ?", "submitted")
     out = out.where("form_answer_transitions.created_at > ?", time_range) if time_range
     out.uniq.count
