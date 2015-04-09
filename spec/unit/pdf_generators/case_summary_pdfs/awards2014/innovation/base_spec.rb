5d9779437714cdab2223c47055b3d38fb553ecd3
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-09 21:30:57 +0300

    [QAE1136] Rspec coverage

---
+++ spec/unit/pdf_generators/case_summary_pdfs/awards2014/innovation/base_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe "CaseSummaryPdfs::Awards2016::Innovation::Base" do
+  let(:award_type) { :innovation }
+  include_context "admin case summary pdf file checks"
+end
---
+++ spec/unit/pdf_generators/case_summary_pdfs/awards2014/trade/base_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe "CaseSummaryPdfs::Awards2016::Trade::Base" do
+  let(:award_type) { :trade }
+  include_context "admin case summary pdf file checks"
+end
