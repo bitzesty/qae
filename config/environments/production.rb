e0847299535b783ea0226a981d00525143a83df3
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-10 16:45:14 +0300

    [ASSET_HOST] commented for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: ENV["MAILER_HOST"] }
-  config.action_mailer.asset_host = ENV["MAILER_HOST"]
+  # TODO: set ASSET_HOST for production
+  # config.action_mailer.asset_host = ENV["MAILER_HOST"]
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
