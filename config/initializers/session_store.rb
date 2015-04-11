ebecb814510dbbd71bd0367fb5963843967a2c3e
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-11 11:25:33 +0300

    [SESSION STORE] oops. sorry

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,6 @@
 # We are setting up session_store settings for staging and production
 # in related environment config files:
 # config/environments/*.rb
-if Rails.development? || Rails.env.test?
+if Rails.env.development? || Rails.env.test?
   Rails.application.config.session_store :cookie_store, key: '_qae_session'
 end
