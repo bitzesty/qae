c9222e5c23ee6958dd60b67cd5d6601da5f9a4c5
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-14 15:05:31 +0300

    [Virus Scanner] population scans for existing attachments script corrects

--- app/tasks/populate_scan_associations_for_existing_attachments.rb
+++ app/tasks/populate_scan_associations_for_existing_attachments.rb
@@ -4,30 +4,36 @@ class PopulateScanAssociationsForExistingAttachments
   class << self
     def run
       AuditCertificate.all.each do |entry|
-        Scan.create(
-          filename: entry.attachment.current_path,
-          uuid: SecureRandom.hex(30),
-          audit_certificate_id: entry.id,
-          status: "clean"
-        )
+        if entry.scan.blank?
+          Scan.create(
+            filename: entry.attachment.current_path,
+            uuid: SecureRandom.hex(30),
+            audit_certificate_id: entry.id,
+            status: "clean"
+          )
+        end
       end
 
       FormAnswerAttachment.all.each do |entry|
-        Scan.create(
-          filename: entry.file.current_path,
-          uuid: SecureRandom.hex(30),
-          form_answer_attachment_id: entry.id,
-          status: "clean"
-        )
+        if entry.scan.blank?
+          Scan.create(
+            filename: entry.file.current_path,
+            uuid: SecureRandom.hex(30),
+            form_answer_attachment_id: entry.id,
+            status: "clean"
+          )
+        end
       end
 
       SupportLetterAttachment.all.each do |entry|
-        Scan.create(
-          filename: entry.attachment.current_path,
-          uuid: SecureRandom.hex(30),
-          support_letter_attachment_id: entry.id,
-          status: "clean"
-        )
+        if entry.scan.blank?
+          Scan.create(
+            filename: entry.attachment.current_path,
+            uuid: SecureRandom.hex(30),
+            support_letter_attachment_id: entry.id,
+            status: "clean"
+          )
+        end
       end
     end
   end
