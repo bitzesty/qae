39630663f4b8aab332c768c88de2c9a5399f5ec1
Vasili Kachalko <amarant.st@gmail.com>
2015-04-15 20:37:39 +0300

    supporters mailer changes

--- app/mailers/users/supporter_mailer.rb
+++ app/mailers/users/supporter_mailer.rb
@@ -4,6 +4,10 @@ class Users::SupporterMailer < ApplicationMailer
     @supporter = Supporter.find(supporter_id).decorate
     @form_answer = @supporter.form_answer.decorate
     @subject = "[Queen's Awards for Enterprise] Support Letter Request"
+    @nominee_name = ""
+    @nominee_name << @form_answer.document["nominee_info_first_name"]
+    @nominee_name << " "
+    @nominee_name << @form_answer.document["nominee_info_last_name"]
 
     mail to: @supporter.email, subject: @subject
   end
--- spec/mailers/users/supporter_mailer_spec.rb
+++ spec/mailers/users/supporter_mailer_spec.rb
@@ -5,16 +5,27 @@ describe Users::SupporterMailer do
   let(:supporter) { create(:supporter) }
   let(:mail) { Users::SupporterMailer.success(supporter.id, user.id) }
 
+ before do
+    form_answer = supporter.form_answer
+    form_answer.document = {
+      nominee_info_first_name: "Jon",
+      nominee_info_last_name: "Snow",
+    }
+    form_answer.save!
+  end
+
   it "renders the headers" do
     expect(mail.subject).to eq("[Queen's Awards for Enterprise] Support Letter Request")
     expect(mail.to).to eq([supporter.email])
     expect(mail.from).to eq(["no-reply@queens-awards-enterprise.service.gov.uk"])
   end
 
-  it "contains link to the support letter form" do
-    expect(mail.body.encoded).to have_link(
-      "go to the support letter form page",
+  it "contains link to the support letter form and nominee's name" do
+    expect(mail.html_part.decoded).to have_link(
+      "go to the support letter form page.",
       href: support_letter_url(access_key: supporter.access_key)
     )
+
+    expect(mail.body.encoded).to match("Jon Snow")
   end
 end
