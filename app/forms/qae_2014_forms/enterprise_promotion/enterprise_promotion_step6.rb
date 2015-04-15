24b34db98d973a57fadf0d7a2dbc7ec6458e6688
Michael Ly <michael.ly@bitzesty.com>
2015-04-15 18:22:30 +0100

    COPY capitalised `The` in `the Queen’s Award for Enterprise Promotion`

--- app/forms/qae_2014_forms/enterprise_promotion/enterprise_promotion_step6.rb
+++ app/forms/qae_2014_forms/enterprise_promotion/enterprise_promotion_step6.rb
@@ -65,7 +65,7 @@ class QAE2014Forms
           ref "F 3"
           required
           text %(
-            By ticking this box, I submit a nomination for consideration for the Queen’s Award for Enterprise Promotion 2015. I certify that all the particulars given, and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld.
+            By ticking this box, I submit a nomination for consideration for The Queen’s Award for Enterprise Promotion 2015. I certify that all the particulars given, and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld.
             <br><br>
             I am not aware of any matter which might cast doubt upon the worthiness of this individual to receive a Queen’s Award. I consent to all necessary enquiries being made by the Queen’s Awards Office in relation to this nomination. This includes enquiries made of Government Departments and Agencies in discharging its responsibilities to vet any individual which might be granted a Queen’s Award to ensure the highest standards of propriety. I undertake to notify the Queen’s Awards Office of any changes to the information provided, including any changes to the nominee’s personal details.
           )
