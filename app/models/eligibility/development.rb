48524f6a9e4b92c0fc8a6df3bc9c40ce5fa6e54e
Michael Ly <michael.ly@bitzesty.com>
2015-04-17 10:32:02 +0100

    Removed sus dev eligibility questions

--- app/models/eligibility/development.rb
+++ app/models/eligibility/development.rb
@@ -7,17 +7,21 @@ class Eligibility::Development < Eligibility
             label: "Have you had a substantial sustainable development initiative for at least the last two years?",
             accept: :true
 
-  property :demonstrate_development_activities,
-            boolean: true,
-            label: "Can you provide quantitative data to demonstrate your sustainable development activities?",
-            accept: :true,
-            hint: "e.g. energy use per unit of production, % of waste recycled, % of materials sourced fair trade.",
-            if: proc { !skipped? }
+  # REMOVE
+  # property :demonstrate_development_activities,
+  #           boolean: true,
+  #           label: "Can you provide quantitative data to demonstrate your sustainable development activities?",
+  #           accept: :true,
+  #           hint: "e.g. energy use per unit of production, % of waste recycled, % of materials sourced fair trade.",
+  #           if: proc { !skipped? }
+  # REMOVE
 
-  property :development_contributed_to_commercial_success,
-            boolean: true,
-            label: "Has your sustainable development had a positive impact on your commercial success in the last two years?",
-            accept: :true,
-            hint: "e.g. increased profitability/turnover, created new markets, improved market share.",
-            if: proc { !skipped? }
+  # REMOVE
+  # property :development_contributed_to_commercial_success,
+  #           boolean: true,
+  #           label: "Has your sustainable development had a positive impact on your commercial success in the last two years?",
+  #           accept: :true,
+  #           hint: "e.g. increased profitability/turnover, created new markets, improved market share.",
+  #           if: proc { !skipped? }
+  # REMOVE
 end
