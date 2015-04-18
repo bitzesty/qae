845ff1567c05575aaebf32ccc3c6e52d9ad5883f
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-18 21:11:11 +0300

    [File Uploaders] let's set extensions list in one place

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,4 +1,8 @@
 class FileUploader < CarrierWave::Uploader::Base
+  def extension_white_list
+    %w(jpg jpeg gif png chm csv diff doc docx dot dxf eps gml ics kml odp ods odt pdf ppt pptx ps rdf rtf sch txt wsdl xls xlsm xlsx xlt xml xsd xslt zip)
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
