50cad16e4dada06c9b668701f5ffd566daf3fce7
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-19 19:52:34 +0300

    [Uploaders] fix

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,8 +1,9 @@
 class FileUploader < CarrierWave::Uploader::Base
-  POSSIBLE_IMG_EXTENSIONS = %w(jpg jpeg gif png chm csv diff doc docx dot dxf eps gml ics kml odp ods odt pdf ppt pptx ps rdf rtf sch txt wsdl xls xlsm xlsx xlt xml xsd xslt zip)
+  POSSIBLE_IMG_EXTENSIONS = %w(jpg jpeg gif png)
+  POSSIBLE_DOC_EXTENSIONS = %w(chm csv diff doc docx dot dxf eps gml ics kml odp ods odt pdf ppt pptx ps rdf rtf sch txt wsdl xls xlsm xlsx xlt xml xsd xslt zip)
 
   def extension_white_list
-    POSSIBLE_IMG_EXTENSIONS
+    POSSIBLE_IMG_EXTENSIONS + POSSIBLE_DOC_EXTENSIONS
   end
 
   def store_dir
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ set :rails_env, 'staging'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.17.205.208', user: 'qae', roles: %w{web app db}
-server '52.17.104.57', user: 'qae', roles: %w{web app db}
+server '52.17.7.10', user: 'qae', roles: %w{web app db}
+server '52.16.156.145', user: 'qae', roles: %w{web app db}
