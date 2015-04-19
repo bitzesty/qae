88d5f3a0822adfe6e92e94d492f15698dede503f
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-19 21:00:45 +0300

    [STAGING] updated IPS

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ set :rails_env, 'staging'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.17.7.10', user: 'qae', roles: %w{web app db}
-server '52.16.156.145', user: 'qae', roles: %w{web app db}
+server '52.17.100.117', user: 'qae', roles: %w{web app db}
+server '52.17.116.152', user: 'qae', roles: %w{web app db}
