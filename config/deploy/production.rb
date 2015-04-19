895b809685c7945858db77c7bafa640a1605994b
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-20 01:25:51 +0300

    [Capistrano] updated Ips

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ set :rails_env, 'production'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.16.27.57', user: 'qae', roles: %w{web app db}
-server '52.17.129.85', user: 'qae', roles: %w{web app db}
+server '52.17.136.135', user: 'qae', roles: %w{web app db}
+server '52.17.176.9', user: 'qae', roles: %w{web app db}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ set :rails_env, 'staging'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.16.27.57', user: 'qae', roles: %w{web app db}
-server '52.17.129.85', user: 'qae', roles: %w{web app db}
+server '52.17.100.117', user: 'qae', roles: %w{web app db}
+server '52.17.116.152', user: 'qae', roles: %w{web app db}
