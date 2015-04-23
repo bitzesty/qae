fd1c1e84b3255c3530836758e2dbd65376a67cfa
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-23 16:20:46 +0300

    [Capistrano] set to use prod branhc

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :default_env, { 'RAILS_ENV' => 'production' }
 set :rails_env, 'production'
-set :branch, ENV["BRANCH"] || 'master'
+set :branch, ENV["BRANCH"] || 'production'
 
 # HARDCODED FOR NOW
 server '52.17.14.201', user: 'qae', roles: %w{web app db}
