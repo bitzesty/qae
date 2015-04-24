d47582a267161395a66e6c1be9f843a8ac898e8c
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-24 13:22:03 +0300

    [Capistrano] updated staging IPs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ set :rails_env, 'staging'
 set :branch, ENV["BRANCH"] || 'master'
 
 # HARDCODED FOR NOW
-server '52.17.100.117', user: 'qae', roles: %w{web app db}
-server '52.17.116.152', user: 'qae', roles: %w{web app db}
+server '52.17.212.1', user: 'qae', roles: %w{web app db}
+server '52.17.204.106', user: 'qae', roles: %w{web app db}
