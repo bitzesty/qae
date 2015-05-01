22af00a24234a4612050c27af70c3af2a9334f5e
Vasili Kachalko <amarant.st@gmail.com>
2015-05-01 15:46:44 +0300

    add promotion notifier for not shortlisted notification

--- app/mailers/users/notify_non_shortlisted_mailer.rb
+++ app/mailers/users/notify_non_shortlisted_mailer.rb
@@ -6,4 +6,12 @@ class Users::NotifyNonShortlistedMailer < ApplicationMailer
 
     mail to: @user.email, subject: @subject
   end
+
+  def ep_notify(form_answer_id)
+    @form_answer = FormAnswer.find(form_answer_id).decorate
+    @user = @form_answer.user.decorate
+    @subject = "Queen's Awards for Enterprise Promotion: Thank you for your nomination"
+
+    mail to: @user.email, subject: @subject
+  end
 end
--- spec/mailers/users/notify_non_shortlisted_mailer_spec.rb
+++ spec/mailers/users/notify_non_shortlisted_mailer_spec.rb
@@ -24,4 +24,22 @@ describe Users::NotifyNonShortlistedMailer do
       expect(mail.html_part.decoded).to match(user.decorate.full_name)
     end
   end
+
+  describe "#ep_notify" do
+    let(:form_answer) { create :form_answer, :promotion, :submitted, user: user }
+    let(:subject) {
+      "Queen's Awards for Enterprise Promotion: Thank you for your nomination"
+    }
+    let(:mail) { Users::NotifyNonShortlistedMailer.ep_notify(form_answer.id) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq(subject)
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["no-reply@queens-awards-enterprise.service.gov.uk"])
+    end
+
+    it "renders the body" do
+      expect(mail.html_part.decoded).to match(user.decorate.full_name)
+    end
+  end
 end
