e5ebab8cb99778b2289da3488c9eb84911fc668b
Matthew Ford <matt@bitzesty.com>
2015-05-02 13:16:35 +0100

    Change offsets of cron jobs and log output
    closes #456

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,12 @@
+set :output, File.expand_path("#{File.dirname __FILE__}/../log/cron_log.log")
+
 # All scheduled tasks need to run with a advisory_lock
 # To prevent two processes running them concurrently
 # User.with_advisory_lock('name', 0) { block }
 
-every 5.minutes do
+# Do not run jobs with the same offset
+
+every 7.minutes do
   runner "User.with_advisory_lock('email_notification', 0) { Notifiers::EmailNotificationService.run }"
 end
 
