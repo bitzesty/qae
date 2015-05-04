3ed633748afc77ebc597fde2af649bd77c26eb3a
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-05-04 20:22:00 +0300

    [Whenever] running of cron tasks based on ENV

--- app/models/cron_job.rb
+++ app/models/cron_job.rb
@@ -1,4 +1,4 @@
-class CronJobRun
+class CronJob
   def self.run(lock_name, timeout_seconds=0, sleep_before_run=5, &block)
     User.with_advisory_lock(lock_name, timeout_seconds) do
       sleep(sleep_before_run) if sleep_before_run.to_i > 0
