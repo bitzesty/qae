bee3b209a355e6d2fbebd3357db78cc32c46014f
Matthew Ford <matt@bitzesty.com>
2015-05-07 00:25:38 +0100

    Removing outdated configs

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,29 +1,14 @@
-# TODO: remove ENV condition - once old servers (dev and demo) will be terminated
 if Rails.env.staging? || Rails.env.production?
-  if ENV["AWS_SES_DOMAIN_NAME"]
-    Rails.application.configure do
-      config.action_mailer.delivery_method = :smtp
-      config.action_mailer.smtp_settings = {
-        address: "email-smtp.eu-west-1.amazonaws.com",
-        port: 587,
-        domain: ENV["AWS_SES_DOMAIN_NAME"],
-        authentication: :login,
-        user_name: ENV["AWS_SES_USERNAME"],
-        password: ENV["AWS_SES_PASSWORD"],
-        enable_starttls_auto: true
-      }
-    end
-  else
-    Rails.application.configure do
-      config.action_mailer.delivery_method = :smtp
-      config.action_mailer.smtp_settings = {
-        port:           587,
-        address:        'smtp.mailgun.org',
-        user_name:      ENV["mailgun_username"],
-        password:       ENV["mailgun_password"],
-        domain:         ENV["mailgun_domain"],
-        authentication: :plain
-      }
-    end
+  Rails.application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "email-smtp.eu-west-1.amazonaws.com",
+      port: 587,
+      domain: ENV["AWS_SES_DOMAIN_NAME"],
+      authentication: :login,
+      user_name: ENV["AWS_SES_USERNAME"],
+      password: ENV["AWS_SES_PASSWORD"],
+      enable_starttls_auto: true
+    }
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,13 @@
-# TODO: remove ENV condition - once old servers (dev and demo) will be terminated
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
-    if ENV["AWS_ACCESS_KEY_ID"]
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-        region: ENV["AWS_REGION"]
-      }
-      config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
-      config.storage = :fog
-      config.fog_public = false
-    else
-      config.storage = :file
-    end
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["AWS_REGION"]
+    }
+    config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
+    config.storage = :fog
+    config.fog_public = false
   end
 end
