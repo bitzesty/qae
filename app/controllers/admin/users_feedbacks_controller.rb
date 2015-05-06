12a03b4bdd3063097b029e97fafae87053df2bc3
Vasili Kachalko <amarant.st@gmail.com>
2015-05-06 21:30:55 +0300

    add ordering and change link

--- app/controllers/admin/users_feedbacks_controller.rb
+++ app/controllers/admin/users_feedbacks_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersFeedbacksController < Admin::BaseController
   def show
     authorize :users_feedback, :show?
-    @feedbacks = SiteFeedback.all.page(params[:page])
+    @feedbacks = SiteFeedback.all.order(created_at: :desc).page(params[:page])
   end
 end
