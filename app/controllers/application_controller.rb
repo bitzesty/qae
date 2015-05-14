078d6704a17570f67fdfd9ebc25607417591cc4c
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-05-14 15:50:35 +0300

    [Security Headers] code review fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  ensure_security_headers if ENV['ENSURE_SECURITY_HEADERS']
+  ensure_security_headers if ENV["ENSURE_SECURITY_HEADERS"]
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
