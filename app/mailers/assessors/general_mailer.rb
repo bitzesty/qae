2f74644637fea65fb056fc3e5e287fa845563f6c
Leszek <andrukanisleszek@gmail.com>
2015-05-14 17:17:46 +0200

    Fix copywriting for the withdrawn email

--- app/mailers/assessors/general_mailer.rb
+++ app/mailers/assessors/general_mailer.rb
@@ -2,7 +2,7 @@ class Assessors::GeneralMailer < ApplicationMailer
   def led_application_withdrawn(form_answer_id, assessor_id)
     @form_answer = FormAnswer.find(form_answer_id)
     @assessor = Assessor.find(assessor_id)
-    @subject = "TODO: copy"
+    @subject = "Application Ref: #{@form_answer.urn} Withdrawn"
     mail to: @assessor.email, subject: @subject
   end
 
