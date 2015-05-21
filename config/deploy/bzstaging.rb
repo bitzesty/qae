57dd89b5262eca99f2ced0098b9d1eb617e72d08
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-05-21 20:06:45 +0300

    [BZStaging] corrects

--- config/deploy/bzstaging.rb
+++ config/deploy/bzstaging.rb
@@ -2,4 +2,4 @@ set :default_env, { 'RAILS_ENV' => 'bzstaging' }
 set :rails_env, 'production'
 set :branch, ENV["BRANCH"] || 'master'
 
-server '<YOUR IP BRO>', user: 'qae', roles: %w{web app db}
+server '95.138.170.133', user: 'qae', roles: %w{web app db}
