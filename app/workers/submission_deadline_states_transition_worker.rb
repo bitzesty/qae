8fe355b352cd04156b0f71ec68ef25dfdc7ec580
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-05-21 14:52:58 +0300

    [DEV SERVER] AWS SQS QUEUE integration

--- app/workers/submission_deadline_states_transition_worker.rb
+++ app/workers/submission_deadline_states_transition_worker.rb
@@ -1,7 +1,7 @@
 class SubmissionDeadlineStatesTransitionWorker
   include Shoryuken::Worker
 
-  shoryuken_options queue: "#{Rails.env}_default", auto_delete: true
+  shoryuken_options queue: "#{ENV['AWS_SQS_QUEUE_ADVANCED_PREFIX']}#{Rails.env}_default", auto_delete: true
 
   def perform(_sqs_msg)
     FormAsnwerStateMachine.trigger_deadlines
