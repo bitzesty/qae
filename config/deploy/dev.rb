1c0516992afdc8e1ae40448cea766cd64e1ac48b
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-05-21 13:54:54 +0300

    [DEV SERVER] AWS SQS QUEUE integration

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,6 +2,6 @@ set :default_env, { 'RAILS_ENV' => 'production' }
 set :rails_env, 'production'
 set :branch, ENV["BRANCH"] || 'master'
 
-set :shoryuken_default_hooks,  false
+# set :shoryuken_default_hooks,  false
 
 server '95.138.174.112', user: 'qae', roles: %w{web app db}
