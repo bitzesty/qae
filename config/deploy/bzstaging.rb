bac6930750815f686906188c18b8d9c888a9be81
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-05-21 21:29:35 +0300

    [BZStaging] capistrano corrects

--- config/deploy/bzstaging.rb
+++ config/deploy/bzstaging.rb
@@ -2,4 +2,4 @@ set :default_env, { 'RAILS_ENV' => 'bzstaging' }
 set :rails_env, 'production'
 set :branch, ENV["BRANCH"] || 'master'
 
-server '95.138.170.133', user: 'qae', roles: %w{web app db}
+server '95.138.174.143', user: 'qae', roles: %w{web app db}
