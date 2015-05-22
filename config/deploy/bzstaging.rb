a371809b6b491a6fcd1dc98f0426f8c2daced6e1
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-05-22 12:12:39 +0300

    [Capistrano | BZStaging] corrected IP address

--- config/deploy/bzstaging.rb
+++ config/deploy/bzstaging.rb
@@ -2,4 +2,4 @@ set :default_env, { 'RAILS_ENV' => 'bzstaging' }
 set :rails_env, 'bzstaging'
 set :branch, ENV["BRANCH"] || 'master'
 
-server '95.138.174.143', user: 'qae', roles: %w{web app db}
+server '162.13.46.189', user: 'qae', roles: %w{web app db}
