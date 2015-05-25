5d28702e00b82265b566352424d75866986b2ede
Leszek <andrukanisleszek@gmail.com>
2015-05-25 16:40:12 +0200

    Extract useless method from FormAnswer model

--- app/models/form_answer.rb
+++ app/models/form_answer.rb
@@ -191,51 +191,6 @@ class FormAnswer < ActiveRecord::Base
     end
   end
 
-  class << self
-    # Used to convert JSON document to Hash
-    # We use it in spec/factories/form_answer_factory.rb
-    # and in Hstore to JSON db migration (MigrateFormAnswersDocumentToJson)
-    def parse_json_document(doc)
-      result = {}
-
-      doc.each do |k, v|
-        parsed_value = begin
-          JSON.parse(v)
-        rescue
-          v
-        end
-
-        parsed_value = if parsed_value.is_a?(Array)
-          parsed_value.map { |el| JSON.parse(el) }
-
-        elsif parsed_value.is_a?(Hash)
-          new_hash = {}
-
-          parsed_value.each do |key, value|
-            new_hash[key] = if value.is_a?(Hash)
-              value
-            else
-              begin
-                JSON.parse(value)
-              rescue
-                value
-              end
-            end
-          end
-
-          new_hash
-        else
-
-          parsed_value
-        end
-
-        result[k] = parsed_value
-      end
-
-      result
-    end
-  end
-
   private
 
   def nominee_full_name_from_document
