4a4019b31f7613aa1455de867ac7af06c987a4e0
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-06-22 14:06:54 +0300

    [BZSTAGING] corrected ASSET_HOST HTTP prefix

--- config/environments/bzstaging.rb
+++ config/environments/bzstaging.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: ENV["MAILER_HOST"] }
-  config.action_mailer.asset_host = "https://#{ENV["ASSET_HOST"]}"
+  config.action_mailer.asset_host = "http://#{ENV["ASSET_HOST"]}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
