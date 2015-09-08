f0b21962e6cd3d8fe47922e2679acfbd0f1abeb4
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-09-08 16:27:22 +0300

    [Assessment Flow] better to comment old spec instead of removing it

--- spec/features/assessor/assessment_flow_spec.rb
+++ spec/features/assessor/assessment_flow_spec.rb
@@ -19,7 +19,87 @@ describe "Assessment flow", %(
     p.save
   end
 
-  it "follows the appropriate assessment flow"
+  #  it "follows the appropriate assessment flow" do
+  #    login_as(lead, scope: :assessor)
+  #    visit assessor_form_answer_path(form_answer)
+  #    expect(page).to_not have_selector("#case-summary-heading")
+  #    login_as(primary, scope: :assessor)
+  #    visit assessor_form_answer_path(form_answer)
+  #    expect(page).to_not have_selector("#case-summary-heading")
+  #    expect(page).to_not have_selector("#appraisal-form-moderated-heading")
+  #    login_as(lead, scope: :assessor)
+  #    visit assessor_form_answer_path(form_answer)
+
+  #    find("#appraisal-form-moderated-heading .panel-title a").click
+  #    within "#section-appraisal-form-moderated" do
+  #      all(".btn-rag").each do |rag|
+  #        rag.click
+  #        find(".dropdown-menu .rag-positive").click
+  #      end
+
+  #      all(".form-edit-link").each(&:click)
+
+  #      all("textarea").each do |textarea|
+  #        textarea.set text
+  #      end
+
+  #      all(".form-save-link").each(&:click)
+  #      click_button "Submit appraisal"
+  #    end
+
+  #    visit assessor_form_answer_path(form_answer)
+  #    expect(page).to_not have_selector("#case-summary-heading")
+  #    login_as(primary, scope: :assessor)
+  #    visit assessor_form_answer_path(form_answer)
+
+  #    page.document.synchronize do
+  #      element = first("#case-summary-heading-primary_case_summary .panel-title a")
+  #      element.click if element
+  #    end
+
+  #    page.document.synchronize do
+  #      within "#section-case-summary-primary_case_summary" do
+  #        expect(page).to have_selector(".form-value p", text: text, count: 4)
+  #        expect(page).to have_selector(".rag-text", text: "Green", count: 3)
+  #        expect(page).to have_selector("input[value='Confirm case summary']")
+  #        first(".btn-rag").click
+  #        find(".dropdown-menu .rag-negative").click
+  #      end
+  #    end
+
+  #    wait_for_ajax
+  #    expect(Assessors::PrimaryCaseSummaryMailer).to receive(:notify).once.and_return(double(deliver_later!: true))
+
+  #    submit_primary_case_summary
+
+  #    visit assessor_form_answer_path(form_answer)
+  #    find("#case-summary-heading-primary_case_summary .panel-title a").click
+
+  #    expect(page).to_not have_selector("input[value='Confirm case summary']")
+  #    login_as(lead, scope: :assessor)
+  #    visit assessor_form_answer_path(form_answer)
+  #    find("#case-summary-heading-primary_case_summary .panel-title a").click
+
+  #    page.document.synchronize do
+  #      if first("#section-case-summary")
+
+  #        within "#section-case-summary" do
+  #          expect(page).to have_selector(".form-value p", text: text, count: 4)
+  #          expect(page).to have_selector(".rag-text", text: "Green", count: 2)
+  #          expect(page).to have_selector(".rag-text", text: "Red", count: 1)
+  #          expect(page).to have_selector("input[value='Confirm case summary']")
+  #        end
+  #      end
+  #    end
+  #  end
+  # end
+
+  # def submit_primary_case_summary
+  #  assessment = form_answer.assessor_assignments.primary_case_summary
+  #  service = AssessmentSubmissionService.new(assessment, primary)
+  #  service.perform
+  # end
+
   # Needs to test the flow properly
   # Primary assessor submits appraisal form
   # Secondary assessor submits appraisal form
