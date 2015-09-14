4c9305fbd62e0e011f2fc3168fe8d6eef317475c
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-09-14 13:29:53 +0300

    [Failed Rspecs] fixed spec/features/assessor/flags_on_form_answers_list_spec.rb

--- spec/features/assessor/flags_on_form_answers_list_spec.rb
+++ spec/features/assessor/flags_on_form_answers_list_spec.rb
@@ -3,14 +3,16 @@ include Warden::Test::Helpers
 
 describe "assessor sees the proper number of flags and sort by it" do
   let!(:assessor) { create(:assessor, :lead_for_all) }
-  let!(:form_answer) { create(:form_answer, assessor_importance_flag: true) }
+  let!(:form_answer) { create(:form_answer, assessor_importance_flag: true, state: 'assessment_in_progress') }
 
   before do
     login_as(assessor, scope: :assessor)
   end
 
   context "no comments" do
-    before { visit assessor_form_answers_path }
+    before {
+      visit assessor_form_answers_path
+    }
     it "sees only the global flag indicator" do
       within ".applications-table" do
         expect(first(".flag-count").text).to eq("1")
@@ -22,11 +24,14 @@ describe "assessor sees the proper number of flags and sort by it" do
     let!(:comment) do
       create(:flagged_comment, :assessor, commentable: form_answer, authorable: assessor)
     end
-    before { visit assessor_form_answers_path }
+    before {
+      visit assessor_form_answers_path
+    }
 
     it "sees sum of comment flags and global flag" do
       within ".applications-table" do
-        expect(first(".flag-count").text).to eq("2")
+        expect(first(".comment-count").text).to eq("1")
+        expect(first(".flag-count").text).to eq("1")
       end
     end
   end
