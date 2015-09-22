2fafa8c27afb472706531a8943760efb63776e8a
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-09-22 18:42:52 +0300

    [Duplicated emails] fix

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,7 +1,7 @@
 class AccountMailer < ApplicationMailer
   default from: ENV["MAILER_FROM"] || "no-reply@queens-awards-enterprise.service.gov.uk",
           reply_to: "info@queensawards.org.uk",
-          bcc: proc { form_answer.account.collaborators_without(form_answer.user).map(&:email) }
+          bcc: proc { form_answer.account.collaborators_without(form_answer.user).confirmed.map(&:email) }
   layout "mailer"
 
   attr_reader :form_answer
