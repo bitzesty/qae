fe1f01885fa72a18577913b6b253fed56501d1a4
Michael Ly <michael.ly@bitzesty.com>
2015-09-22 17:53:04 +0100

    Contain admin application fields in a grey box

--- app/helpers/appraisal_form_helper.rb
+++ app/helpers/appraisal_form_helper.rb
@@ -1,10 +1,13 @@
 module AppraisalFormHelper
   def render_section(form_answer, f)
+    section_odd_even = false
     AppraisalForm.struct(form_answer, f).map do |k, section|
       section_obj = OpenStruct.new(section.merge(desc: "#{k}_desc", rate: "#{k}_rate"))
       partial = "admin/form_answers/appraisal_form_components/#{section[:type]}_section"
 
-      render partial: partial, locals: { section: section_obj, f: f }
+      section_odd_even = !section_odd_even
+
+      render partial: partial, locals: { section: section_obj, f: f, section_odd_even: section_odd_even }
     end.join.html_safe
   end
 end
