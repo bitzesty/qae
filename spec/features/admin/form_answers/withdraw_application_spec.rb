8d813fcf61efe5b04470e12fb9e5fa800875f6b0
Michael Ly <michael.ly@bitzesty.com>
2015-09-25 14:51:12 +0100

    SPEC assessor cannot withdraw an application, admin can

--- spec/features/admin/form_answers/withdraw_application_spec.rb
+++ spec/features/admin/form_answers/withdraw_application_spec.rb
@@ -1,20 +1,20 @@
 require "rails_helper"
 include Warden::Test::Helpers
 
-describe "Assessor withdraws the application", js: true do
-  let!(:assessor) { create(:assessor, :lead_for_all) }
+describe "Admin withdraws the application", js: true do
+  let!(:admin) { create(:admin) }
   let!(:form_answer) { create(:form_answer) }
 
   before do
     setting = Settings.current_submission_deadline
     setting.update(trigger_at: DateTime.now - 1.hour)
-    login_as(assessor, scope: :assessor)
+    login_admin(admin)
     form_answer.state_machine.perform_transition(:assessment_in_progress, nil, false)
   end
 
   describe "application withdrawn" do
     it "withdraws the app with the select box" do
-      visit assessor_form_answer_path(form_answer)
+      visit admin_form_answer_path(form_answer)
       within ".section-applicant-status" do
         find(".state-toggle").click
       end
