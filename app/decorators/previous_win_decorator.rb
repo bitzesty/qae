5bd3d4ba3c084d903d1b60f0e85c0311d29a9c53
Emiliano Ritiro <eritiro@gmail.com>
2015-09-29 20:35:22 -0300

    previous wins modified to use document attributes

--- app/decorators/previous_win_decorator.rb
+++
@@ -1,9 +0,0 @@
-class PreviousWinDecorator < ApplicationDecorator
-  def category_name
-    PreviousWin::CATEGORIES.invert[object.category]
-  end
-
-  def description
-    "#{category_name} - #{object.year}".strip
-  end
-end
