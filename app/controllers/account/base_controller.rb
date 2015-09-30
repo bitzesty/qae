298cdbeb1906e579cd81fc1960b2ad7ae405d3bf
Matthew Ford <matt@bitzesty.com>
2015-09-30 14:46:00 +0100

    clean up duplicate code

--- app/controllers/account/base_controller.rb
+++ app/controllers/account/base_controller.rb
@@ -4,13 +4,4 @@ class Account::BaseController < ApplicationController
   before_action :restrict_access_if_admin_in_read_only_mode!, only: [
     :new, :create, :update, :destroy
   ]
-
-  private
-
-  def require_to_be_account_admin!
-    unless current_user.account_admin?
-      redirect_to root_path,
-                  notice: "Access denied!"
-    end
-  end
 end
