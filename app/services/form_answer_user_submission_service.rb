96b9906a57dd348480076f3d5f6959b13117dcbd
Emiliano Ritiro <eritiro@gmail.com>
2015-09-30 09:29:53 -0300

    deleted service logic no longer in use. deleted service test that the service logic. deleted factory no longer used

--- app/services/form_answer_user_submission_service.rb
+++ app/services/form_answer_user_submission_service.rb
@@ -9,20 +9,6 @@ class FormAnswerUserSubmissionService
   end
 
   def perform
-    assign_previous_wins
     Notifiers::Submission::SuccessNotifier.new(form_answer).run
-  end
-
-  private
-
-  def assign_previous_wins
-    return unless form_answer.previous_wins.blank?
-
-    award_holder_details = form_answer.document["queen_award_holder_details"]
-    if award_holder_details.present?
-      award_holder_details.each do |win|
-        form_answer.previous_wins.create(category: win["category"], year: win["year"] )
-      end
-    end
   end
 end
