d953b4dfea9e0391c09275c2295bdb02768a2a84
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-11-11 12:16:43 +0300

    [Forms Stuff Refactoring] from lib to app/forms

--- app/forms/qae_2014_forms.rb
+++ app/forms/qae_2014_forms.rb
@@ -1,4 +1,3 @@
-require 'qae_form_builder'
 require 'qae_2014_forms/innovation'
 require 'qae_2014_forms/international_trade'
 require 'qae_2014_forms/sustainable_development'
--- app/forms/qae_form_builder/qae_form.rb
+++ app/forms/qae_form_builder/qae_form.rb
@@ -1,31 +1,5 @@
 class QAEFormBuilder
 
-  class QAEFormDecorator < QAEDecorator
-
-    def form_name
-      @decorator_options[:form_name] || 'form'
-    end
-
-    def progress
-      required_visible_questions_filled.to_f / required_visible_questions_total
-    end
-
-    def required_visible_questions_filled
-      count_questions :required_visible_questions_filled
-    end
-
-    def required_visible_questions_total
-      count_questions :required_visible_questions_total
-    end
-
-    private
-
-    def count_questions meth
-      steps.map{|step| step.send meth}.reduce(:+)
-    end
-
-  end
-
   class QAEForm
     attr_reader :title, :opts, :steps, :questions_by_key
 
@@ -53,6 +27,5 @@ class QAEFormBuilder
       @steps << step
       step
     end
-
   end
 end
