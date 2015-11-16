96fd06800eb6dc772ca8b3e19f18694355908acd
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-11-16 13:50:24 +0300

    [Admin Users] unlock user's feature (Rspec coverage)

--- spec/acceptance/steps/admin_user_management_steps.rb
+++ spec/acceptance/steps/admin_user_management_steps.rb
@@ -46,6 +46,11 @@ step "a not confirmed user exists" do
   @not_confirmed_user = FactoryGirl.create(:user, confirmed_at: nil, password: "my98ssdkjv9823kds=2")
 end
 
+step "a locked user exists" do
+  @locked_user = FactoryGirl.create(:user, password: "my98ssdkjv9823kds=2")
+  @locked_user.lock_access!
+end
+
 step "I am on confirmed user page" do
   visit "/admin/users/#{@user.id}/edit"
 end
@@ -58,14 +63,34 @@ step "I am on not confirmed user page" do
   visit "/admin/users/#{@not_confirmed_user.id}/edit"
 end
 
+step "I am on locked user page" do
+  visit "/admin/users/#{@locked_user.id}/edit"
+end
+
 step "I see resend confirmation link" do
   expect(page).to have_link('Resend confirmation email')
+end
+
+step "I see unlock user link" do
+  expect(page).to have_link('Unlock')
 end
 
 step "I do resend confirmation instructions" do
   click_link 'Resend confirmation email'
 end
 
+step "I do unlock of user" do
+  click_link 'Unlock'
+end
+
 step "I should see flash message about confirmation email sending" do
   expect_to_see "Confirmation instructions were successfully sent to #{@not_confirmed_user.decorate.full_name} (#{@not_confirmed_user.email})"
+end
+
+step "I should see flash message about unlocked access for user" do
+  expect_to_see "User #{@locked_user.decorate.full_name} (#{@locked_user.email}) successfully unlocked!"
+end
+
+step "user should be unlocked" do
+  expect(@locked_user.reload.access_locked?).to be_falsey
 end
