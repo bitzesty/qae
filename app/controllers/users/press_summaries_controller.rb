956a4ef063ee9f0252b78da5178654fe1cf37793
Mario Andrés Correa <a@macool.me>
2016-02-22 12:03:37 -0500

    copytext / ui updates
    includes points 5, 6, 7 & 8 from provided document

--- app/controllers/users/press_summaries_controller.rb
+++ app/controllers/users/press_summaries_controller.rb
@@ -11,6 +11,17 @@ class Users::PressSummariesController < Users::BaseController
   expose(:award) do
     form_answer.decorate
   end
+  expose(:employees_figure) do
+    row = FormFinancialPointer.new(award, {
+      exclude_ignored_questions: true,
+      financial_summary_view: true
+    }).data.detect { |r| r[:employees].present? }
+    if row.present?
+      row[:employees].reverse.detect do |data|
+        data[:value].present? && data[:value] != "-"
+      end
+    end
+  end
 
   def update
     @press_summary.reviewed_by_user = true
@@ -37,7 +48,7 @@ class Users::PressSummariesController < Users::BaseController
 
   def press_summary_params
     params.require(:press_summary).permit(
-      :comment, :correct, :phone_number, :first_name, :email
+      :comment, :correct, :phone_number, :name, :email
     )
   end
 
