7dfbb5c7587d5eff2ea80de961ec5fcca25a1a47
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-02-24 17:56:32 +0300

    [Winners Email] For winners email, it says "winners at noon on <date>". should change to "winners at <date time>"
    [Trello Story](https://trello.com/c/enjh8HLa/635-qae-support-change-successful-business-winners-email-copy-currently-titled-in-settings-as-winners-email-notifying-all-winning-ap)

--- app/mailers/users/buckingham_palace_invite_mailer.rb
+++ app/mailers/users/buckingham_palace_invite_mailer.rb
@@ -7,7 +7,7 @@ class Users::BuckinghamPalaceInviteMailer < ApplicationMailer
     @deadline = Settings.current.deadlines.where(kind: "buckingham_palace_attendees_details").first
     @deadline = @deadline.trigger_at
     @media_deadline = Settings.current.deadlines.where(kind: "buckingham_palace_media_information").first
-    @media_deadline = @media_deadline.try :strftime, "%A %d %B %Y"
+    @media_deadline = @media_deadline.try :strftime, "%H.%M on %A %d %B %Y"
     @book_notes_deadline = Settings.current.deadlines.where(kind: "buckingham_palace_confirm_press_book_notes").first
     @book_notes_deadline = @book_notes_deadline.try :strftime, "%l %p on %A %d %B"
     @attendees_invite_date = Settings.current.deadlines.where(kind: "buckingham_palace_attendees_invite").first
