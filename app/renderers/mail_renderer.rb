a3be6d697ad4877b2ad6b02730fd98d02cc00c50
Emiliano <eritiro@gmail.com>
2016-02-24 09:05:27 -0300

    unsuccessful ep email fixes: removed full stop after heading; modified email example to show a EP urn

--- app/renderers/mail_renderer.rb
+++ app/renderers/mail_renderer.rb
@@ -25,7 +25,7 @@ class MailRenderer
     assigns[:user] = dummy_user("Jon", "Doe", "Jane's Company")
     assigns[:form_answer] = form_answer
     assigns[:nominee_name] = "Nominee Name"
-
+    form_answer.urn = "QA0128/16EP"
     render(assigns, "users/unsuccessful_feedback_mailer/ep_notify")
   end
 
