94c711d0c2607124ea5ed087c06758cad6b44661
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-02-25 13:26:55 +0300

    [Mailers] fixes
    * Winner Mailer - should be AccountMailer (so that collaborators and account owner would be notified too)
    * Unsuccessfull Mailer - we had to use [title last_name] in 'Dear' block
    
    [Trello Story](https://trello.com/c/rmNDkLs6/660-qae-support-email-to-unsuccessful-business-categories-registered-account-holder)

--- app/services/notifiers/email_notification_service.rb
+++ app/services/notifiers/email_notification_service.rb
@@ -71,8 +71,7 @@ class Notifiers::EmailNotificationService
   def winners_notification(award_year)
     award_year.form_answers.winners.each do |form_answer|
       document = form_answer.document
-      account_holder = form_answer.account.owner
-      email = form_answer.promotion? ? document["nominee_email"] : account_holder.email
+      email = form_answer.promotion? ? document["nominee_email"] : form_answer.user.email
 
       shoryuken_ops = {
         email: email,
