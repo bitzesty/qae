c01324e6c4350ba5824b8963c9769eb7682811c1
Emiliano <eritiro@gmail.com>
2016-03-08 14:33:39 -0300

    fix shoryuken process

--- app/services/notifiers/winners/buckingham_palace_invite.rb
+++ app/services/notifiers/winners/buckingham_palace_invite.rb
@@ -4,8 +4,8 @@ class Notifiers::Winners::BuckinghamPalaceInvite
   shoryuken_options queue: "#{Rails.env}_default", auto_delete: true
 
   def perform(sqs_msg, ops={})
-    email = ops[:email]
-    form_answer_id = ops[:form_answer_id]
+    email = ops["email"]
+    form_answer_id = ops["form_answer_id"]
 
     invite = PalaceInvite.where(email: email, form_answer_id: form_answer_id).first_or_create
     Users::BuckinghamPalaceInviteMailer.invite(invite.id).deliver_later!
