d44e0405158c9567f702df4a85b0cb2271f66b04
Mario Andrés Correa <a@macool.me>
2016-03-14 11:03:01 -0500

    remove capistrano configs
    we’ll be using convox for deployment

--- config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-set :default_env, { 'RAILS_ENV' => 'production' }
-set :rails_env, 'production'
-set :branch, ENV["BRANCH"] || 'production'
-
-# HARDCODED FOR NOW
-server '52.18.126.123', user: 'qae', roles: %w{web app db}
-server '52.19.230.94', user: 'qae', roles: %w{web app db}
