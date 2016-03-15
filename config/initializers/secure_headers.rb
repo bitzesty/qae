3830dac4b7fddb6d3f729d08348e24ebfbfbad02
Matthew Ford <matt@bitzesty.com>
2016-03-15 17:52:36 +0000

    Note to update to v3

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,4 +1,5 @@
 # See https://github.com/twitter/secureheaders#options-for-ensure_security_headers
+# TODO Update to x3
 ::SecureHeaders::Configuration.configure do |config|
   config.hsts = { max_age: 20.years.to_i, include_subdomains: true }
   config.x_frame_options = "SAMEORIGIN"
