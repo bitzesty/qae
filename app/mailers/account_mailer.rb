706b245ac385c9248b17d904bc462e6ea2a2efc9
Matthew Ford <matt@bitzesty.com>
2016-03-17 14:33:21 +0000

    Update support email address
    https://trello.com/c/PxjhVGYB/308-qae-support-update-support-email-address

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,6 +1,6 @@
 class AccountMailer < ApplicationMailer
   default from: ENV["MAILER_FROM"] || "no-reply@queens-awards-enterprise.service.gov.uk",
-          reply_to: "info@queensawards.org.uk",
+          reply_to: "queensawards@bis.gsi.gov.uk",
           bcc: proc { form_answer.account.collaborators_without(form_answer.user).confirmed.map(&:email) }
   layout "mailer"
 
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV["MAILER_FROM"] || "no-reply@queens-awards-enterprise.service.gov.uk",
-          reply_to: "info@queensawards.org.uk"
+          reply_to: "queensawards@bis.gsi.gov.uk"
   layout "mailer"
 end
