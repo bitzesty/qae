0cfa30bba87ee025b61198dbc79973627a91d1b1
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-03-23 13:40:05 +0300

    [Feedbacks] replace 'approved' with 'submitted'
    [Trello Story](https://trello.com/c/TZn8wVCJ/292-qae-support-email-to-unsuccessful-business-categories-registered-account-holder)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314141838) do
+ActiveRecord::Schema.define(version: 20160323103504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,7 +194,6 @@ ActiveRecord::Schema.define(version: 20160314141838) do
   create_table "feedbacks", force: :cascade do |t|
     t.integer  "form_answer_id"
     t.boolean  "submitted",       default: false
-    t.boolean  "approved",        default: false
     t.hstore   "document"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
