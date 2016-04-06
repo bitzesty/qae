35c899372eb18d24fc57304dd68e011ebfd56077
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-04-06 21:00:55 +0300

    [Rspec] fixed spec/models/settings_spec.rb

--- spec/models/settings_spec.rb
+++ spec/models/settings_spec.rb
@@ -4,7 +4,7 @@ describe Settings do
   context "after create" do
     let(:settings) { Settings.current }
     it "creates all kinds of deadlines" do
-      expect(settings.deadlines.count).to eq(7)
+      expect(settings.deadlines.count).to eq(8)
       expected = %w(
         audit_certificates
         buckingham_palace_attendees_details
