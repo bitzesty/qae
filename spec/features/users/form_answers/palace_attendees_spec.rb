da291d188d7aa4f3075ad13c911ac7aa00864a19
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-04-14 19:37:27 +0300

    [Palace Invite] Rspec fixes
    [Trello Story](https://trello.com/c/HVheU1Oq/305-qae-support-palace-invites)

--- spec/features/users/form_answers/palace_attendees_spec.rb
+++ spec/features/users/form_answers/palace_attendees_spec.rb
@@ -15,6 +15,19 @@ So that I provide a full list of attendees for Buckingham Palace reception
     create :form_answer, :innovation, :awarded, user: user
   end
 
+  let!(:award_year) do
+    create(:award_year)
+  end
+
+  let!(:year_settings) do
+    settings = award_year.settings
+
+    invite = settings.deadlines.where(kind: "buckingham_palace_attendees_invite").first
+    invite.update_column(:trigger_at, DateTime.new(Date.current.year, 7, 14, 18, 00))
+
+    settings.reload
+  end
+
   let!(:palace_invite) do
     create :palace_invite, form_answer: form_answer, email: user.email
   end
