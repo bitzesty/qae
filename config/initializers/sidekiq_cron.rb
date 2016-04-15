b3c38621d1d4a000c89c0848e08a6a73e6cf242e
Matthew Ford <matt@bitzesty.com>
2016-04-15 02:04:15 +0100

    production jobs should be merged into the default ones

--- config/initializers/sidekiq_cron.rb
+++ config/initializers/sidekiq_cron.rb
@@ -11,9 +11,10 @@ production_schedule = {
     "class" => "Scheduled::PerformancePlatformServiceWorker"
   }
 }
+
 unless ENV["ONLY_ASSETS"]
   if ENV["SCHEDULE_PRODUCTION_JOBS"].present?
-    Sidekiq::Cron::Job.load_from_hash(production_schedule)
+    Sidekiq::Cron::Job.load_from_hash(default_schedule.merge(production_schedule))
   else
     Sidekiq::Cron::Job.load_from_hash(default_schedule)
   end
