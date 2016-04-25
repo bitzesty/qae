96dda3dc70096d79b187094032279c50190b72dd
Mario Andrés Correa <a@macool.me>
2016-04-25 16:04:52 -0500

    users can upload a max of 3 attachments
    (section E)
    
    also revise questions copytext

--- app/forms/qae_2014_forms/sustainable_development/sustainable_development_step5.rb
+++ app/forms/qae_2014_forms/sustainable_development/sustainable_development_step5.rb
@@ -2,10 +2,12 @@ class QAE2014Forms
   class << self
     def development_step5
       @development_step5 ||= proc do
-        upload :innovation_materials, "If there is additional material you feel would help us to assess your entry then you can add up to 4 files or website addresses here." do
+        upload :innovation_materials, "If there is additional material you feel would help us to assess your entry then you can add up to 3 files or website addresses here." do
           ref "E 1"
           context %(
-            <p>We can't guarantee these will be reviewed, so include any vital information within the form.</p>
+            <p>
+              We can't guarantee these will be reviewed, so include any vital information within the form. It is also essential to reference these in your application to ensure the assessors are aware of them and can relate this information to the relevant questions.
+            </p>
             <p>You can upload files in all common formats, as long as they're less than 5mb each.</p>
             <p>You may link to videos, websites or other media you feel relevant.</p>
             <p>We won't consider business plans, annual accounts or company policy documents. Additional materials should not be used as a substitute for completing sections of the form.</p>
@@ -18,7 +20,7 @@ class QAE2014Forms
               chm, csv, diff, doc, docx, dot, dxf, eps, gif, gml, ics, jpg, kml, odp, ods, odt, pdf, png, ppt, pptx, ps, rdf, rtf, sch, txt, wsdl, xls, xlsm, xlsx, xlt, xml, xsd, xslt, zip
             </p>
           )
-          max_attachments 4
+          max_attachments 3
           links
           description
         end
