771a1bf98407af951bb0fde4325549c8da5024fd
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-04-29 12:43:18 +0300

    [Eligibility] Innovation & Sust. Dev. Eligibility questions
    [Trello Story](https://trello.com/c/pkI13U5v/880-qae16imp-innovation-sust-dev-eligibility-questions)

--- app/models/eligibility/innovation.rb
+++ app/models/eligibility/innovation.rb
@@ -6,20 +6,21 @@ class Eligibility::Innovation < Eligibility
   property :innovative_product,
             values: %w[yes no skip],
             acts_like_boolean: true,
-            label: "Do you have an innovative product/service/initiative?",
+            label: "Do you have one or more innovative product, service or business model?",
             accept: :true,
-            hint: 'A product/service/initiative is innovative if it has not been done before, and provides benefit/ solves prior problems for its users. Fields include invention, design and production (for goods), performance (for services), marketing, distribution, after-sale support.'
+            hint: 'A product, service or business model is innovative if it has not been done before, and provides benefit or solves prior problems for its users.'
 
   property :number_of_innovative_products,
             positive_integer: true,
-            label: "How many innovative products/services/initiatives do you have?",
+            label: "How many innovative products, services or business models do you have?",
             accept: :not_nil,
             if: proc { !skipped? && (innovative_product.nil? || innovative_product?) }
 
   property :was_on_market_for_two_years,
             boolean: true,
-            label: "Has it been on the market for at least two years?",
+            label: "Has the innovation been on the market for at least two years?",
             accept: :true,
+            hint: "If you have more than one innovation, please decide which one you will be applying for first and answer this and subsequent questions with that innovation in mind.",
             if: proc { !skipped? }
 
   property :had_impact_on_commercial_performace_over_two_years,
