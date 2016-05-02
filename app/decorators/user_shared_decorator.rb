acdaff0bd67306cedfcb60f6f1f6dfe9cb7ea928
Hildebrando Rueda <harq.soft@gmail.com>
2016-05-02 17:00:42 -0500

    Update UserSharedDecorator module

--- app/decorators/user_shared_decorator.rb
+++ app/decorators/user_shared_decorator.rb
@@ -8,10 +8,10 @@ module UserSharedDecorator
   end
 
   def formatted_last_sign_in_at_long
-    object.last_sign_in_at && object.last_sign_in_at.strftime("%e %b %Y at %H:%M")
+    object.last_sign_in_at && object.last_sign_in_at.strftime("%e %b %Y at %-l:%M%P")
   end
 
   def formatted_last_sign_in_at_short
-    object.last_sign_in_at && object.last_sign_in_at.strftime("%d/%m/%Y %H:%M")
+    object.last_sign_in_at && object.last_sign_in_at.strftime("%d/%m/%Y %-l:%M%P")
   end
 end
