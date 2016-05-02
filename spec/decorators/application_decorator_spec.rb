09840ac9c0972d8718d08eebdd95c6550626d8e2
Hildebrando Rueda <harq.soft@gmail.com>
2016-05-02 15:15:09 -0500

    Update ApplicationDecorator timestamp
    Add specs too

---
+++ spec/decorators/application_decorator_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe ApplicationDecorator do
+
+  let(:date) { DateTime.new(2015,2,6,8,30) }
+
+  describe "#created_at" do
+    it "Returns the expected format" do
+      user = build_stubbed(:user, created_at: date).decorate
+      expect(user.created_at).to eq("Feb_06_2015")
+    end
+  end
+
+  describe "#created_at_timestamp" do
+    it "Returns the expected format" do
+      user = build_stubbed(:user, created_at: date).decorate
+      expect(user.created_at_timestamp).to eq("06022015_8:30am")
+    end
+  end
+end
