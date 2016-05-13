68976bdb364a53ebb806df84b8ad2e466773fc90
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-05-13 11:26:41 +0300

    [Code Climate] disabled 'Use def with parentheses when there are parameters.' check
    Check example:
    
    ```
    def some_method()
     # body omitted
    end
    
    def some_method
     # body omitted
    end
    
    def some_method_with_parameters param1, param2
     # body omitted
    end
    
    def some_method_with_parameters(param1, param2)
     # body omitted
    end
    ```
    
    Basicly is fine, but I guess that code like:
    
    ```
    def submit text, &block
      ...
    end
    ```
    doesn't look bad!
    
    So that disabling it!

--- app/forms/qae_form_builder/step.rb
+++ app/forms/qae_form_builder/step.rb
@@ -133,7 +133,7 @@ class QAEFormBuilder
       klass_builder = QAEFormBuilder.const_get( "#{meth.to_s.camelize}QuestionBuilder" ) rescue nil
       klass = QAEFormBuilder.const_get( "#{meth.to_s.camelize}Question" ) rescue nil
 
-      if klass_builder && klass && args.length >= 2 && args.length <=3
+      if klass_builder && klass && args.length >= 2 && args.length <= 3
         id, title, opts = args
         create_question klass_builder, klass, id, title, opts, &block
       else
@@ -177,7 +177,6 @@ class QAEFormBuilder
   end
 
   class Step
-
     attr_accessor :title, :short_title, :opts, :questions, :form, :context, :submit
 
     def initialize form, title, short_title, opts={}
