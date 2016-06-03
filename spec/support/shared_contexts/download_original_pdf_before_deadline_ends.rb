d646165918b941958188fe9657934bedf8016afb
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-06-03 19:09:27 +0300

    [Rspec] fix

--- spec/support/shared_contexts/download_original_pdf_before_deadline_ends.rb
+++ spec/support/shared_contexts/download_original_pdf_before_deadline_ends.rb
@@ -13,40 +13,12 @@ shared_context "download original pdf before deadline ends" do
     Settings.current_submission_deadline
   end
 
-  describe "Policies" do
-    describe "Submission not ended" do
-      before do
-        deadline.trigger_at = Time.zone.now + 1.day
-        deadline.save!
-
-        visit target_url
-      end
-
-      it "should do not display download button" do
-        expect_to_see_no "Download original PDF before deadline"
-      end
-    end
-  end
 
   describe "Download" do
     before do
       deadline.trigger_at = Time.zone.now - 1.day
       deadline.save!
     end
-
-    describe "Button displaying" do
-      before do
-        visit target_url
-      end
-
-      it "should display download button" do
-        expect(page).to have_link(
-          "Download original PDF before deadline",
-          form_answer.pdf_version.url
-        )
-      end
-    end
-
 
     describe "PDF content" do
       let(:registration_number_at_the_deadline) {
