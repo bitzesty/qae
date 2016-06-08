2bf14ff925440aa2e3bd7d9c012c629eccb0e6bb
Vasili Kachalko <amarant.st@gmail.com>
2016-06-08 19:52:22 +0300

    eligiblity copy for social mobility awards

--- app/models/eligibility/mobility.rb
+++ app/models/eligibility/mobility.rb
@@ -1,9 +1,19 @@
 class Eligibility::Mobility < Eligibility
   AWARD_NAME = 'Social Mobility'
 
-  property :sustainable_development,
-            values: %w[yes no skip],
-            acts_like_boolean: true,
-            label: "Have you had a substantial sustainable development initiative for at least the last two years?",
+  property :programme_validation,
+            boolean: true,
+            label: "Do you have one or more of the following:",
+            accept: :true
+
+  property :programme_operation,
+            boolean: true,
+            label: "Has the programme(s) been operational for at least the last two years?",
+            accept: :true
+
+  property :programme_commercial_success,
+            boolean: true,
+            label: "Has the programme(s) had a positive impact on your commercial success?",
+            hint: "Commercial success means financial success in terms of savings or growth and might also include non-financial factors, e.g. reputation, employee relations.",
             accept: :true
 end
