a40caa9e4a7399b0c2cb60d637e38f8d64425637
Vasili Kachalko <amarant.st@gmail.com>
2016-06-14 16:05:32 +0300

    fix regions question validation

--- app/forms/qae_form_builder/regions_question.rb
+++ app/forms/qae_form_builder/regions_question.rb
@@ -1,5 +1,18 @@
 class QAEFormBuilder
   class RegionsQuestionValidator < DropdownQuestionValidator
+    def errors
+      result = super
+
+      if question.required?
+        if question.input_value.empty?
+          result[question.hash_key] = "Can't be blank."
+        elsif question.input_value.any? { |v| question.regions.exclude?(v) }
+          result[question.hash_key] = "Is not a valid region."
+        end
+      end
+
+      result
+    end
   end
 
   class RegionsQuestionBuilder < DropdownQuestionBuilder
