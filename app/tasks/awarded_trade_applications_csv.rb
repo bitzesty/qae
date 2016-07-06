ad8cf03edf874e842597ac365167abb1b469b486
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-07-06 13:47:10 +0300

    [Awarded Trade Applications] prepare CSV
    [Trello Story](https://trello.com/c/AUolELnn/448-qae16imp-how-many-award-winners-were-dependant-on-eu-trade)

---
+++ app/tasks/awarded_trade_applications_csv.rb
@@ -0,0 +1,29 @@
+# Run:
+# AwardedTradeApplicationsCsv.run
+
+class AwardedTradeApplicationsCsv
+  CSV_HEADERS = [
+    "Application number",
+    "B4 Describe the geographical spread of your overseas markets.",
+    "B4 What percentage of total overseas sales was made to each of your top 5 overseas markets (ie. individual countries) during the final year of your entry?"
+  ]
+
+  class << self
+    def run
+      ::CSV.generate(encoding: "UTF-8", force_quotes: true) do |csv_out|
+        csv_out << CSV_HEADERS
+
+        AwardYear.first
+                 .form_answers
+                 .for_award_type(:trade)
+                 .where(state: "awarded").each do |item|
+          csv_out << [
+            item.urn,
+            item.document["markets_geo_spread"],
+            item.document["top_overseas_sales"]
+          ]
+        end
+      end
+    end
+  end
+end
