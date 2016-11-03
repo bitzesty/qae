6b6c23eb4afa4aaf50026177137d3dd341671c03
Ruslan Khamidullin <rusllonrails@gmail.com>
2016-11-03 13:37:36 +0300

    [Shortlisted Mailer] fixed time format
    [TRELLO STORY](https://trello.com/c/ux2Nxd7I/1364-qae16imp-update-of-copy-shortlisted-reminder-to-submit-verification-of-commercial-figures-for-shortlisted-applicants-who-have-no)

--- app/mailers/users/audit_certificate_request_mailer.rb
+++ app/mailers/users/audit_certificate_request_mailer.rb
@@ -3,9 +3,24 @@ class Users::AuditCertificateRequestMailer < ApplicationMailer
     @form_answer = FormAnswer.find(form_answer_id).decorate
     @recipient = User.find(user_id).decorate
     @deadline = Settings.current.deadlines.where(kind: "audit_certificates").first
-    @deadline = @deadline.trigger_at.strftime("%d/%m/%Y")
+    @trigger_at = @deadline.trigger_at
+    @deadline = @trigger_at.strftime("%d/%m/%Y")
+
+    @deadline_time = @trigger_at.strftime("%-l:%M%P")
+    @deadline_time = "midnight" if midnight?
+    @deadline_time = "midday" if midday?
 
     @subject = "Queen's Awards for Enterprise: Reminder to submit your Verification of Commercial Figures"
     mail to: @recipient.email, subject: @subject
+  end
+
+  private
+
+  def midday?
+    @trigger_at == @trigger_at.midday
+  end
+
+  def midnight?
+    @trigger_at == @trigger_at.midnight
   end
 end
