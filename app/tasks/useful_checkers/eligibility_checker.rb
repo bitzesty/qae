dbb4d11c40f01b75f68db57d5be1a0f04a96966e
Ruslan Khamidullin <rusllonrails@gmail.com>
2017-02-23 12:10:16 +0300

    [USEFUL CHECKERS] added Eligibility checker
    [TRELLO STORY](https://trello.com/c/NyXNr3xJ/1543-qae16imp-red-arch-manufacturing-ltd-should-not-have-been-eligible-find-out-how-they-got-past-eligibility)

--- app/tasks/useful_checkers/eligibility_checker.rb
+++ app/tasks/useful_checkers/eligibility_checker.rb
@@ -19,7 +19,7 @@ module UsefulCheckers
       ActiveRecord::Base.logger.level = Logger::INFO
 
       self.year = AwardYear.current
-      self.all_apps = year.form_answers.where("state NOT IN (?)", ["eligibility_in_progress", "not_eligible"])
+      self.all_apps = year.form_answers.where("state NOT IN (?)", ["eligibility_in_progress", "not_eligible", "not_submitted"])
 
       self.trade_apps = all_apps.where(award_type: "trade")
       self.innovation_apps = all_apps.where(award_type: "innovation")
@@ -31,53 +31,89 @@ module UsefulCheckers
       puts ""
 
       self.wrong_trade_apps = trade_apps.select do |app|
-        !app.eligible?
+        e = app.eligibility
+        e.force_validate_now = true
+
+        !e.valid?
       end
 
-      puts "[TRADE]                    #{wrong_trade_apps.count}"
+      puts "[TRADE] #{wrong_trade_apps.count} entries"
       puts ""
 
       wrong_trade_apps.map do |app|
-        puts "   [#{app.id} | #{app.urn}] STATE: #{app.state}, SUBMITTED: #{app.submitted?}"
+        details(app)
       end
 
       puts ""
 
       self.wrong_innovation_apps = innovation_apps.select do |app|
-        !app.eligible?
+        e = app.eligibility
+        e.force_validate_now = true
+
+        !e.valid?
       end
 
-      puts "[INNOVATION]                    #{wrong_innovation_apps.count}"
+      puts "[INNOVATION] #{wrong_innovation_apps.count} entries"
       puts ""
 
       wrong_innovation_apps.map do |app|
-        puts "   [#{app.id} | #{app.urn}] STATE: #{app.state}, SUBMITTED: #{app.submitted?}"
+        details(app)
       end
 
       puts ""
 
       self.wrong_development_apps = development_apps.select do |app|
-        !app.eligible?
+        e = app.eligibility
+        e.force_validate_now = true
+
+        !e.valid?
       end
 
-      puts "[DEVELOPMENT]                    #{wrong_development_apps.count}"
+      puts "[DEVELOPMENT] #{wrong_development_apps.count} entries"
       puts ""
 
       wrong_development_apps.map do |app|
-        puts "   [#{app.id} | #{app.urn}] STATE: #{app.state}, SUBMITTED: #{app.submitted?}"
+        details(app)
       end
       puts ""
 
       self.wrong_mobility_apps = mobility_apps.select do |app|
-        !app.eligible?
+        e = app.eligibility
+        e.force_validate_now = true
+
+        !e.valid?
       end
 
-      puts "[MOBILITY]                    #{wrong_mobility_apps.count}"
+      puts "[MOBILITY] #{wrong_mobility_apps.count} entries"
       puts ""
 
       wrong_mobility_apps.map do |app|
-        puts "   [#{app.id} | #{app.urn}] STATE: #{app.state}, SUBMITTED: #{app.submitted?}"
+        details(app)
       end
+      puts ""
+    end
+
+    def details(app)
+      b = app.form_basic_eligibility
+      e = app.eligibility
+      e.force_validate_now = true
+      e.valid?
+
+      puts "   [#{app.id} | #{app.award_type}] STATE: #{app.state}"
+      puts ""
+      puts "        BASIC ELIGIBILITY"
+      puts ""
+      b.answers.map do |k, v|
+        puts "        #{k}: #{v}"
+      end
+      puts ""
+      puts "        #{app.award_type.upcase} ELIGIBILITY"
+      puts ""
+      e.answers.map do |k, v|
+        puts "        #{k}: #{v}"
+      end
+      puts ""
+      puts "        #{e.errors.full_messages.join(', ')}"
       puts ""
     end
   end
