533db9496f9016752113d6bb9c9e04b41dd60f75
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2017-02-28 10:11:56 -0300

    Change email and name of BEIS department across the app
    - Change 'queensawards@bis.gsi.gov.uk' to 'queensawards@beis.gov.uk'
    - Change 'Department Business, Innovation & Skills' to  'Department for Business, Energy & Industrial Strategy'

--- app/mailers/account_mailers/base_mailer.rb
+++ app/mailers/account_mailers/base_mailer.rb
@@ -1,6 +1,6 @@
 class AccountMailers::BaseMailer < ApplicationMailer
   default from: ENV["MAILER_FROM"] || "no-reply@queens-awards-enterprise.service.gov.uk",
-          reply_to: "queensawards@bis.gsi.gov.uk"
+          reply_to: "queensawards@beis.gov.uk"
 
   layout "mailer"
 
