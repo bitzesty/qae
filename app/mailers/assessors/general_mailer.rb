b03fa763edeb6e6bdf860e3c4ed1696f994f7184
Ruslan Khamidullin <rusllonrails@gmail.com>
2017-02-28 11:18:33 +0300

    [FORM] As an Admin I want to be able to set an Application to the state 'Ineligible' post submission
    [TRELLO STORY](https://trello.com/c/yloqZ4Qt/1563-as-an-admin-i-want-to-be-able-to-set-an-application-to-the-state-ineligible-post-submission-so-i-don-t-have-to-wait-for-the-auto)

--- app/mailers/assessors/general_mailer.rb
+++ app/mailers/assessors/general_mailer.rb
@@ -2,7 +2,7 @@ class Assessors::GeneralMailer < ApplicationMailer
   def led_application_withdrawn(form_answer_id, assessor_id)
     @form_answer = FormAnswer.find(form_answer_id)
     @assessor = Assessor.find(assessor_id)
-    @subject = "Application Ref: #{@form_answer.urn} Withdrawn"
+    @subject = "Application Ref: #{@form_answer.urn} Withdrawn/Ineligible"
     mail to: @assessor.email, subject: @subject
   end
 
