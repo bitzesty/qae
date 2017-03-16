c0a78a2072b0650e5d1623fcf7d699303eba9fa3
Vasili Kachalko <amarant.st@gmail.com>
2017-03-16 22:46:17 +0300

    update winner's emails and winners section page
    https://trello.com/c/pSPLjYUB/1586-qae16imp-prepare-the-winners-resources-page-for-2017-because-it-currently-just-says-todo

--- app/mailers/account_mailers/business_apps_winners_mailer.rb
+++ app/mailers/account_mailers/business_apps_winners_mailer.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class AccountMailers::BusinessAppsWinnersMailer < AccountMailers::BaseMailer
   def notify(form_answer_id, collaborator_id)
     @form_answer = FormAnswer.find(form_answer_id).decorate
@@ -6,13 +7,17 @@ class AccountMailers::BusinessAppsWinnersMailer < AccountMailers::BaseMailer
 
     @name = "#{@user.title} #{@user.last_name}"
 
-    @deadline = Settings.current.deadlines.where(kind: "buckingham_palace_attendees_details").first
+    deadlines = Settings.current.deadlines
+    @deadline = deadlines.where(kind: "buckingham_palace_attendees_details").first
     @deadline = @deadline.trigger_at
-    @media_deadline = Settings.current.deadlines.where(kind: "buckingham_palace_media_information").first
-    @media_deadline = @media_deadline.try :strftime, "%H.%M on %A %d %B %Y"
-    @book_notes_deadline = Settings.current.deadlines.where(kind: "buckingham_palace_confirm_press_book_notes").first
+
+    @media_deadline = deadlines.where(kind: "buckingham_palace_media_information").first
+    @media_deadline = @media_deadline.try :strftime, "%A %d %B %Y"
+
+    @book_notes_deadline = deadlines.where(kind: "buckingham_palace_confirm_press_book_notes").first
     @book_notes_deadline = @book_notes_deadline.try :strftime, "%H.%M hrs on %d %B %Y"
-    @attendees_invite_date = Settings.current.deadlines.where(kind: "buckingham_palace_attendees_invite").first
+
+    @attendees_invite_date = deadlines.where(kind: "buckingham_palace_attendees_invite").first
     @attendees_invite_date = @attendees_invite_date.try :strftime, "%A %d %B %Y"
 
     subject = "Important information about your Queen’s Award"
