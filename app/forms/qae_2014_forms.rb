1eaa7698e1ad63535bf86b70ef0c7b0201428319
Vasili Kachalko <amarant.st@gmail.com>
2017-04-06 15:59:19 +0300

    remove 2014 from class names
    https://trello.com/c/KXmTeSSO/380-qae17imp-tech-debt-remove-2014-hardcoding-from-form-and-related-classes

--- app/forms/qae_2014_forms.rb
+++
@@ -1,8 +0,0 @@
-require 'qae_2014_forms/innovation'
-require 'qae_2014_forms/international_trade'
-require 'qae_2014_forms/sustainable_development'
-require 'qae_2014_forms/social_mobility'
-require 'qae_2014_forms/enterprise_promotion'
-
-class QAE2014Forms
-end
