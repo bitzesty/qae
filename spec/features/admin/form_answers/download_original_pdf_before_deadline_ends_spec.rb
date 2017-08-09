6c107706f60ae4ac66820ea3c551f0f64e2b8f20
Ruslan Khamidullin <rusllonrails@gmail.com>
2017-08-09 19:12:18 +0300

    [RSPEC TEST ENV] speed up attempt

--- spec/features/admin/form_answers/download_original_pdf_before_deadline_ends_spec.rb
+++ spec/features/admin/form_answers/download_original_pdf_before_deadline_ends_spec.rb
@@ -5,7 +5,7 @@ describe "Admin: Download original pdf of application at the deadline", %q{
 As an Admin
 I want to download original PDF of application at the deadline
 So that I can see original application data was at the deadline moment
-} do
+}, skip_ci: true do
 
   before do
     login_admin(create(:admin))
--- spec/features/assessor/press_summary_spec.rb
+++ spec/features/assessor/press_summary_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 include Warden::Test::Helpers
 
-describe "Assessor press_summary management", js: true do
+describe "Assessor press_summary management", js: true, skip_ci: true do
   let(:assessor) { create(:assessor, :lead_for_all) }
   let!(:form_answer) { create(:form_answer, :innovation, state: "awarded") }
 
