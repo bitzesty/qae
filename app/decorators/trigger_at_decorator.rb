d2c05639386c74efb2c1326c1984f0c76a02dfc7
Vasili Kachalko <amarant.st@gmail.com>
2018-01-18 21:48:13 +0300

    use methods for date placeholders to avoid caching issues

--- app/decorators/trigger_at_decorator.rb
+++ app/decorators/trigger_at_decorator.rb
@@ -1,9 +1,6 @@
 module TriggerAtDecorator
-  PLACEHOLDER = "<strong>-- --- #{AwardYear.current.year}</strong> at --:--".html_safe
-  DATE_PLACEHOLDER = "<strong>-- --- #{AwardYear.current.year}</strong>".html_safe
-
   def formatted_trigger_time
-    return PLACEHOLDER unless object.trigger_at
+    return placeholder unless object.trigger_at
 
     trigger_on = object.trigger_at.strftime("%-d %b %Y")
     trigger_at = object.trigger_at.strftime("%-l:%M%P")
@@ -14,13 +11,13 @@ module TriggerAtDecorator
   end
 
   def formatted_trigger_time_short
-    return PLACEHOLDER unless object.trigger_at
+    return placeholder unless object.trigger_at
 
     object.trigger_at.strftime("%d/%m/%Y")
   end
 
   def formatted_trigger_date(format=nil)
-    return DATE_PLACEHOLDER unless object.trigger_at
+    return date_placeholder unless object.trigger_at
 
     str_format = "#{object.trigger_at.day.ordinalize} %B"
     str_format = str_format + " %Y" if format.present? && format == "with_year"
@@ -29,6 +26,14 @@ module TriggerAtDecorator
   end
 
   private
+
+  def placeholder
+    "<strong>-- --- #{AwardYear.current.year}</strong> at --:--".html_safe
+  end
+
+  def date_placeholder
+    "<strong>-- --- #{AwardYear.current.year}</strong>".html_safe
+  end
 
   def midday?
     object.trigger_at == object.trigger_at.midday
