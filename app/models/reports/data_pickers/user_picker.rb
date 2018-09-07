6f35df9df715d5c30f0475d3c215ca5d2a7b4131
Vasili Kachalko <amarant.st@gmail.com>
2018-09-07 14:58:27 +0300

    fix reports breaking when user has been deleted
    https://trello.com/c/wC2JgCe6/814-qae18-fix-the-reports-for-the-previous-award-years-on-production

--- app/models/reports/data_pickers/user_picker.rb
+++ app/models/reports/data_pickers/user_picker.rb
@@ -1,66 +1,70 @@
 module Reports::DataPickers::UserPicker
+  def user
+    obj.user
+  end
+
   def contact_title
-    obj.user.title
+    user.try(:title)
   end
 
   def contact_first_name
-    obj.user.first_name
+    user.try(:first_name)
   end
 
   def contact_surname
-    obj.user.last_name
+    user.try(:last_name)
   end
 
   def contact_position
-    obj.user.job_title
+    user.try(:job_title)
   end
 
   def contact_email
-    obj.user.email
+    user.try(:email)
   end
 
   def contact_telephone
-    obj.user.phone_number
+    user.try(:phone_number)
   end
 
   def address_line1
-    obj.user.company_address_first
+    user.try(:company_address_first)
   end
 
   def address_line2
-    obj.user.company_address_second
+    user.try(:company_address_second)
   end
 
   def address_line3
-    obj.user.company_city
+    user.try(:company_city)
   end
 
   def postcode
-    obj.user.company_postcode
+    user.try(:company_postcode)
   end
 
   def telephone1
-    obj.user.phone_number
+    user.try(:phone_number)
   end
 
   def telephone2
-    obj.user.company_phone_number
+    user.try(:company_phone_number)
   end
 
   def qae_info_source
-    obj.user.qae_info_source
+    user.try(:qae_info_source)
   end
 
   def qae_info_source_other
-    obj.user.qae_info_source_other
+    user.try(:qae_info_source_other)
   end
 
   def user_creation_date
-    obj.user.created_at
+    user.try(:created_at)
   end
 
   def company_name
-    obj.user.company_name
+    user.try(:company_name)
   end
 
   def organisation_name
@@ -68,6 +72,6 @@ module Reports::DataPickers::UserPicker
   end
 
   def qao_permission
-    bool obj.user.subscribed_to_emails
+    bool user.try(:subscribed_to_emails)
   end
 end
