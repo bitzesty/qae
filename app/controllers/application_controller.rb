984f6b7d26d68a5d93d96ce5152769a5fdbebefa
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-11-01 18:27:21 +0530

    [RAILS-5] Fixed "Can't verify CSRF token authenticity" error
    - Fixed "Can't verify CSRF token authenticity" error.
    - Fixed "Attempting to generate a URL from non-sanitized request
    parameters!.." error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require "app_responder"
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
 
   ensure_security_headers if ENV["ENSURE_SECURITY_HEADERS"]
 
