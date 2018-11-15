716a8b9029cdaea8e860ca23df10d1f055fc7a81
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-11-15 10:37:39 +0530

    [RAILS-5] Remove config which is depecrated in Rails 5
    - Remove 'config.active_record.raise_in_transactional_callbacks = true'

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,6 @@ module Qae
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     config.autoload_paths += %W( #{config.root}/app/forms/ #{config.root}/app/search/ )
