ebacfad44effd1a0a3a53f3abf9f196acb4efde7
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-11-15 12:57:18 +0530

    [RAILS-5] More changes in controller specs.

--- spec/controllers/form/current_queen_awards_controller_spec.rb
+++ spec/controllers/form/current_queen_awards_controller_spec.rb
@@ -19,13 +19,13 @@ RSpec.describe Form::CurrentQueensAwardsController do
 
   describe "GET new" do
     it "renders the new template" do
-      get :new, form_answer_id: form_answer.id
+      get :new, params: { form_answer_id: form_answer.id }
       expect(response).to render_template("new")
     end
   end
   describe "GET edit" do
     it "renders the edit template" do
-      get :edit, id: '000', form_answer_id: form_answer.id, current_queens_award: { category: 'title', year: Date.today.year }
+      get :edit, params: { id: '000', form_answer_id: form_answer.id, current_queens_award: { category: 'title', year: Date.today.year } }
       expect(response).to render_template("edit")
     end
   end
@@ -33,7 +33,7 @@ RSpec.describe Form::CurrentQueensAwardsController do
   describe "POST create" do
     it "should create a resource" do
       allow_any_instance_of(CurrentQueensAward).to receive(:valid?) {true}
-      post :create, form_answer_id: form_answer.id, current_queens_award: { category: 'title', year: Date.today.year }
+      post :create, params: { form_answer_id: form_answer.id, current_queens_award: { category: 'title', year: Date.today.year } }
       expect(response).to redirect_to edit_form_url(form_answer, anchor: "non_js_applied_for_queen_awards_details-list-question")
       expect(form_answer.reload.document.present?).to be_truthy
     end
@@ -42,14 +42,14 @@ RSpec.describe Form::CurrentQueensAwardsController do
   describe "PUT update" do
     it "should update a resource" do
       allow_any_instance_of(CurrentQueensAward).to receive(:valid?) {true}
-      put :update, id: '000', form_answer_id: form_answer.id, current_queens_award: { category: 'title2' }
+      put :update, params: { id: '000', form_answer_id: form_answer.id, current_queens_award: { category: 'title2' } }
       expect(response).to redirect_to edit_form_url(form_answer, anchor: "non_js_applied_for_queen_awards_details-list-question")
     end
   end
 
   describe "Delete destroy" do
     it "should destroy a resource" do
-      delete :destroy, id: '000', form_answer_id: form_answer.id
+      delete :destroy, params: { id: '000', form_answer_id: form_answer.id }
       expect(response).to redirect_to edit_form_url(form_answer, anchor: "non_js_applied_for_queen_awards_details-list-question")
       expect(form_answer.reload.document.present?).to be_truthy
     end
--- spec/controllers/form_controller_spec.rb
+++ spec/controllers/form_controller_spec.rb
@@ -32,10 +32,12 @@ describe FormController do
     expect(Notifiers::Submission::SuccessNotifier).to receive(:new).with(form_answer) { notifier }
     expect_any_instance_of(FormAnswer).to receive(:eligible?).at_least(:once).and_return(true)
 
-    post :save, id: form_answer.id,
-                form: form_answer.document,
-                current_step_id: form_answer.award_form.steps.last.title.parameterize,
-                submit: "true"
+    post :save, params: {
+      id: form_answer.id,
+      form: form_answer.document,
+      current_step_id: form_answer.award_form.steps.last.title.parameterize,
+      submit: "true"
+    }
   end
 
   describe '#new_international_trade_form' do
