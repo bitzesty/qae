c7e96e60e3d780d1fcc01bcd460dfe6550130047
Muntasim <ahmed2tul@gmail.com>
2018-11-26 20:30:14 +0600

    existing nil field changed to false

--- db/migrate/20181102125923_add_constraints_to_most_recent_for_form_answer_transitions.rb
+++ db/migrate/20181102125923_add_constraints_to_most_recent_for_form_answer_transitions.rb
@@ -5,6 +5,7 @@ class AddConstraintsToMostRecentForFormAnswerTransitions < ActiveRecord::Migrati
     Rake::Task['statesman:backfill_most_recent'].invoke("FormAnswer")
 
     add_index :form_answer_transitions, [:form_answer_id, :most_recent], unique: true, where: "most_recent", name: "index_form_answer_transitions_parent_most_recent", algorithm: :concurrently rescue ""
+     FormAnswerTransition.where(most_recent: nil).update_all(most_recent: false)
     change_column_null :form_answer_transitions, :most_recent, false
   end
 
