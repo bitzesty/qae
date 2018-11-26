cc2618ba5ecfb9a6225e345b63e16163271cf9cb
Muntasim <ahmed2tul@gmail.com>
2018-11-26 23:16:53 +0600

    migration issue resolved

--- db/migrate/20181102125923_add_constraints_to_most_recent_for_form_answer_transitions.rb
+++ db/migrate/20181102125923_add_constraints_to_most_recent_for_form_answer_transitions.rb
@@ -3,12 +3,13 @@ class AddConstraintsToMostRecentForFormAnswerTransitions < ActiveRecord::Migrati
 
   def up
     system('rake statesman:backfill_most_recent[FormAnswer]')
-    add_index :form_answer_transitions, [:form_answer_id, :most_recent], unique: true, where: "most_recent", name: "index_form_answer_transitions_parent_most_recent", algorithm: :concurrently
+    FormAnswerTransition.where(most_recent: nil).update_all(most_recent: false)
+    add_index :form_answer_transitions, [:form_answer_id, :most_recent], unique: true, where: "most_recent", name: "index_form_answer_transitions_parent_most_recent", algorithm: :concurrently rescue ""
     change_column_null :form_answer_transitions, :most_recent, false
   end
 
   def down
-    remove_index :form_answer_transitions, name: "index_form_answer_transitions_parent_most_recent"
+    remove_index :form_answer_transitions, name: "index_form_answer_transitions_parent_most_recent" rescue ""
     change_column_null :form_answer_transitions, :most_recent, true
   end
 end
