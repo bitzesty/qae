493eb768d7e1252ccc17e488c605ec10588c46d8
Muntasim <ahmed2tul@gmail.com>
2018-11-26 19:30:03 +0600

    deploying after commenting out the problematic index

--- db/migrate/20181102125923_add_constraints_to_most_recent_for_form_answer_transitions.rb
+++ db/migrate/20181102125923_add_constraints_to_most_recent_for_form_answer_transitions.rb
@@ -4,12 +4,12 @@ class AddConstraintsToMostRecentForFormAnswerTransitions < ActiveRecord::Migrati
   def up
     Rake::Task['statesman:backfill_most_recent'].invoke("FormAnswer")
 
-    add_index :form_answer_transitions, [:form_answer_id, :most_recent], unique: true, where: "most_recent", name: "index_form_answer_transitions_parent_most_recent", algorithm: :concurrently
+    add_index :form_answer_transitions, [:form_answer_id, :most_recent], unique: true, where: "most_recent", name: "index_form_answer_transitions_parent_most_recent", algorithm: :concurrently rescue ""
     change_column_null :form_answer_transitions, :most_recent, false
   end
 
   def down
-    remove_index :form_answer_transitions, name: "index_form_answer_transitions_parent_most_recent"
+    remove_index :form_answer_transitions, name: "index_form_answer_transitions_parent_most_recent" rescue ""
     change_column_null :form_answer_transitions, :most_recent, true
   end
 end
