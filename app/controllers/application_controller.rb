ba4965ce7a19270f8b13a4d5381d0e1f0d06b7d5
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-12-12 11:51:11 +0530

    Remove calling method `ensure_security_headers` form the code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, prepend: true
 
-  ensure_security_headers if ENV["ENSURE_SECURITY_HEADERS"]
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_paper_trail_whodunnit
 
