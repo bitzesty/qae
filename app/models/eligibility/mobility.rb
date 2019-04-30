c59ced11235adb484eea7c4f15350c433a5c5977
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-04-30 11:38:51 +0300

    [PROMOTING OPPORTUNITY] Eligibility updates
    [TRELLO CARD](https://trello.com/c/P7TcR2Mo/748-qae20imp-review-and-update-eligibility-for-promoting-opportunity-12)

--- app/models/eligibility/mobility.rb
+++ app/models/eligibility/mobility.rb
@@ -3,8 +3,21 @@ class Eligibility::Mobility < Eligibility
 
   property :programme_validation,
             boolean: true,
-            label: "Do you have one or more social mobility programmes?",
-            accept: :true
+            label: "Do you have one or more qualifying social mobility programme?",
+            accept: :true,
+            hint: %(
+              <p class='question-context'>
+                Qualifying programme should be in one of the following areas to help socially disadvantaged individuals or groups:
+              </p>
+              <ul class='question-context'>
+                <li>Work experience, careers advice or mentoring for young people;</li>
+                <li>Offering non-graduate routes such as traineeships or changing recruitment practices;</li>
+                <li>Giving equal support and progression opportunities to all employees.</li>
+              </ul>
+              <p class='question-context'>
+                Please note, a programme could be an initiative, activity, course, system, business model approach or strategy, service or application, practice, policy or product.
+              </p>
+            )
 
   property :programme_operation,
             boolean: true,
@@ -13,7 +26,7 @@ class Eligibility::Mobility < Eligibility
 
   property :programme_commercial_success,
             boolean: true,
-            label: "Has the programme(s) benefited your organisation?",
-            hint: "For example, it has improved your reputation, employee relations, diversity, collaboration or led to savings or growth in the business.",
+            label: "Can you demonstrate that the programme(s) benefited your organisation or employees?",
+            hint: "For example, can you provide evidence that it has improved your reputation, employee relations, diversity, collaboration or led to savings or growth in the business?",
             accept: :true
 end
