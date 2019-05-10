73a6152058c82f470564e90330fa96b2068397af
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-05-10 13:42:01 +0300

    [GOV UK Notify API] removed no longer using ApologyMailer

--- app/mailers/users/apology_mailer.rb
+++
@@ -1,8 +0,0 @@
-class Users::ApologyMailer < ApplicationMailer
-  def notify(user_id)
-    @user = User.find(user_id)
-    @subject = "Queen's Awards for Enterprise: Multiple reminders sent by mistake"
-
-    send_mail_if_not_bounces ENV['GOV_UK_NOTIFY_API_TEMPLATE_ID'], to: @user.email, subject: @subject
-  end
-end
