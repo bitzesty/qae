bd370cbe1410cfe534e31efcdd8fbc4323fd3615
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-05-13 15:08:03 +0300

    [Applicants - Unsubscribe Feature] Submission Reminder
    [TRELLO CARD](https://trello.com/c/vysyOoXp/631-qae18imp-as-an-account-holder-i-want-to-be-able-to-unsubscribe-from-certain-system-emails-so-that-i-dont-receive-unwanted-emails)

--- app/services/notifiers/email_notification_service.rb
+++ app/services/notifiers/email_notification_service.rb
@@ -56,9 +56,19 @@ class Notifiers::EmailNotificationService
   end
 
   def reminder_to_submit(award_year)
-    gather_data_and_send_emails!(
-      award_year.form_answers.business.where(submitted_at: nil),
-      AccountMailers::ReminderToSubmitMailer
+    collaborator_data = []
+    scope = award_year.form_answers.business.where(submitted_at: nil)
+
+    scope.each do |form_answer|
+      form_answer.collaborators.each do |collaborator|
+        unless collaborator.notification_when_submission_deadline_is_coming?
+          collaborator_data << { form_answer_id: form_answer.id, collaborator_id: collaborator.id }
+        end
+      end
+    end
+
+    send_emails_to_collaborators!(
+      collaborator_data, AccountMailers::ReminderToSubmitMailer
     )
   end
 
