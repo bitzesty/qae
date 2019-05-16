e29168a7863798561c764c5d5ce32149d300dcc1
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-05-16 17:34:33 +0300

    [DEBOUNCE API] 7 is ok

--- app/workers/check_applicants_emails_on_bounce_worker.rb
+++ app/workers/check_applicants_emails_on_bounce_worker.rb
@@ -2,7 +2,7 @@ class CheckApplicantsEmailsOnBounceWorker
   include Sidekiq::Worker
 
   def perform
-    User.order(id: :asc).not_checked_on_bounced_emails.find_each.map do |user|
+    User.order(id: :asc).where(debounce_api_response_code: "7").find_each.map do |user|
       CheckAccountOnBouncesEmail.new(
         user
       ).run!
