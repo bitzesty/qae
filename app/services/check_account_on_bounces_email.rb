75329187a0ad25a4e02586ce1481212ca8fd07b8
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-05-20 20:43:17 +0300

    [DEBOUNCE API IMPROVEMENTS] Admin - User's detail page: added debounce check indicator
    [TRELLO CARD](https://trello.com/c/YARhM82w/906-qae1819-optimization-of-debounce-api-checks-make-them-automated-once-per-period-eg-quarter-or-half-of-year)

--- app/services/check_account_on_bounces_email.rb
+++ app/services/check_account_on_bounces_email.rb
@@ -42,7 +42,7 @@ class CheckAccountOnBouncesEmail
     end
 
     def bounce_reason(code)
-      DEBOUNCE_API_RESPONSE_CODES[code]
+      "#{DEBOUNCE_API_RESPONSE_CODES[code]} (code: '#{code}')"
     end
   end
 
