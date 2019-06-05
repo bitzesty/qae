b1f16e8bac9519f2c8f26d812b3c96c44087bedf
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-06-05 21:22:03 +0300

    [BASE ELIGIBILITY] Question 2: copy and logic updates
    [TRELLO STORY](https://trello.com/c/A59p8uII/926-qae0619-support-request-edit-eligibilty-question-2)

--- app/helpers/eligibility_helper.rb
+++ app/helpers/eligibility_helper.rb
@@ -2,4 +2,23 @@ module EligibilityHelper
   def final_eligibility_page?(step)
     !step || step.to_s == "wicked_finish"
   end
+
+  def eligibility_tax_returns_question_ops
+    [
+      ['Yes', 'true'], 
+      ['No', 'false'], 
+      ['N/A', 'na']
+    ]
+  end
+
+  def eligibility_tax_returns_formatted_answer(answer)
+    case answer
+    when 'true'
+      'Yes'
+    when 'false'
+      'No'
+    when 'na'
+      'N/A'
+    end
+  end
 end
