2eab9cf7da289de54473b4bf26a9b4802a487852
Vasili Kachalko <amarant.st@gmail.com>
2019-06-20 15:34:39 +0300

    use externally accessible keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,12 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_credentials = PaasResolver.s3_config
-    config.fog_directory = PaasResolver.s3_bucket
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["AWS_REGION"]
+    }
+    config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
     config.storage = :fog
     config.fog_public = false
   else
--- lib/paas_resolver.rb
+++ lib/paas_resolver.rb
@@ -14,34 +14,4 @@ module PaasResolver
   rescue
     ENV["DATABASE_URL"]
   end
-
-  def s3_config
-    creds = s3_credentials
-
-    {
-      provider: 'AWS',
-      aws_access_key_id: creds["aws_access_key_id"],
-      aws_secret_access_key: creds["aws_secret_access_key"],
-      region: creds["aws_region"]
-    }
-  rescue
-    {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_REGION"]
-    }
-  end
-
-  def s3_bucket
-    s3_credentials["bucket_name"]
-  rescue
-    ENV["AWS_S3_BUCKET_NAME"]
-  end
-
-  def s3_credentials
-    buckets = JSON.parse(ENV["VCAP_SERVICES"])["aws-s3-bucket"]
-    bucket = buckets.detect { |b| b["instance_name"] == "uploadsbucket" }
-    bucket["credentials"]
-  end
 end
