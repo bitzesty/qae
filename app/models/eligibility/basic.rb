ed149de0d35d085feec676aa25a0e5b3fee472da
Dave J <david@dvj.me.uk>
2020-07-02 15:23:51 +0100

    📝 Adding new "financial figures" basic question
    This commit adds a new basic eligibility question, asking the applicant
    whether they can provide figures for the past 3 financial years.

--- app/models/eligibility/basic.rb
+++ app/models/eligibility/basic.rb
@@ -19,6 +19,13 @@ class Eligibility::Basic < Eligibility
             accept: :true,
             hint: "Part-time staff should be counted in full-time equivalents (FTE). "
 
+  property :can_provide_financial_figures,
+            boolean: true,
+            label: "Are you able to provide financial figures for the last three years for your organisation?",
+            accept: :true,
+            hint: "You will have to submit data for the last three financial years. Your latest financial year has to be the one that falls before the #{Settings.current_submission_deadline.decorate.formatted_trigger_time} (the submission deadline). If you haven't reached or finalised your latest year-end yet, you will be able to provide estimated figures."
+
+
   property :organization_kind,
             values: %w[business charity],
             label: "What kind of organisation is it?",
