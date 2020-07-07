c982e18949c76f120f42a7bf0c68d953be0670fa
Dave J <david@dvj.me.uk>
2020-07-07 16:57:59 +0100

    💄 Change styling of questions B7a and B7b
    Whilst discussing the so upcoming changes to Section C, we spotted that
    two of our Section B questions, those focusing on community/social
    impact of an organisation/award, deviated slightly from the mocked-up
    designs.
    
    This commit implements the correctly styling by removing redundant
    header elements and making use of the `question_sub_title` method, which
    until now hasn't been used in the particular form.

--- app/forms/award_years/v2021/social_mobility/social_mobility_step2.rb
+++ app/forms/award_years/v2021/social_mobility/social_mobility_step2.rb
@@ -525,15 +525,12 @@ class AwardYears::V2021::QAEForms
           conditional :application_category, "initiative"
         end
 
-        header :initiative_header_b7a, "Impact on community and society." do
-          ref "B 7a"
-          conditional :application_category, "initiative"
-        end
-
-        textarea :initiative_community_society_impact, "What is the impact of your initiative on the local community and at a regional and national level; and how is this exemplary?" do
-          classes "sub-question"
-          sub_ref "B 7.1a"
+        textarea :initiative_community_society_impact, "Impact on community and society." do
+          sub_ref "B 7a"
           required
+          question_sub_title %{
+            What is the impact of your initiative on the local community and at a regional and national level; and how is this exemplary?
+          }
           context %(
             <p>For example, has your initiative led to there being more people from disadvantaged backgrounds being in employment in your area that is higher than the national average? Has it increased recognition and awareness of these initiatives as being a valid route to employment? Has it led to higher employment outcomes regionally?</p>
           )
@@ -874,15 +871,12 @@ class AwardYears::V2021::QAEForms
           conditional :application_category, "organisation"
         end
 
-        header :initiative_header_b7b, "Impact on community and society." do
-          ref "B 7b"
-          conditional :application_category, "organisation"
-        end
-
-        textarea :organisation_community_society_impact, "What is the impact of your organisation on the local community and at a regional and national level; and how is this exemplary?" do
-          classes "sub-question"
-          sub_ref "B 7.1b"
+        textarea :organisation_community_society_impact, "Impact on community and society." do
+          sub_ref "B 7b"
           required
+          question_sub_title %{
+            What is the impact of your organisation on the local community and at a regional and national level; and how is this exemplary?
+          }
           context %(
             <p>For example, has your organisation’s activities led to there being more people from disadvantaged backgrounds being in employment in your area that is higher than the national average? Has it increased recognition and awareness of these initiatives as being a valid route to employment? Has it led to higher employment outcomes regionally?</p>
           )
