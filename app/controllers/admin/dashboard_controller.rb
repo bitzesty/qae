3ec400ed4b4d5a100b3c2a06e438b995726829b6
Mauricio Andre Cinelli <mauricio.andre.cinelli@gmail.com>
2020-08-14 17:05:48 -0300

    Dashboard reports + Downloads page changes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,7 +4,19 @@ class Admin::DashboardController < Admin::BaseController
     @statistics = FormAnswerStatistics::Picker.new(@award_year)
   end
 
+  def downloads
+    authorize :dashboard, :index?
+  end
+
   def totals_by_month
     authorize :dashboard, :totals_by_month?
+  end
+
+  def totals_by_week
+    authorize :dashboard, :totals_by_week?
+  end
+
+  def totals_by_day
+    authorize :dashboard, :totals_by_day?
   end
 end
