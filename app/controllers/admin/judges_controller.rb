0b9bf3eb3dac8f37a077db8a3c506f8e6d15e42e
Vasili Kachalko <amarant.st@gmail.com>
2020-09-18 14:17:06 +0300

    add user model for judges

---
+++ app/controllers/admin/judges_controller.rb
@@ -0,0 +1,64 @@
+class Admin::JudgesController < Admin::UsersController
+  def index
+    params[:search] ||= JudgeSearch::DEFAULT_SEARCH
+    params[:search].permit!
+    authorize :judge, :index?
+
+    @search = JudgeSearch.new(Judge.all).
+                             search(params[:search])
+    @resources = @search.results.page(params[:page])
+  end
+
+  def new
+    @resource = Judge.new
+    authorize @resource, :create?
+  end
+
+  def create
+    @resource = Judge.new(resource_params)
+    authorize @resource, :create?
+
+    @resource.save
+    location = @resource.persisted? ? admin_judges_path : nil
+    respond_with :admin, @resource, location: location
+  end
+
+  def update
+    authorize @resource, :update?
+
+    if resource_params[:password].present?
+      @resource.update(resource_params)
+    else
+      @resource.update_without_password(resource_params)
+    end
+
+    respond_with :admin, @resource, location: admin_judges_path
+  end
+
+  def destroy
+    authorize @resource, :destroy?
+    @resource.soft_delete!
+
+    respond_with :admin, @resource, location: admin_judges_path
+  end
+
+  private
+
+  def find_resource
+    @resource = Judge.find(params[:id])
+  end
+
+  def resource_params
+    params.require(:judge).
+      permit(:email,
+             :password,
+             :password_confirmation,
+             :first_name,
+             :last_name,
+             :trade_role,
+             :innovation_role,
+             :development_role,
+             :mobility_role,
+             :promotion_role)
+  end
+end
---
+++ app/search/judge_search.rb
@@ -0,0 +1,7 @@
+class JudgeSearch < Search
+  DEFAULT_SEARCH = {
+    sort: "full_name"
+  }
+
+  include FullNameSort
+end
