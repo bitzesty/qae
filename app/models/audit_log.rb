5c6109f6e8c050718e4d7ee41ab5bacfb6e38fa9
Dave J <dave.jones@bitzesty.com>
2020-10-14 14:14:44 +0100

    ✨ Implement descriptive text for audit logs
    There are several points within the QAE application where audit events
    can be rendered to the user interface. To keep our code DRY and ensure
    consistency across different pages, let's centralise the logic for
    rendering an event as a descriptive sentence.
    
    This commit defines a `to_s` method on the `AuditLog` model, as well as
    several (private) helper methods for building up the sentence.
    
    https://trello.com/c/YQlNYgcA/1160-qaeimp-last-updated-column-on-applications-page

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -5,4 +5,30 @@ class AuditLog < ApplicationRecord
 
   belongs_to :subject, polymorphic: true
   belongs_to :auditable, polymorphic: true
+
+  ACTION_DESCRIPTIONS = {
+    "application_update": "updated their application",
+  }
+
+  def to_s
+    "On #{date_string} at #{time_string} #{user_string} #{description_for_action_type(action_type)}"
+  end
+
+  private
+
+  def date_string
+    created_at.in_time_zone("London").strftime("%d/%m/%Y")
+  end
+
+  def time_string
+    created_at.in_time_zone("London").strftime("%H:%M")
+  end
+
+  def description_for_action_type(action_type)
+    ACTION_DESCRIPTIONS[action_type.to_sym] || "Unsupported action"
+  end
+
+  def user_string
+    "#{subject.full_name} (#{subject.class.name})"
+  end
 end
