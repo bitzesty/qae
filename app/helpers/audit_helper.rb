ef0d6223b334e92b77c6016ff526b5f20f3a7a90
Dave J <dave.jones@bitzesty.com>
2020-10-19 17:59:37 +0100

    🐛 Use dummy user if papertrail whodunnit is nil
    We've recently seen some Sentry errors trying to call `.split` on a
    `nil` object. After looking closer at the issue, it seems we're
    occasionally having trouble generating our list of `AuditEvent` records
    due to some `PaperTrail::Version` objects having a `nil` value for
    `whodunnit`.
    
    This commit avoids the runtime error by moving our `dummy_user` helper
    method out of `ApplicationController` and into a reusable `audit_helper`
    module. This allows us to return a dummy user record whenever the
    `PaperTrail` `whodunnit` value is `nil`.

---
+++ app/helpers/audit_helper.rb
@@ -0,0 +1,19 @@
+module AuditHelper
+
+  def dummy_user
+    User.find_by(email: "dummy_user@bitzesty.com") || User.create!(dummy_user_params)
+  end
+
+  private
+
+  def dummy_user_params
+   {
+     email: "dummy_user@example.com",
+     password: SecureRandom.base64(16),
+     agreed_with_privacy_policy: '1',
+     role: "regular",
+     first_name: "Unknown",
+     last_name: "User"
+   }
+  end
+end
