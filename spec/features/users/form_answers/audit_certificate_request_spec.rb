e34980b6fa8bb64e9227fc037bfba9779c128562
Dave J <dave.jones@bitzesty.com>
2020-10-22 18:03:22 +0100

    ✅ Update feature spec to look for renamed link

--- spec/features/users/form_answers/audit_certificate_request_spec.rb
+++ spec/features/users/form_answers/audit_certificate_request_spec.rb
@@ -30,7 +30,7 @@ So that I can check, complete it and then upload it to application
     it "should render page" do
       expect_to_see "Verification of Commercial Figures"
       expect(page).to have_link(
-        "this Verification of Commercial Figures",
+        "Download the External Accountant's Report form",
         href: users_form_answer_audit_certificate_url(form_answer, format: :pdf)
       )
     end
