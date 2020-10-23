171693c580eab129d56712fbf3cb05e2eae839fb
Dave J <dave.jones@bitzesty.com>
2020-10-23 12:57:23 +0100

    ✨ Add new ListOfProcedures model
    For this new award year, QAE are adding an extra verification step for
    applicants who have been short-listed for an award. Going forward, in
    addition to uploading the regular "Verification of Figures" document,
    now known as an "External Accountant's Report", applicants must also
    upload a "List of Procedures" document.
    
    This commit introduces a new `ListOfProcedure` model as well as the
    database migrations needed for persistence.
    
    https://trello.com/c/UGpnY8ei/1194-qaeimp-add-an-upload-list-of-procedures-feature

---
+++ app/models/list_of_procedures.rb
@@ -0,0 +1,46 @@
+class ListOfProcedures < ApplicationRecord
+  mount_uploader :attachment, ListOfProceduresUploader
+  scan_file      :attachment
+
+  include ::InfectedFileCleaner
+  clean_after_scan :attachment
+
+  begin :associations
+    belongs_to :form_answer
+    belongs_to :reviewable, polymorphic: true
+  end
+
+  begin :validations
+    validates :form_answer_id, uniqueness: true,
+                               presence: true
+    validates :attachment, presence: true,
+                           on: :create,
+                           file_size: {
+                             maximum: 5.megabytes.to_i
+                           }
+    validates :reviewable_type,
+              :reviewable_id,
+              :reviewed_at,
+              presence: true,
+              if: :reviewed?
+  end
+
+  before_save :clean_changes_description
+
+  enum status: {
+    no_changes_necessary: 0,
+    confirmed_changes: 1
+  }
+
+  def reviewed?
+    reviewed_at.present?
+  end
+
+  private
+
+  def clean_changes_description
+    if status_changed? && status == "no_changes_necessary" && changes_description.present?
+      self.changes_description = nil
+    end
+  end
+end
