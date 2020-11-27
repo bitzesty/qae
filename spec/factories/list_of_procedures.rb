3c2469559030827913abead24ea310a4342cc663
Vasili Kachalko <amarant.st@gmail.com>
2020-11-27 13:46:34 +0300

    FactoryGirl -> FactoryBot

--- spec/factories/list_of_procedures.rb
+++ spec/factories/list_of_procedures.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :list_of_procedures do
     association :form_answer, factory: :form_answer
-    attachment_scan_results "clean"
+    attachment_scan_results { "clean" }
     attachment do
       Rack::Test::UploadedFile.new(
         File.join(
