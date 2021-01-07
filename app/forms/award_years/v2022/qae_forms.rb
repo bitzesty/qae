c385ddc16a9db675eec98aeaaa52c79c5288614b
Vasili Kachalko <amarant.st@gmail.com>
2021-01-07 16:36:47 +0300

    add 2022 award year support

---
+++ app/forms/award_years/v2022/qae_forms.rb
@@ -0,0 +1,7 @@
+require 'award_years/v2022/innovation'
+require 'award_years/v2022/international_trade'
+require 'award_years/v2022/sustainable_development'
+require 'award_years/v2022/social_mobility'
+
+class AwardYears::V2022::QAEForms
+end
