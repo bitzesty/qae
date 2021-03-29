d51310ff1eb0049ac6f5e710e41043ea0fac30f6
Louis Kirkham <louiskirkham@gmail.com>
2021-03-29 08:35:33 +0100

    Adds event logging for press summary creation
    Adds an audit log when editing press summary notes or contact details for the first time.
    Adds a description to the yml

--- app/controllers/admin/press_summaries_controller.rb
+++ app/controllers/admin/press_summaries_controller.rb
@@ -1,4 +1,4 @@
 class Admin::PressSummariesController < Admin::BaseController
-  after_action :log_event, only: [:update, :signoff]
+  after_action :log_event, only: [:create, :update, :signoff]
   include PressSummaryMixin
 end
