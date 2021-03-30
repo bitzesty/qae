2e1c8e10e6d182329c402d3a6bf78f3c6eed6f29
Louis Kirkham <louiskirkham@gmail.com>
2021-03-30 10:16:22 +0100

    Adds log event to verification of commercial figures
    Removes duplicated on click listener from javascript.
    Adds methods to assign form answer and action type.
    Adds translation for new action type.
    Adds log event function if audit certificate review is saved.

--- app/controllers/concerns/review_audit_certificates_mixin.rb
+++ app/controllers/concerns/review_audit_certificates_mixin.rb
@@ -1,11 +1,16 @@
 module ReviewAuditCertificatesMixin
+
+  def self.included(base)
+    base.before_action :form_answer
+  end
+
   def create
     @review_audit_certificate = ReviewAuditCertificate.new(create_params)
     @review_audit_certificate.subject = current_subject
-
+    
     authorize @review_audit_certificate, :create?
 
-    @review_audit_certificate.save
+    log_event if @review_audit_certificate.save
 
     respond_to do |format|
       format.js { head :ok }
@@ -23,5 +28,13 @@ module ReviewAuditCertificatesMixin
       :form_answer_id,
       :status
     )
+  end
+
+  def form_answer
+    form_answer = FormAnswer.find(params[:review_audit_certificate][:form_answer_id])
+  end
+
+  def action_type
+    "verification_of_commercial_figures_#{action_name}"
   end
 end
