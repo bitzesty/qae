e5254cf1ce9c16c677677ff7b6b1b3f278a50a0a
Daniela Sinclair <daniela.sinclair@bitzesty.com>
2021-10-20 11:25:21 +0200

    Removed legends from step 5, these were questions

--- app/forms/award_years/v2022/innovation/innovation_step5.rb
+++ app/forms/award_years/v2022/innovation/innovation_step5.rb
@@ -5,11 +5,10 @@ class AwardYears::V2022::QAEForms
         upload :innovation_materials, "If there is additional material you feel would help us to assess your entry, then you can add up to 3 files or website addresses here." do
           ref "E 1"
           context %(
-            <legend class="govuk-fieldset__legend">
-              <p>
-                Please include any vital information in the form as we can't guarantee the additional material will be reviewed. It is also essential to reference these in your application to ensure the assessors are aware of them and can relate this information to the relevant questions.
-              </p>
-            </legend>
+            <p>
+              Please include any vital information in the form as we can't guarantee the additional material will be reviewed. It is also essential to reference these in your application to ensure the assessors are aware of them and can relate this information to the relevant questions.
+            </p>
+
             <p>
               You can upload files in all common formats, as long as they're less than 5mb each. You may link to videos, websites or other media you feel relevant.
             </p>
