b877aa132be7b9ae06836a50c7bb53a50e50cc42
Daniela Sinclair <daniela.sinclair@bitzesty.com>
2021-11-04 16:00:22 +0000

    Added non-js errors to head_of_business question

--- app/forms/qae_form_builder/head_of_business_question.rb
+++ app/forms/qae_form_builder/head_of_business_question.rb
@@ -1,13 +1,51 @@
 class QAEFormBuilder
   class HeadOfBusinessQuestionValidator < QuestionValidator
+    NO_VALIDATION_SUB_FIELDS = [:honours]
+    def errors
+      result = super
+
+      if question.required?
+        question.required_sub_fields.each do |sub_field|
+          suffix = sub_field.keys[0]
+          if !question.input_value(suffix: suffix).present? && NO_VALIDATION_SUB_FIELDS.exclude?(suffix)
+            result[question.hash_key(suffix: suffix)] ||= ""
+            result[question.hash_key(suffix: suffix)] << " Can't be blank."
+          end
+        end
+      end
+
+      # need to add govuk-form-group--errors class
+      result[question.hash_key] ||= "" if result.any?
+
+      result
+    end
   end
 
   class HeadOfBusinessQuestionDecorator < QuestionDecorator
     def required_sub_fields
-      [
-        {first_name: "First name"},
-        {last_name: "Last name"}
-      ]
+      if sub_fields.present?
+        sub_fields
+      else
+        [
+          { title: "Title" },
+          { first_name: "First name" },
+          { last_name: "Last name" },
+          { honours: "Personal Honours", ignore_validation: true },
+          { job_title: "Job title / role in the organisation" },
+          { email: "Email address" }
+        ]
+      end
+    end
+
+    def rendering_sub_fields
+      # We are rejecting :street, because :building and :street
+      # are rendering together in same block
+      # and the :building is the first one
+      required_sub_fields.reject do |f|
+        f.keys.include?(:honours)
+      end.map do |f|
+        [f.keys.first, f.values.first]
+      end
     end
   end
 
