7f532d998df469ed19a0d0fdb352f009b52ff928
Daniela Sinclair <daniela.sinclair@bitzesty.com>
2021-11-11 12:56:24 +0000

    Added section_info attribute to 'About this section' text

--- app/forms/award_years/v2022/international_trade/international_trade_step1.rb
+++ app/forms/award_years/v2022/international_trade/international_trade_step1.rb
@@ -4,6 +4,7 @@ class AwardYears::V2022::QAEForms
     def trade_step1
       @trade_step1 ||= proc do
         header :company_information_header, "" do
+          section_info
           context %(
             <h3 class='govuk-heading-m'>About this section</h3>
             <p class='govuk-body'>
