6a2546a77ac62de2f88fc32b2c9ffa4f7c00aa46
Daniela Sinclair <daniela.sinclair@bitzesty.com>
2021-11-18 11:16:10 +0000

    Added section_info attribute to remaining innovation form sections

--- app/forms/award_years/v2022/innovation/innovation_step3.rb
+++ app/forms/award_years/v2022/innovation/innovation_step3.rb
@@ -4,6 +4,7 @@ class AwardYears::V2022::QAEForms
     def innovation_step3
       @innovation_step3 ||= proc do
         header :commercial_success_info_block, "" do
+          section_info
           context %(
             <h3 class="govuk-heading-m">About this section</h3>
             <p class="govuk-body">
