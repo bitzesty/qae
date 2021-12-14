5921a5ca35ce2946551fa02f62073b3c7aad266f
Vasili Kachalko <amarant.st@gmail.com>
2021-12-14 10:50:13 +0100

    be less paranoid about unlock instructions and confirmation errors

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,21 +1,3 @@
 class Users::PasswordsController < Devise::PasswordsController
-  private
-
-  def successfully_sent?(resource)
-    notice = if Devise.paranoid && should_be_paranoid_about?(resource.errors[:email])
-      resource.errors.clear
-      :send_paranoid_instructions
-    elsif resource.errors.empty?
-      :send_instructions
-    end
-
-    if notice
-      set_flash_message! :notice, notice
-      true
-    end
-  end
-
-  def should_be_paranoid_about?(errors)
-    errors.count != 1 || errors.first != "This field cannot be blank"
-  end
+  include Users::ReasonablyParanoidable
 end
