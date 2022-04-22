6dad6f6e466e0ee1c60ce2791f57fa4e766d805f
DaniBitZesty <daniela.sinclair@bitzesty.com>
2022-04-22 08:04:03 +0100

    Printing progress

--- spec/decorators/form_answer_decorator_spec.rb
+++ spec/decorators/form_answer_decorator_spec.rb
@@ -92,6 +92,7 @@ describe FormAnswerDecorator do
   describe "#dashboard_status" do
     it "returns fill progress when application is not submitted" do
      form_answer = create(:form_answer, :trade, state: "application_in_progress", document: { sic_code:  SICCode.first.code })
+     puts described_class.new(form_answer).dashboard_status
       expect(described_class.new(form_answer).dashboard_status).to eq("Application in progress...10%")
     end
 
