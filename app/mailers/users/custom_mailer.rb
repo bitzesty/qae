61c52a5d38f6fa4c45a7b827439cb84c78dfdbfe
Vasili Kachalko <amarant.st@gmail.com>
2022-08-03 13:17:08 +0200

    use text template for custom mailer for govuk notify compatibility

--- app/mailers/users/custom_mailer.rb
+++ app/mailers/users/custom_mailer.rb
@@ -1,12 +1,8 @@
-require "rails_autolink"
-
 class Users::CustomMailer < ApplicationMailer
-  include ActionView::Helpers::TextHelper
-
   def notify(user_id, user_class, body, subject)
     if %w(Admin User Assessor).include?(user_class)
       @user = user_class.constantize.find(user_id).decorate
-      @body = auto_link(simple_format(body))
+      @body = body
 
       send_mail_if_not_bounces ENV['GOV_UK_NOTIFY_API_TEMPLATE_ID'], to: @user.email, subject: subject_with_env_prefix(subject)
     end
