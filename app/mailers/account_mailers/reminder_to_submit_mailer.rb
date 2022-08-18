4172c4148d596485dd133bf21768bc696f3c0103
Vasili Kachalko <amarant.st@gmail.com>
2022-08-18 15:28:22 +0200

    change subject, avoid using hardcoded time

--- app/mailers/account_mailers/reminder_to_submit_mailer.rb
+++ app/mailers/account_mailers/reminder_to_submit_mailer.rb
@@ -2,11 +2,16 @@ class AccountMailers::ReminderToSubmitMailer < AccountMailers::BaseMailer
   def notify(form_answer_id, collaborator_id)
     @form_answer = FormAnswer.find(form_answer_id)
     @user = @form_answer.user
-    @deadline = Settings.current_submission_deadline.strftime("%A %d %B %Y")
+
+    submission_deadline = Settings.current_submission_deadline
+    @deadline = submission_deadline.strftime("%l%P on %A %d %B %Y")
+
+    ordinal = submission_deadline.trigger_at.day.ordinal
+    subject_deadline = submission_deadline.strftime("%e#{ordinal} %B")
+
     collaborator = User.find(collaborator_id)
 
-    # subject = "Queen's Awards for Enterprise: Reminder to submit"
-    subject = "Complete your Queen's Award application by 22nd September"
+    subject = "Complete your Queen's Award application by #{subject_deadline}"
 
     send_mail_if_not_bounces ENV['GOV_UK_NOTIFY_API_TEMPLATE_ID'], to: collaborator.email, subject: subject_with_env_prefix(subject)
   end
