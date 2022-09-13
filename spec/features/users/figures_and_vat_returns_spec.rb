484f351db75c82653f6f753a56d90e42a42094a9
Vasili Kachalko <amarant.st@gmail.com>
2022-09-13 12:54:38 +0200

    add vat returns/actual figures feature specs

---
+++ spec/features/users/figures_and_vat_returns_spec.rb
@@ -0,0 +1,55 @@
+require "rails_helper"
+include Warden::Test::Helpers
+
+describe "User uploads VAT returns and actual figures" do
+  let(:user) { create(:user, :completed_profile) }
+  let!(:settings) { create(:settings, :expired_submission_deadlines, award_year_id: AwardYear.current.id) }
+  let!(:form_answer) { create(:form_answer, :mobility, :recommended, user: user) }
+
+  before do
+    login_as user
+
+    settings.email_notifications.create!(
+      kind: "shortlisted_notifier",
+      trigger_at: DateTime.now - 1.day
+    )
+  end
+
+  it "allows to submit vat returns and actual figures" do
+    visit dashboard_url
+
+    expect(page).to have_content("Congratulations! Your application was shortlisted.")
+
+    click_link "Provide actual figures and VAT returns"
+
+    # can't submit without a single vat returns file
+    click_button "Submit"
+    expect(page).to have_content("You need to add at least one VAT returns file")
+
+    # uploading actual figures
+    within(".vat-returns-wrapper") do
+      click_link "Upload a file"
+    end
+
+    attach_file("Upload VAT returns", Rails.root + "spec/fixtures/cat.jpg")
+
+    click_button "Upload"
+
+    expect(page).to have_link("cat.jpg")
+
+    # uploading actual figures
+    within(".actual-figures-wrapper") do
+      click_link "Upload a file"
+    end
+
+    attach_file("Upload actual figures", Rails.root + "spec/fixtures/users.csv")
+
+    click_button "Upload"
+
+    expect(page).to have_link("users.csv")
+
+    click_button "Submit"
+
+    expect(page).to have_link("Completed")
+  end
+end
