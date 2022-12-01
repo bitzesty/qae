81bd19184e92d7795ab18d317b4655b0de15e925
Lubos Hricak <Lubosky@users.noreply.github.com>
2022-12-01 11:11:21 -0300

    Add service class to handle extracting company registration numbers
    Responsible for extracting company registration numbers from simple strings as
    well as from HTML blocks. Will probably have to watch and resolve some edge cases
    when tthey emerge.

---
+++ app/services/company_registration_number.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class CompanyRegistrationNumber
+  COMPANY_REGISTRATION_NUMBER_REGEX = %r{(?!\s)((AC|ZC|FC|GE|LP|OC|SE|SA|SZ|SF|GS|SL|SO|SC|ES|NA|NZ|NF|GN|NL|NC|R0|NI|EN|\d{0,2}|SG|FE(\s?))\d{5}(\d|C|R))|((RS|SO)\d{3}(\d{3}|\d{2}[WSRCZF]|\d(FI|RS|SA|IP|US|EN|AS)|CUS))|((NI|SL)\d{5}[\dA])|(OC(([\dP]{5}[CWERTB])|([\dP]{4}(OC|CU))))}i.freeze
+
+  def self.extract_from(content)
+    new(content).extract
+  end
+
+  attr_reader :content, :sanitizer
+
+  def initialize(content)
+    @sanitizer ||= Rails::Html::FullSanitizer.new
+    @content = content.to_s
+  end
+
+  def extract
+    return [] if content.blank?
+
+    sanitized = sanitize(content).strip
+    parts = tokenize(content)
+    numbers = parts.each_with_object([]) do |part, memo|
+      memo << part if part =~ COMPANY_REGISTRATION_NUMBER_REGEX
+    end
+
+    numbers
+  end
+
+  private
+
+  def sanitize(value)
+    sanitizer.sanitize(value)
+  end
+
+  def tokenize(value)
+    value
+      .remove(/(\(|\[).*(\)|\])/)
+      .scan(/[[:word:]]*/i)
+      .reject(&:blank?)
+  end
+end
