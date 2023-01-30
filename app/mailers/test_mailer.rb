d4aa13c15782140dc3cfe3131f18055c19c0ff12
Lubos Hricak <Lubosky@users.noreply.github.com>
2023-01-30 17:20:34 -0300

    Replace `Queen` references in mailers

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,7 +1,7 @@
 class TestMailer < ApplicationMailer
   def welcome(email)
     @email = email
-    @subject = "[Queen's Awards for Enterprise] test mailer!"
+    @subject = "[King's Awards for Enterprise] test mailer!"
 
     send_mail_if_not_bounces ENV['GOV_UK_NOTIFY_API_TEMPLATE_ID'], to: @email, subject: subject_with_env_prefix(@subject)
   end
--- app/mailers/users/supporter_mailer.rb
+++ app/mailers/users/supporter_mailer.rb
@@ -3,7 +3,7 @@ class Users::SupporterMailer < ApplicationMailer
     @user = User.find(user_id).decorate
     @supporter = Supporter.find(supporter_id).decorate
     @form_answer = @supporter.form_answer.decorate
-    @subject = "[Queen's Awards for Enterprise] Support Letter Request"
+    @subject = "[King's Awards for Enterprise] Support Letter Request"
     @nominee_name = @form_answer.nominee_full_name
     @nominator_name = @form_answer.nominator_full_name
     @nominator_email = @form_answer.nominator_email
