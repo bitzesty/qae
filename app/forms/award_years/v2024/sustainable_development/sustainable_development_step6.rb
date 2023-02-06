25f72900fb250fe72a27b2973e06d83bc9519219
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-06 16:46:41 +0000

    Moves question F3.2 to E2

--- app/forms/award_years/v2024/sustainable_development/sustainable_development_step6.rb
+++ app/forms/award_years/v2024/sustainable_development/sustainable_development_step6.rb
@@ -59,19 +59,6 @@ class AwardYears::V2024::QAEForms
           )
         end
 
-        confirm :entry_confirmation, "Confirmation of entry" do
-          ref "F 4"
-          required
-          text -> do
-            %(
-              By ticking this box, I submit an entry for consideration for The Queen's Awards for Enterprise #{AwardYear.current.year}. I certify that all the particulars given and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld. I undertake to notify The Queen's Awards Office of any changes to the information I have provided in this entry form.
-              <br>
-              <br>
-              I am not aware of any matter which might cast doubt on the worthiness of my organisation to receive a Queen's Award for Enterprise. I consent to all necessary enquiries being made by The Queen's Awards Office concerning this entry. This includes enquiries made of Government Departments and Agencies in discharging its responsibilities to vet any business unit which might be granted a Queen's Award to ensure the highest standards of propriety.
-            )
-          end
-        end
-
         confirm :due_diligence, "" do
           ref "F 4.1"
           required
