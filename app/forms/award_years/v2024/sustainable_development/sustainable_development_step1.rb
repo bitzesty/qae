8c3859d5784075b1e60a2a4bcf134a048cd060d9
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-06 17:02:47 +0000

    Adds section A copy and header

--- app/forms/award_years/v2024/sustainable_development/sustainable_development_step1.rb
+++ app/forms/award_years/v2024/sustainable_development/sustainable_development_step1.rb
@@ -3,8 +3,20 @@ class AwardYears::V2024::QAEForms
   class << self
     def development_step1
       @development_step1 ||= proc do
-        header :head_of_bussines_header, "Head of your organisation" do
-          ref "F 1"
+        header :consent_and_due_diligence_header, "Consent & Due Diligence" do
+          section_info
+          context %(
+            <h3 class='govuk-heading-m'>About section A</h3>
+            <p class='govuk-body'>
+              This section is to confirm that you have the authorisation to apply, that your organisation is financially stable, worthy, and that you understand what will happen after you apply in terms of due diligence. We recommend you carefully answer section A questions before proceeding with the rest of the application.
+            </p>
+          )
+          pdf_context_with_header_blocks [
+            [:bold, "About section A"],
+            [:normal, %(
+              This section is to confirm that you have the authorisation to apply, that your organisation is financially stable, worthy, and that you understand what will happen after you apply in terms of due diligence. We recommend you carefully answer section A questions before proceeding with the rest of the application.
+            )]
+          ]
         end
 
         confirm :confirmation_of_consent, "Consent to apply by head of organisation" do
