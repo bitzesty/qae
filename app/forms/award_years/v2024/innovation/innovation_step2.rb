883cb35b2c9b7d90f2b9158b74cb73b70cfa21f5
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-12 18:16:01 +0000

    Changes question A13.2 to B13.2

--- app/forms/award_years/v2024/innovation/innovation_step2.rb
+++ app/forms/award_years/v2024/innovation/innovation_step2.rb
@@ -330,7 +330,7 @@ class AwardYears::V2024::QAEForms
 
         options_business_name_changed :business_name_changed, "Have you changed the name of your organisation since your last entry?" do
           classes "sub-question"
-          sub_ref "A 13.2"
+          sub_ref "B 13.2"
 
           conditional :applied_for_queen_awards, :yes
 
