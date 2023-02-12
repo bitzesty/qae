ef5c07d51941aab69a4b968efce9f6537c6a77ec
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-12 17:41:22 +0000

    Changes question A3 to B3

--- app/forms/award_years/v2024/innovation/innovation_step2.rb
+++ app/forms/award_years/v2024/innovation/innovation_step2.rb
@@ -73,10 +73,10 @@ class AwardYears::V2024::QAEForms
 
         options :principal_business, "Does your organisation operate as a principal?" do
           required
-          ref "A 3"
+          ref "B 3"
           context %(
             <p>
-            We recommend that you apply as a principal. A principal invoices its customers (or their buying agents) and is the body to receive those payments.
+              We recommend that you apply as a principal. A principal invoices its customers (or their buying agents) and is the body to receive those payments.
             </p>
           )
           yes_no
