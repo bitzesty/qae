afa81f29ec3eadd9bcd20e0969cf8668733d382e
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-15 13:36:20 +0000

    Move B1.1 to C1.1

--- app/forms/award_years/v2024/international_trade/international_trade_step3.rb
+++ app/forms/award_years/v2024/international_trade/international_trade_step3.rb
@@ -66,8 +66,8 @@ class AwardYears::V2024::QAEForms
         end
 
         textarea :trade_brief_history, "Provide a brief history of your company, corporate targets and direction." do
-          classes "sub-question"
-          sub_ref "B 1.1"
+          classes "sub-question word-max-strict"
+          sub_ref "C 1.1"
           required
           rows 5
           words_max 500
