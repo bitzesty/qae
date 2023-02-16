bec383a6041a60f321b3d7431e3d3426f37fe93c
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-16 09:32:54 +0000

    Move F4 to F2

--- app/forms/award_years/v2024/international_trade/international_trade_step6.rb
+++ app/forms/award_years/v2024/international_trade/international_trade_step6.rb
@@ -2,27 +2,34 @@ class AwardYears::V2024::QAEForms
   class << self
     def trade_step6
       @trade_step6 ||= proc do
-        upload :innovation_materials, "If there is additional material you feel would help us to assess your entry then you can add up to 3 files or website addresses here" do
-          ref "E 1"
+        upload :innovation_materials, "Supplementary materials" do
+          ref "F 1"
           context %(
+            <p>If there are additional materials you feel would help us to assess your entry, then you can add up to three files or online links in this section.</p>
+            <h4 class="govuk-heading-s">What can and cannot be included:</h3>
+            <ul>
+              <li>You <strong>can</strong> include links to promotional videos, websites, other media, or documents you feel are relevant and help illustrate your application.</li>
+              <li>We <strong>will not</strong> consider business plans, annual accounts or company policy documents.</li>
+            </ul>
+            <h4 class="govuk-heading-s">Please note:</h3>
+            <ul>
+              <li>For assessors to review the supporting material, you must reference them by their names in your answers. Please do so to ensure they are reviewed.</li>
+              <li>Do not use the supporting material as a substitute for providing narrative answers to the questions.</li>
+              <li>Assessors have limited time to evaluate your application, so any additional documents should be kept short and relevant.</li>
+            </ul>
+          )
+          pdf_context %(
+            <p>If there are additional materials you feel would help us to assess your entry, then you can add up to three files or online links in this section.</p>
+            <h4 class="govuk-heading-s">What can and cannot be included:</h3>
             <p>
-              Please include any vital information in the form as we can't guarantee the additional material will be reviewed. It is also essential to reference these in your application to ensure the assessors are aware of them and can relate this information to the relevant questions.
+              \u2022 You can include links to promotional videos, websites, other media, or documents you feel are relevant and help illustrate your application.
+              \u2022 We will not consider business plans, annual accounts or company policy documents.
             </p>
-
+            <h4 class="govuk-heading-s">Please note:</h3>
             <p>
-              You can upload files in all common formats, as long as they're less than 5mb each.
-            </p>
-
-            <p>
-              You may link to videos, websites or other media you feel relevant.
-            </p>
-
-            <p>
-              We won't consider business plans, annual accounts or company policy documents.
-            </p>
-
-            <p>
-              Additional materials should not be used as a substitute for completing sections of the form.
+              \u2022 For assessors to review the supporting material, you must reference them by their names in your answers. Please do so to ensure they are reviewed.
+              \u2022 Do not use the supporting material as a substitute for providing narrative answers to the questions.
+              \u2022 Assessors have limited time to evaluate your application, so any additional documents should be kept short and relevant.
             </p>
           )
           hint "What are the allowed file formats?", %(
@@ -33,6 +40,16 @@ class AwardYears::V2024::QAEForms
           max_attachments 3
           links
           description
+        end
+
+        confirm :entry_confirmation, "Confirmation of entry" do
+          ref "F 2"
+          required
+          text -> do
+            %(
+              By submitting this entry for consideration for The King's Awards for Enterprise #{AwardYear.current.year}, I certify that all the given particulars and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld. I undertake to notify The King's Awards Office of any changes to the information I have provided in this entry form.
+            )
+          end
         end
 
         confirm :agree_being_contacted_about_issues_not_related_to_application, "Confirmation of contact" do
@@ -48,16 +65,6 @@ class AwardYears::V2024::QAEForms
             I am happy to be contacted by the Department for Business, Energy & Industrial Strategy.
           )
           show_ref_always true
-        end
-
-        confirm :entry_confirmation, "Confirmation of entry" do
-          ref "F 4"
-          required
-          text -> do
-            %(
-              By submitting this entry for consideration for The King's Awards for Enterprise #{AwardYear.current.year}, I certify that all the given particulars and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld. I undertake to notify The King's Awards Office of any changes to the information I have provided in this entry form.
-            )
-          end
         end
 
         submit "Submit application" do
