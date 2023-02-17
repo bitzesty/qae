b1e372127c72a62a087dbbb1f5a56db1837779bb
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-17 09:53:22 +0000

    About section B copy change

--- app/forms/award_years/v2024/social_mobility/social_mobility_step2.rb
+++ app/forms/award_years/v2024/social_mobility/social_mobility_step2.rb
@@ -6,144 +6,28 @@ class AwardYears::V2024::QAEForms
         header :company_information_header, "" do
           section_info
           context %(
-            <h3 class='govuk-heading-m'>About this section</h3>
+            <h3 class='govuk-heading-m'>About section B</h3>
             <p class='govuk-body'>
-              The purpose of this section is to collect all vital information about your company. For example, your company's registration number and address. It is important for the details provided to be accurate as it cannot be changed later. This information will also be used to enable us to undertake due diligence checks with other government departments and agencies if your application is shortlisted. Please be aware due diligence checks inform the decision to confer an award.
+              The purpose of this section is to collect specific information that identifies your organisation, for example, your registration number and address. It is important that the details are accurate as they cannot be changed after the submission deadline.
             </p>
             <p class='govuk-body'>
-              Before you apply, please consider any issues that may prevent your application from receiving routine clearance as part of the due diligence we undertake with a number of Government Departments and Agencies. For example, this may be fines or penalties you have received or non-compliance with regulations.
+              This information will also be used to enable The King's Awards Office to undertake due diligence checks with other government departments and agencies if your application is shortlisted. Please be aware due diligence checks inform the decision to confer an award.
             </p>
-            <p class='govuk-body'>
-              Please check with your accountant and legal representatives if there are any outstanding or recent issues, as The King's Awards for Enterprise Office are unable to perform due diligence again once it has been completed.
-            </p>
-            <p class='govuk-body'>
-              Some examples of issues that may prevent your organisation from receiving clearance for The King's Awards for Enterprise Award:
-            </p>
-            <ul class='govuk-list govuk-list--bullet'>
-              <li>A failure to pay staff the minimum wage;</li>
-              <li>An accident within the workplace, which has resulted in harm being caused to the environment or employees;</li>
-              <li>A failure to fully comply with administrative filing requirements as stipulated by any Government Department or Agency.</li>
-            </ul>
-            <details class='govuk-details' data-module="govuk-details">
-              <summary class="govuk-details__summary">
-                <span class="govuk-details__summary-text">
-                  View Government Departments and Agencies we undertake due diligence checks with
-                </span>
-              </summary>
-              <div class="govuk-details__text">
-                <ul>
-                  <li>Biotechnology & Biological Sciences Research Council</li>
-                  <li>Charity Commission</li>
-                  <li>Companies House</li>
-                  <li>Competition and Markets Authority</li>
-                  <li>Crown Commercial Service</li>
-                  <li>Department for Business, Energy and Industrial Strategy</li>
-                  <li>Department for Communities and Local Government</li>
-                  <li>Department for Culture Media & Sport</li>
-                  <li>Department for Education</li>
-                  <li>Department for Environment, Food & Rural Affairs</li>
-                  <li>Department for International Trade</li>
-                  <li>Department for Transport</li>
-                  <li>Department of Economic Development, Isle of Man</li>
-                  <li>Department for the Economy NI</li>
-                  <li>Department of Health</li>
-                  <li>Environment Agency</li>
-                  <li>Financial Conduct Authority</li>
-                  <li>Food Standards Agency</li>
-                  <li>Forestry Commission</li>
-                  <li>Guernsey Government</li>
-                  <li>Health and Safety Executive</li>
-                  <li>HM Courts & Tribunals Service</li>
-                  <li>HM Revenue & Customs</li>
-                  <li>Home Office</li>
-                  <li>Insolvency Service</li>
-                  <li>Intellectual Property Office</li>
-                  <li>Invest NI</li>
-                  <li>Jersey Government</li>
-                  <li>Ministry of Defence</li>
-                  <li>Ministry of Justice</li>
-                  <li>Medical Research Council Technology</li>
-                  <li>National Measurement Office</li>
-                  <li>Natural England</li>
-                  <li>Natural Environment Research Council</li>
-                  <li>Office of the Scottish Charity Regulator</li>
-                  <li>Scottish Government</li>
-                  <li>Scottish Environment Protection Agency</li>
-                  <li>Scottish Funding Council</li>
-                  <li>Serious Fraud Office</li>
-                  <li>UK Export Finance</li>
-                  <li>Wales Government</li>
-                </ul>
-              </div>
-            </details>
             <h3 class="govuk-heading-m">Small organisations</h3>
             <p class="govuk-body">
-              King’s Awards for Enterprise is committed to acknowledging efforts of organisations of all sizes. When assessing we consider what is reasonable performance given the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer all of the questions to a degree you can.
+              King's Awards for Enterprise is committed to acknowledging the efforts of organisations of all sizes. When assessing, we consider the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer all questions to the degree you can.
             </p>
           )
           pdf_context_with_header_blocks [
-            [:bold, "About this section"],
+            [:bold, "About section B"],
             [:normal, %(
-              The purpose of this section is to collect all vital information about your company. For example, your company's registration number and address. It is important for the details provided to be accurate as it cannot be changed later. This information will also be used to enable us to undertake due diligence checks with other government departments and agencies if your application is shortlisted. Please be aware due diligence checks inform the decision to confer an award.
+              The purpose of this section is to collect specific information that identifies your organisation, for example, your registration number and address. It is important that the details are accurate as they cannot be changed after the submission deadline.
 
-              Before you apply, please consider any issues that may prevent your application from receiving routine clearance as part of the due diligence we undertake with a number of Government Departments and Agencies. For example, this may be fines or penalties you have received or non-compliance with regulations.
-
-              Please check with your accountant and legal representatives if there are any outstanding or recent issues, as The King's Awards for Enterprise Office are unable to perform due diligence again once it has been completed.
-
-              Some examples of issues that may prevent your organisation from receiving clearance for The King's Awards for Enterprise Award:
-
-              \u2022 A failure to pay staff the minimum wage;
-              \u2022 An accident within the workplace, which has resulted in harm being caused to the environment or employees;
-              \u2022 A failure to fully comply with administrative filing requirements as stipulated by any Government Department or Agency.
+              This information will also be used to enable The King's Awards Office to undertake due diligence checks with other government departments and agencies if your application is shortlisted. Please be aware due diligence checks inform the decision to confer an award.
             )],
-            [:bold, "Government Departments and Agencies we undertake due diligence checks with:"],
-            [:normal, %(
-                \u2022 Biotechnology & Biological Sciences Research Council
-                \u2022 Charity Commission
-                \u2022 Companies House
-                \u2022 Competition and Markets Authority
-                \u2022 Crown Commercial Service
-                \u2022 Department for Business, Energy and Industrial Strategy
-                \u2022 Department for Communities and Local Government
-                \u2022 Department for Culture Media & Sport
-                \u2022 Department for Education
-                \u2022 Department for Environment, Food & Rural Affairs
-                \u2022 Department for International Trade
-                \u2022 Department for Transport
-                \u2022 Department of Economic Development, Isle of Man
-                \u2022 Department for the Economy NI
-                \u2022 Department of Health
-                \u2022 Environment Agency
-                \u2022 Financial Conduct Authority
-                \u2022 Food Standards Agency
-                \u2022 Forestry Commission
-                \u2022 Guernsey Government
-                \u2022 Health and Safety Executive
-                \u2022 HM Courts & Tribunals Service
-                \u2022 HM Revenue & Customs
-                \u2022 Home Office
-                \u2022 Insolvency Service
-                \u2022 Intellectual Property Office
-                \u2022 Invest NI
-                \u2022 Jersey Government
-                \u2022 Ministry of Defence
-                \u2022 Ministry of Justice
-                \u2022 Medical Research Council Technology
-                \u2022 National Measurement Office
-                \u2022 Natural England
-                \u2022 Natural Environment Research Council
-                \u2022 Office of the Scottish Charity Regulator
-                \u2022 Scottish Government
-                \u2022 Scottish Environment Protection Agency
-                \u2022 Scottish Funding Council
-                \u2022 Serious Fraud Office
-                \u2022 UK Export Finance
-                \u2022 Wales Government
-              )
-            ],
             [:bold, "Small organisations"],
             [:normal, %(
-                King’s Awards for Enterprise is committed to acknowledging efforts of organisations of all sizes. When assessing we consider what is reasonable performance given the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer all of the questions to a degree you can.
+                King's Awards for Enterprise is committed to acknowledging the efforts of organisations of all sizes. When assessing, we consider the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer all questions to the degree you can.
               )
             ]
           ]
