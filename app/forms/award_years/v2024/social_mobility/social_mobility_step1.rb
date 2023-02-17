321445c7ef752fc44805b3f995ca81c3b5004167
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-17 09:36:16 +0000

    Add header A2 and copy

--- app/forms/award_years/v2024/social_mobility/social_mobility_step1.rb
+++ app/forms/award_years/v2024/social_mobility/social_mobility_step1.rb
@@ -58,22 +58,19 @@ class AwardYears::V2024::QAEForms
           type "email"
         end
 
-        confirm :entry_confirmation, "Confirmation of entry" do
-          ref "F 4"
-          required
-          text -> do
-            %(
-              By submitting this entry for consideration for The King's Awards for Enterprise #{AwardYear.current.year}, I certify that all the given particulars and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld. I undertake to notify The King's Awards Office of any changes to the information I have provided in this entry form.
-            )
-          end
-        end
-
-        confirm :shortlisted_case_confirmation_i_am_not_aware_of_any_matter, "" do
-          ref "F 4.1"
-          required
-          show_ref_always true
-          text %(
-            I am not aware of any matter which might cast doubt on the worthiness of my organisation to receive a King's Award for Enterprise. I consent to all necessary enquiries being made by The King's Awards Office concerning this entry. This includes enquiries made of Government Departments and Agencies in discharging its responsibilities to vet any business unit which might be granted a King's Award to ensure the highest standards of propriety.
+        header :due_diligence_header, "Organisation worthiness & due diligence checks" do
+          ref "A 2"
+          context %{
+            <p class="govuk-body">Before you apply, please consider any issues that may prevent your application from receiving routine clearance as part of the due diligence process that we undertake with a number of Government Departments and Agencies. For example, this may be fines or penalties you have received or non-compliance with regulations.</p>
+            <p class="govuk-body">Therefore please check with your accountant and legal representatives if there are any outstanding or recent issues, as The King's Awards for Enterprise Office starts the due diligence process immediately after the submission and is unable to repeat the due diligence process.</p>
+            <p class="govuk-body">Some examples of issues that may prevent your organisation from receiving final clearance for The King's Awards for Enterprise Award:</p>
+            <ul class="govuk-list govuk-list--bullet">
+              <li>Fines or penalties;</li>
+              <li>Non-compliance with regulations;</li>
+              <li>Failure to pay staff the minimum wage;</li>
+              <li>Accident within the workplace, which has resulted in harm being caused to the environment or employees;</li>
+              <li>Failure to fully comply with administrative filing requirements as stipulated by any Government Department or Agency.</li>
+            </ul>
             <details class='govuk-details govuk-!-margin-top-3 govuk-!-margin-bottom-0' data-module="govuk-details">
               <summary class="govuk-details__summary">
                 <span class="govuk-details__summary-text">
@@ -126,6 +123,84 @@ class AwardYears::V2024::QAEForms
                 </ul>
               </div>
             </details>
+          }
+          pdf_context_with_header_blocks [
+            [:normal, %(
+              Before you apply, please consider any issues that may prevent your application from receiving routine clearance as part of the due diligence process that we undertake with a number of Government Departments and Agencies. For example, this may be fines or penalties you have received or non-compliance with regulations.
+
+              Therefore please check with your accountant and legal representatives if there are any outstanding or recent issues, as The King's Awards for Enterprise Office starts the due diligence process immediately after the submission and is unable to repeat the due diligence process.
+
+              Some examples of issues that may prevent your organisation from receiving final clearance for The King's Awards for Enterprise Award:
+
+              \u2022 Fines or penalties;
+              \u2022 Non-compliance with regulations;
+              \u2022 Failure to pay staff the minimum wage;
+              \u2022 Accident within the workplace, which has resulted in harm being caused to the environment or employees;
+              \u2022 Failure to fully comply with administrative filing requirements as stipulated by any Government Department or Agency.
+
+              Government Departments and Agencies we undertake due diligence checks with:
+
+              \u2022 Biotechnology & Biological Sciences Research Council
+              \u2022 Charity Commission
+              \u2022 Companies House
+              \u2022 Competition and Markets Authority
+              \u2022 Crown Commercial Service
+              \u2022 Department for Business, Energy and Industrial Strategy
+              \u2022 Department for Communities and Local Government
+              \u2022 Department for Culture Media & Sport
+              \u2022 Department for Education
+              \u2022 Department for Environment, Food & Rural Affairs
+              \u2022 Department for International Trade
+              \u2022 Department for Transport
+              \u2022 Department of Economic Development, Isle of Man
+              \u2022 Department for the Economy NI
+              \u2022 Department of Health
+              \u2022 Environment Agency
+              \u2022 Financial Conduct Authority
+              \u2022 Food Standards Agency
+              \u2022 Forestry Commission
+              \u2022 Guernsey Government
+              \u2022 Health and Safety Executive
+              \u2022 HM Courts & Tribunals Service
+              \u2022 HM Revenue & Customs
+              \u2022 Home Office
+              \u2022 Insolvency Service
+              \u2022 Intellectual Property Office
+              \u2022 Invest NI
+              \u2022 Jersey Government
+              \u2022 Ministry of Defence
+              \u2022 Ministry of Justice
+              \u2022 Medical Research Council Technology
+              \u2022 National Measurement Office
+              \u2022 Natural England
+              \u2022 Natural Environment Research Council
+              \u2022 Office of the Scottish Charity Regulator
+              \u2022 Scottish Government
+              \u2022 Scottish Environment Protection Agency
+              \u2022 Scottish Funding Council
+              \u2022 Serious Fraud Office
+              \u2022 UK Export Finance
+              \u2022 Wales Government
+            )]
+          ]
+        end
+
+        confirm :entry_confirmation, "Confirmation of entry" do
+          ref "F 4"
+          required
+          text -> do
+            %(
+              By submitting this entry for consideration for The King's Awards for Enterprise #{AwardYear.current.year}, I certify that all the given particulars and those in any accompanying statements are correct to the best of my knowledge and belief and that no material information has been withheld. I undertake to notify The King's Awards Office of any changes to the information I have provided in this entry form.
+            )
+          end
+        end
+
+        confirm :shortlisted_case_confirmation_i_am_not_aware_of_any_matter, "" do
+          ref "F 4.1"
+          required
+          show_ref_always true
+          text %(
+            I am not aware of any matter which might cast doubt on the worthiness of my organisation to receive a King's Award for Enterprise. I consent to all necessary enquiries being made by The King's Awards Office concerning this entry. This includes enquiries made of Government Departments and Agencies in discharging its responsibilities to vet any business unit which might be granted a King's Award to ensure the highest standards of propriety.
           )
 
           pdf_text %(
