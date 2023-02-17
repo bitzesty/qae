ac7bcd5f14a0bfb6137f864fc739c3e3cc6813aa
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-02-17 13:55:29 +0000

    Move E1 to F1 and help copy change

--- app/forms/award_years/v2024/social_mobility/social_mobility_step6.rb
+++ app/forms/award_years/v2024/social_mobility/social_mobility_step6.rb
@@ -2,14 +2,38 @@ class AwardYears::V2024::QAEForms
   class << self
     def mobility_step6
       @mobility_step6 ||= proc do
-        upload :innovation_materials, "If there is additional material you feel would help us to assess your entry then you can add up to 5 files or website addresses here" do
-          ref "E 1"
+        upload :innovation_materials, "Supplementary materials" do
+          ref "F 1"
           context %(
             <p>
-              Please include any vital information in the form as we can't guarantee the additional material will be reviewed. It is also essential to reference these in your application to ensure the assessors are aware of them and can relate this information to the relevant questions.
+              If there are additional materials you feel would help us to assess your entry, you can add up to three files or online links in this section.
             </p>
-            <p>You can upload files in all common formats, as long as they're less than 5mb each. You may link to videos, websites or other media you feel relevant.</p>
-            <p>We won't consider business plans, annual accounts or company policy documents. Additional materials should not be used as a substitute for completing sections of the form.</p>
+            <h4 class="govuk-heading-s">What can and cannot be included:</h3>
+            <ul>
+              <li>You <strong>can</strong> include links to promotional videos, websites, other media, or documents you feel are relevant and help illustrate your application.</li>
+              <li>We <strong>will not</strong> consider business plans, annual accounts or company policy documents.</li>
+            </ul>
+            <h4 class="govuk-heading-s">Please note:</h3>
+            <ul>
+              <li>For assessors to review the supporting material, you must reference them by their names in your answers. Please do so to ensure they are reviewed.</li>
+              <li>Do not use the supporting material as a substitute for providing narrative answers to the questions.</li>
+              <li>Assessors have limited time to evaluate your application, so any additional documents should be kept short and relevant.</li>
+            </ul>
+          )
+          pdf_context %(
+            <p>If there are additional materials you feel would help us to assess your entry, then you can add up to three files or online links in this section.
+
+              What can and cannot be included:
+
+              \u2022 You can include links to promotional videos, websites, other media, or documents you feel are relevant and help illustrate your application.
+              \u2022 We will not consider business plans, annual accounts or company policy documents.
+            </p>
+            <h4 class="govuk-heading-s">Please note:</h3>
+            <p>
+              \u2022 For assessors to review the supporting material, you must reference them by their names in your answers. Please do so to ensure they are reviewed.
+              \u2022 Do not use the supporting material as a substitute for providing narrative answers to the questions.
+              \u2022 Assessors have limited time to evaluate your application, so any additional documents should be kept short and relevant.
+            </p>
           )
           hint "What are the allowed file formats?", %(
             <p>
