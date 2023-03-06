ef6785eb55a79676f02cc76d6b6eecb0ecfcc4ec
Lubos Hricak <Lubosky@users.noreply.github.com>
2023-03-05 22:18:48 -0300

    Add `Utils` module to hold useful methods

---
+++ app/services/utils/elapsed_time.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Utils
+  class ElapsedTime
+    @clock = if Rack::Utils.respond_to?(:clock_time)
+      -> { Rack::Utils.clock_time }
+    else
+      -> { Time.current }
+    end.freeze
+
+    def self.call
+      @clock.()
+    end
+  end
+end
