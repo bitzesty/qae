5b36ee2104a4c119a98c11362a5eeb30bd412ed3
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-03-13 14:26:09 +0000

    adds logs for create and submit actions for press summary

--- app/controllers/admin/press_summaries_controller.rb
+++ app/controllers/admin/press_summaries_controller.rb
@@ -1,4 +1,4 @@
 class Admin::PressSummariesController < Admin::BaseController
-  after_action :log_event, only: [:update, :signoff]
+  after_action :log_event, only: [:create, :update, :submit, :signoff]
   include PressSummaryMixin
 end
