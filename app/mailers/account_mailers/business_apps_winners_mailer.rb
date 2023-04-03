4ce959416cf175e6e4e09ce9d4af62b7b00b745e
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-04-03 13:19:29 +0100

    copy change kings's to king's

--- app/mailers/account_mailers/business_apps_winners_mailer.rb
+++ app/mailers/account_mailers/business_apps_winners_mailer.rb
@@ -3,7 +3,7 @@ class AccountMailers::BusinessAppsWinnersMailer < AccountMailers::BaseMailer
   before_action :set_end_of_embargo_deadline
   before_action :set_media_deadline
   before_action :set_press_summary_deadline
-  
+
   def notify(form_answer_id, collaborator_id)
     @form_answer = FormAnswer.find(form_answer_id).decorate
     @user = @form_answer.user.decorate
@@ -11,7 +11,7 @@ class AccountMailers::BusinessAppsWinnersMailer < AccountMailers::BaseMailer
 
     @name = "#{@user.title} #{@user.last_name}"
 
-    subject = "Important information about your Kings's Award"
+    subject = "Important information about your King's Award"
     send_mail_if_not_bounces ENV['GOV_UK_NOTIFY_API_TEMPLATE_ID'], to: collaborator.email, subject: subject_with_env_prefix(subject)
   end
 end
