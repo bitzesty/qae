9c5556190951bf743bbe8733b411ae4d769603fb
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-04-03 09:59:36 +0100

    Adds new SD eligibility questions

--- app/models/eligibility/development.rb
+++ app/models/eligibility/development.rb
@@ -1,9 +1,49 @@
 class Eligibility::Development < Eligibility
   AWARD_NAME = 'Sustainable Development'
 
+  property :able_to_provide_actual_figures,
+            boolean: true,
+            label: "Will you be able to provide financial figures for your three most recent financial years, covering 36 months?",
+            accept: :true,
+            hint: %(
+              <p class='govuk-hint'>
+                For the purpose of this application, your most recent financial year is your last financial year ending before the #{Settings.current.deadlines.where(kind: "submission_end").first.decorate.formatted_trigger_date('with_year')} - the application submission deadline.
+              </p>
+              <p class='govuk-hint'>
+                If you haven't reached your most recent year-end, you can provide estimated figures in the interim.
+              </p>
+            )
+
+  property :has_management_and_two_employees,
+            label: "Did your organisation have at least two full-time UK employees or full-time equivalent employees (FTEs) in your three most recent financial years?",
+            accept: :true,
+            boolean: true,
+            hint: %(
+              <p class='govuk-hint'>
+                You can calculate the number of full-time employees at the year-end or the average for each 12-month period. Part-time employees should be expressed in full-time equivalents (FTEs).
+              </p>
+            )
+
   property :sustainable_development,
-            values: %w[yes no skip],
-            acts_like_boolean: true,
-            label: "Have you had a substantial sustainable development action or intervention for at least the last two years?",
-            accept: :true
+            boolean: true,
+            label: "Can you demonstrate outstanding achievement in sustainable development for at least the last two years?",
+            accept: :true,
+            hint: %(
+              <p class='govuk-hint'>
+                You may find it helpful to familiarise yourself with the United Nations 17 Sustainable Development Goals (UN SDGs).
+              </p>
+              <p class='govuk-hint'>
+                You will not need to show impact in each of these areas, only the ones that are most applicable to your sustainable development interventions.
+              </p>
+            )
+
+  property :adheres_to_sustainable_principles,
+            boolean: true,
+            label: "Can you demonstrate that the organisation as a whole adheres to sustainable practices?",
+            accept: :true,
+            hint: %(
+              <p class='govuk-hint'>
+                Please note, we are assessing the whole organisation, not just particular interventions. At a minimum, we expect all winning organisations to have good practices around climate change.
+              </p>
+            )
 end
