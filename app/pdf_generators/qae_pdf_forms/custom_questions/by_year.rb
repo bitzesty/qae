b538989c43d3c923c0c559d6186932902c2b297a
Lubos Hricak <Lubosky@users.noreply.github.com>
2023-05-02 16:59:25 -0300

    PDF headers w/ dates

--- app/pdf_generators/qae_pdf_forms/custom_questions/by_year.rb
+++ app/pdf_generators/qae_pdf_forms/custom_questions/by_year.rb
@@ -1,17 +1,13 @@
 module QaePdfForms::CustomQuestions::ByYear
   EMPTY_STRING = "".freeze
   YEAR_LABELS = %w(day month year).freeze
-  FINANCIAL_YEAR_PREFIX = "Financial year".freeze
-  YEAR_ENDING_IN_PREFIX = "Year ended".freeze
-  AS_AT_DATE_PREFIX = "As at".freeze
-  AS_AT_DATE_PREFIX_QUESTION_KEYS = [
-    :total_net_assets
-  ].freeze
+  FORMATTED_FINANCIAL_YEAR_WITH_DATE = "Financial year %<index>d ended %<date>s".freeze
+  FORMATTED_FINANCIAL_YEAR_WITHOUT_DATE = "Financial year %<index>d".freeze
+  FORMATTED_AS_AT_DATE = "As at %<date>s".freeze
+  AS_AT_DATE_PREFIX_QUESTION_KEYS = [:total_net_assets].freeze
   ANSWER_FONT_START = "<color rgb='#{FormPdf::DEFAULT_ANSWER_COLOR}'>".freeze
   ANSWER_FONT_END = "</color>".freeze
-  CALCULATED_FINANCIAL_DATA = [
-    :uk_sales
-  ].freeze
+  CALCULATED_FINANCIAL_DATA = [:uk_sales].freeze
   OMIT_COLON_KEYS = [:financial_year_changed_dates].freeze
 
   def render_years_labels_table
@@ -19,18 +15,17 @@ module QaePdfForms::CustomQuestions::ByYear
       a.split("/")
     end
 
-    # not needed for 2021
     # rows.push(latest_year_label)
 
-    financial_dates_changed_year_headers.each_with_index do |header_item, placement|
+    financial_dates_year_headers(format: FORMATTED_FINANCIAL_YEAR_WITHOUT_DATE).each_with_index do |header_item, placement|
       form_pdf.default_bottom_margin
-      if OMIT_COLON_KEYS.include?(question.key)
-        title = "#{header_item} #{ANSWER_FONT_START}#{rows[placement].join("/")}#{ANSWER_FONT_END}"
-      else
-        title = "#{header_item}: #{ANSWER_FONT_START}#{rows[placement].join(" ")}#{ANSWER_FONT_END}"
-      end
-      form_pdf.text title,
-                    inline_format: true
+      title = if OMIT_COLON_KEYS.include?(question.key)
+                "#{header_item} #{ANSWER_FONT_START}#{rows[placement].join("/")}#{ANSWER_FONT_END}"
+              else
+                "#{header_item}: #{ANSWER_FONT_START}#{rows[placement].join(" ")}#{ANSWER_FONT_END}"
+              end
+
+      form_pdf.text title, inline_format: true
     end
   end
 
@@ -52,45 +47,37 @@ module QaePdfForms::CustomQuestions::ByYear
   end
 
   def year_headers
-    if financial_year_changed_dates?
-      financial_dates_changed_year_headers
-    else
-      financial_dates_not_changed_year_headers
-    end
+    financial_dates_year_headers
   end
 
-  def financial_dates_changed_year_headers
-    res = []
-    size = financial_table_headers.size
-
-    financial_table_headers.each_with_index do |item, placement|
-      header_item = "#{FINANCIAL_YEAR_PREFIX} #{placement + 1}"
-      header_item += " (most recent)" if size == (placement + 1)
-
-      res << header_item
-    end
-
-    res
-  end
-
-  def financial_dates_not_changed_year_headers
-    prefix = if AS_AT_DATE_PREFIX_QUESTION_KEYS.include?(question.key)
-      AS_AT_DATE_PREFIX
-    else
-      YEAR_ENDING_IN_PREFIX
-    end
-
+  def financial_dates_year_headers(**opts)
     if form_pdf.pdf_blank_mode.present? # BLANK FOR MODE
       financial_table_default_headers.map.with_index do |item, index|
         financial_table_default_headers.size == (index + 1) ? "#{item} (most recent)" : item
       end
     else
+      frmt = opts.dig(:format)
+      res = []
       size = financial_table_headers.size
 
-      financial_table_headers.map.with_index do |item, index|
-        item = "#{prefix} #{item}"
-        size == (index.to_i + 1) && item.include?(FINANCIAL_YEAR_PREFIX) ? "#{item} (most recent)" : item
+      financial_table_headers.each_with_index do |item, idx|
+        if AS_AT_DATE_PREFIX_QUESTION_KEYS.include?(question.key)
+          frmt ||= FORMATTED_AS_AT_DATE
+          res << format(frmt, date: item)
+        else
+          if item.split("/").any?(&:blank?)
+            frmt = FORMATTED_FINANCIAL_YEAR_WITHOUT_DATE
+          end
+
+          frmt ||= FORMATTED_FINANCIAL_YEAR_WITH_DATE
+
+          temp = format(frmt, date: item, index: idx.to_i)
+          temp = "#{temp} (most recent)" if size == (idx.to_i + 1)
+          res << temp
+       end
       end
+
+      res
     end
   end
 
