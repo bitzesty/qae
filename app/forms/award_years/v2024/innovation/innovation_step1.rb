6fc7311a94581459a79245ab5201a54763d48bd9
DaniBitZesty <84323332+DaniBitZesty@users.noreply.github.com>
2023-05-02 14:43:11 +0100

    Adds required attribute and changes question ref

--- app/forms/award_years/v2024/innovation/innovation_step1.rb
+++ app/forms/award_years/v2024/innovation/innovation_step1.rb
@@ -174,6 +174,7 @@ class AwardYears::V2024::QAEForms
         confirm :agree_being_contacted_by_kao, "Consent to enquiries by The King's Awards Office" do
           sub_ref "A 2.2"
           show_ref_always true
+          required
           text %(
             I consent to all necessary enquiries being made by The King's Awards Office concerning this entry. This includes enquiries made of Government Departments and Agencies in discharging their responsibilities to vet any business unit which might be granted a King's Award to ensure the highest standards of propriety.
           )
