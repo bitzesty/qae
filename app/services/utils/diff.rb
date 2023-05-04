8b1975467dc012f339b8db3884ddcdd64701f852
Lubos Hricak <Lubosky@users.noreply.github.com>
2023-05-04 03:11:52 -0300

    Allow for `Utils::Diff` to return absolute value - do not return it by default

--- app/services/utils/diff.rb
+++ app/services/utils/diff.rb
@@ -2,10 +2,11 @@
 
 module Utils
   class Diff
-    def self.calc(v1, v2)
+    def self.calc(v1, v2, abs: true)
       return nil unless ::Utils::String.integer?(v1) && ::Utils::String.integer?(v2)
 
-      (v1.to_i - v2.to_i).abs
+      res = v1.to_i - v2.to_i
+      abs ? res.abs : res
     rescue
       nil
     end
