af594c38a2d3b57e7b68a1712488e012a4114941
Lubos Hricak <Lubosky@users.noreply.github.com>
2023-05-11 00:13:58 -0300

    Fix FY if it can be and has been changed

--- app/form_pointers/latest_year_generator.rb
+++ app/form_pointers/latest_year_generator.rb
@@ -1,13 +1,13 @@
 module LatestYearGenerator
   def calculate_last_year(form_answer, day, month)
     # Conditional latest year
-    # If from 3rd of September to December -> then previous year
-    # If from January to 2nd of September -> then current year
+    # If from 7th of September to December -> then previous year
+    # If from January to 6th of September -> then current year
     #
 
     @form_answer_award_year = form_answer.award_year.year unless @form_answer_award_year.present?
 
-    if (month.to_i == 9 && day.to_i >= 3) || month.to_i > 9
+    if form_answer.financial_year_changeable? || (month.to_i == 9 && day.to_i > 6) || month.to_i > 9
       @form_answer_award_year - 2
     else
       @form_answer_award_year - 1
