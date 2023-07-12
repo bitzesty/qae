1d5bd92f45c6da9e2758247b0a2c76db211c587f
Louis Kirkham <louis.kirkham@bitzesty.com>
2023-07-12 14:52:29 +0100

    Changes email address

--- app/mailers/account_mailers/base_mailer.rb
+++ app/mailers/account_mailers/base_mailer.rb
@@ -1,6 +1,6 @@
 class AccountMailers::BaseMailer < ApplicationMailer
   default from: ENV["MAILER_FROM"] || "no-reply@kings-awards-enterprise.service.gov.uk",
-          reply_to: "kingsawards@beis.gov.uk"
+          reply_to: "kingsawards@businessandtrade.gov.uk"
 
   layout "mailer"
 
