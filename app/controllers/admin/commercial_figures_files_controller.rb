2f1ae150a3ac7326c93e62955a76ffa6d4105068
Lubos Hricak <Lubosky@users.noreply.github.com>
2023-07-14 00:38:44 -0300

    Re-work `AdminShortlistedDocsContext`

--- app/controllers/admin/commercial_figures_files_controller.rb
+++ app/controllers/admin/commercial_figures_files_controller.rb
@@ -1,7 +1,15 @@
 class Admin::CommercialFiguresFilesController < Admin::BaseController
   include AdminShortlistedDocsContext
 
-  def file_model_name
-    "commercial_figures_files"
+  def build_resource
+    begin_of_association_chain.build_commercial_figures_file(permitted_params)
+  end
+
+  def relationship_name
+    :commercial_figures_file
+  end
+
+  def resource_class
+    CommercialFiguresFile
   end
 end
