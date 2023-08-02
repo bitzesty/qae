4506e8a1eee0645735b5bddc23c210db41174131
DaniBitZesty <84323332+DaniBitZesty@users.noreply.github.com>
2023-08-02 14:56:19 +0100

    Updates Added Audit Certificate mailer copy

--- app/mailers/users/audit_certificate_mailer.rb
+++ app/mailers/users/audit_certificate_mailer.rb
@@ -3,7 +3,7 @@ class Users::AuditCertificateMailer < ApplicationMailer
     @form_answer = FormAnswer.find(form_answer_id).decorate
     @recipient = User.find(user_id).decorate
 
-    subject = "King's Awards for Enterprise: Verification of Commercial Figures submitted"
+    subject = "King's Awards for Enterprise: Verification of Commercial Figures submitted - Application ref #{@form_answer.urn}"
     send_mail_if_not_bounces ENV['GOV_UK_NOTIFY_API_TEMPLATE_ID'], to: @recipient.email, subject: subject_with_env_prefix(subject)
   end
 end
