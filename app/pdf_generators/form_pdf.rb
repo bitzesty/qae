5bccc6ce3f0adb00eb13a59fbfa6eb50e918a7a5
Vasili Kachalko <amarant.st@gmail.com>
2023-08-09 16:36:17 +0200

    post zeitwerk update fixes

--- app/pdf_generators/form_pdf.rb
+++ app/pdf_generators/form_pdf.rb
@@ -49,9 +49,9 @@ class FormPdf < Prawn::Document
       a.clean?
     end
     @filled_answers = fetch_filled_answers
-    
+
     @financial_pointer = FormFinancialPointer.new(@form_answer.decorate)
-    
+
     generate!
   end
 
--- forms/award_years/v2020/innovation/innovation_step3.rb
+++ forms/award_years/v2020/innovation/innovation_step3.rb
@@ -5,13 +5,13 @@ class AwardYears::V2020::QaeForms
       @innovation_step3 ||= proc do
         header :commercial_success_info_block, "" do
           context %(
-            <h3>About this section</h3>    
-            <p> 
+            <h3>About this section</h3>
+            <p>
               All applicants for any Queen’s Award must demonstrate a certain level of financial performance. This section enables you to show the impact that your innovation had on your organisation's financial performance.
-            </p>  
+            </p>
             <h3>Small organisations</h3>
             <p>
-              Queen’s Awards for Enterprise is committed to acknowledging efforts of organisations of all sizes. When assessing we consider what is reasonable performance given the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer them to a degree you can. 
+              Queen’s Awards for Enterprise is committed to acknowledging efforts of organisations of all sizes. When assessing we consider what is reasonable performance given the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer them to a degree you can.
             </p>
             <h3>Estimated figures</h3>
             <p>
@@ -25,7 +25,7 @@ class AwardYears::V2020::QaeForms
             )],
             [:bold, "Small organisations"],
             [:normal, %(
-              Queen’s Awards for Enterprise is committed to acknowledging efforts of organisations of all sizes. When assessing we consider what is reasonable performance given the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer them to a degree you can. 
+              Queen’s Awards for Enterprise is committed to acknowledging efforts of organisations of all sizes. When assessing we consider what is reasonable performance given the size and sector of your organisation. If you are a small organisation, do not be intimidated by the questions that are less relevant to you - answer them to a degree you can.
             )],
             [:bold, "Estimated figures"],
             [:normal, %(
