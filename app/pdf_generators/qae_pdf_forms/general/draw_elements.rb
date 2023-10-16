3cd27f21d3041e4cb5ca6ad24a8efbcbef116300
Lubos Hricak <Lubosky@users.noreply.github.com>
2023-10-16 11:30:50 -0300

    Update phone number

--- app/pdf_generators/qae_pdf_forms/general/draw_elements.rb
+++ app/pdf_generators/qae_pdf_forms/general/draw_elements.rb
@@ -156,7 +156,7 @@ module QaePdfForms::General::DrawElements
     render_text("Need help?", size: 14, style: :bold)
 
     block_3 = %(If you need digital assistance with filling in the form or have any questions, please feel free to get in touch with us:
-      By calling 020 7215 6880
+      By calling 020 4551 0081
       Or emailing <b>kingsawards@businessandtrade.gov.uk</b>)
 
     render_text(block_3)
