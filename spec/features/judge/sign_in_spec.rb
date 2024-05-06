2c17d14d8b83e818e46e5a14aceae59bd54870f4
Lubos Hricak <Lubosky@users.noreply.github.com>
2024-05-06 07:09:13 -0300

    Update passwords in specs & seeds to be valid

--- spec/features/judge/sign_in_spec.rb
+++ spec/features/judge/sign_in_spec.rb
@@ -8,13 +8,13 @@ describe "Judge is able do sign in", %(
          I want to be able to sign in to the system
 ) do
 
-  let(:judge) { create(:judge, password: "#ur9EkLm@1W") }
+  let(:judge) { create(:judge, password: "#ur9EkLm@1W+OaDvg") }
 
   it "allows judge to sign in" do
     visit judge_root_path
 
     fill_in "judge[email]", with: judge.email
-    fill_in "judge[password]", with: "#ur9EkLm@1W"
+    fill_in "judge[password]", with: "#ur9EkLm@1W+OaDvg"
 
     click_button "Sign in"
 
