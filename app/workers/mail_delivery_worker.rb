fa8079e843f54e401ae703d204e2adcc18e2a919
Lubos Hricak <Lubosky@users.noreply.github.com>
2024-05-07 13:19:17 -0300

    Allow to set rate limitting parameters via ENV variables

--- app/workers/mail_delivery_worker.rb
+++ app/workers/mail_delivery_worker.rb
@@ -4,7 +4,12 @@ class MailDeliveryWorker
   include Sidekiq::Worker
   include Sidekiq::Throttled::Job
 
-  sidekiq_throttle(threshold: { limit: 1_000, period: 1.hour })
+  sidekiq_throttle(
+    threshold: {
+      limit: ENV.fetch("MAIL_NOTIFY_THROTTLE_LIMIT") { 3_000 }.to_i,
+      period: ENV.fetch("MAIL_NOTIFY_THROTTLE_PERIOD_IN_SECONDS") { 60 }.to_i.seconds
+    }
+  )
 
   def perform(mailer, mail_method, delivery_method, *args)
     mailer_class = mailer.safe_constantize
