6e18d309e29767cbca5a0e2df8a3f3ce8ea4ddb1
Matthew Ford <matt@bitzesty.com>
2024-05-09 19:49:07 +0100

    Update production.rb
    Removing old comments for AWS SES (prior to the Notify migration)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,9 +93,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # AWS SES mail settings are in config/initializers/aws_ses.rb
-
-  # configure the devise email layout
-  # config.to_prepare { Devise::Mailer.layout "mailer" }
 end
