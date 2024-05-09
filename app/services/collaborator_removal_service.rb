5cd12f959c985c1cd9cb62435e3b1152025044ef
DaniBitZesty <84323332+DaniBitZesty@users.noreply.github.com>
2024-05-09 14:37:25 +0100

    Add attr_reader to CollaboratorRemovalService

--- app/services/collaborator_removal_service.rb
+++ app/services/collaborator_removal_service.rb
@@ -1,4 +1,6 @@
 class CollaboratorRemovalService
+  attr_reader :collaborator, :account
+
   def initialize(collaborator, account)
     @collaborator = collaborator
     @account = account
@@ -6,10 +8,10 @@ class CollaboratorRemovalService
 
   def reassign_form_answers
     User.transaction do
-      @collaborator.account_id = Account.create(owner: @collaborator).id
-      @collaborator.role = "account_admin"
-      @collaborator.save!
-      @collaborator.form_answers.update_all(user_id: @account.owner_id)
+      collaborator.account_id = Account.create(owner: collaborator).id
+      collaborator.role = "account_admin"
+      collaborator.save!
+      collaborator.form_answers.update_all(user_id: account.owner_id)
     end
   end
 end
