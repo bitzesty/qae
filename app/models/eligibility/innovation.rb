fceb355fb287bca0303acb1f1e9db2f843499420
Louis Kirkham <louis.kirkham@bitzesty.com>
2024-05-24 16:20:51 +0100

    Removes help copy from eligibility

--- app/models/eligibility/innovation.rb
+++ app/models/eligibility/innovation.rb
@@ -6,7 +6,7 @@ class Eligibility::Innovation < Eligibility
   property :able_to_provide_financial_figures,
     boolean: true,
     label: "Will you be able to provide financial figures for at least your two most recent financial years, covering the full 24 months?",
-    hint: "For the purpose of this application, your most recent financial year-end is your last financial year-end before the #{Settings.current_submission_deadline.decorate.formatted_trigger_date("with_year")} - the application submission deadline. If you haven't reached your most recent year-end, you can provide estimated figures in the interim.<br><br>If your financial year end has changed within your last two financial years, and one of the periods is shorter than 12 months, you will have to provide figures for at least one additional year.",
+    hint: "If you haven't reached your most recent year-end, you can provide estimated figures in the interim.<br><br>If your financial year end has changed within your last two financial years, and one of the periods is shorter than 12 months, you will have to provide figures for at least one additional year.",
     accept: :true
 
   property :has_two_full_time_employees,
