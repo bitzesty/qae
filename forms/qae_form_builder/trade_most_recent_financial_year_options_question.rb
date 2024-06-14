a62a7527436fd72fb2f59340c09d44b0d18444f1
Louis Kirkham <louis.kirkham@bitzesty.com>
2024-06-14 16:17:14 +0100

    Removes custom error validation

--- forms/qae_form_builder/trade_most_recent_financial_year_options_question.rb
+++ forms/qae_form_builder/trade_most_recent_financial_year_options_question.rb
@@ -1,18 +1,5 @@
 class QaeFormBuilder
   class TradeMostRecentFinancialYearOptionsQuestionValidator < OptionsQuestionValidator
-    def errors
-      result = {}
-
-      date, range = question.get_changeable_date_range
-
-      if date.present? && range.present? && !date.in?(range) && question.year_has_changed?
-        from = Settings.current_award_year_switch_date
-        to = Settings.current_submission_deadline
-        result[question.key] = "You can only change the year if your dates in question D2 range between #{from.decorate.formatted_trigger_date} to #{to.decorate.formatted_trigger_date}."
-      end
-
-      result
-    end
   end
 
   class TradeMostRecentFinancialYearOptionsQuestionBuilder < OptionsQuestionBuilder
