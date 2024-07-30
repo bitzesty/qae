6ec38eb1f8343c5baa8604d50b96243382d00a89
Matthew Ford <matt@bitzesty.com>
2024-07-31 00:54:46 +0100

    chore: enhance Docker setup for production environment
    - Update the Dockerfile to use a more recent Ruby version and streamline the build process.
    - Add a new .node-version file to specify the Node.js version.
    - Introduce a new docker-compose configuration for production with Nginx and Redis.
    - Remove the outdated Dockerfile.local and docker-compose.yml.local files.
    - Delete deprecated environment variable settings
    
    Signed-off-by: Matthew Ford <matt@bitzesty.com>

--- config/puma.rb
+++ config/puma.rb
@@ -14,8 +14,7 @@ preload_app!
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
-
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RACK_ENV") { "development" }
