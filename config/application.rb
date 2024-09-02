57c4837983445a44146cb37481b2bc0e39ebb835
Lubos Hricak <Lubosky@users.noreply.github.com>
2024-09-02 13:29:51 +0200

    Allow to pass multiple origins for cross site requests

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Qae
     if ENV["CORS_HOST"].present?
       config.middleware.insert_before 0, Rack::Cors do
         allow do
-          origins ENV["CORS_HOST"] || "*"
+          origins(*(ENV["CORS_HOST"] || "*").split(","))
           resource "*",
             headers: :any,
             methods: %i[get options]
