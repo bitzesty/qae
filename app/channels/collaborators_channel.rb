e51feb5d61bb08f260bc920db88bff534d132f7f
sammo1235 <samrhysperry93@hotmail.co.uk>
2024-10-11 12:40:24 +0100

    Create collaborator channels, stop editing unless editor, and switching forms behaviour
    Implements two main behaviours using ActionCable:
    1. If a second user has the same section of a form open, they cannot edit it. However if the first user closes their tab
    or moves on to a different section then editing rights are released to the second user.
    2. If user 1 opens a second tab of the same form, they cannot edit in the second tab. They are shown a message telling them
    to close the tab and edit in the first tab.

---
+++ app/channels/collaborators_channel.rb
@@ -0,0 +1,41 @@
+class CollaboratorsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from params["channel_name"]
+    collaborators = Rails.cache.read(params["channel_name"])
+    user = User.find(params["user_id"])
+
+    if collaborators.blank?
+      collaborators = "#{user.id}:#{params["current_tab"]}:#{user.email}:#{user.full_name}:EDITOR"
+    else
+      collaborators += "/#{user.id}:#{params["current_tab"]}:#{user.email}:#{user.full_name}"
+    end
+
+    Rails.cache.write(params["channel_name"], collaborators)
+
+    Collaborators::BroadcastCollabWorker.perform_async(params["channel_name"], collaborators)
+  end
+
+  def unsubscribed
+    user_id = params["user_id"]
+    new_collaborators = []
+    collaborators = Rails.cache.read(params["channel_name"])
+
+    collaborators.split("/").each do |collaborator|
+      # remove the unsubscribing user
+      new_collaborators << collaborator unless collaborator.split(":")[0] == user_id && collaborator.split(":")[1] == params["current_tab"]
+    end
+
+    if !new_collaborators.empty? && new_collaborators.join.exclude?("EDITOR")
+      # editor has left the channel, so we update the next in line to be editor
+      temp_collabs = new_collaborators
+      temp_collabs[0] += ":EDITOR"
+      new_collaborators = temp_collabs.join("/")
+    else
+      new_collaborators = new_collaborators.join("/")
+    end
+
+    Rails.cache.write(params["channel_name"], new_collaborators)
+
+    Collaborators::BroadcastCollabWorker.perform_async(params["channel_name"], new_collaborators)
+  end
+end
