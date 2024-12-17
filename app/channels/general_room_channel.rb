2230ad1cf6feb688b22a6c1843a4a9b8b674a1ac
phil-l-brockwell <phil.l.brockwell@gmail.com>
2024-12-17 14:52:10 +0000

    Use tab_ident in GeneralRoomChannel

--- app/channels/general_room_channel.rb
+++ app/channels/general_room_channel.rb
@@ -1,27 +1,22 @@
 class GeneralRoomChannel < ApplicationCable::Channel
   def subscribed
-    room_members = Rails.cache.read(params["channel_name"])
-
-    if room_members.blank?
-      room_members = [params["user_id"]]
-    elsif room_members.exclude?(params["user_id"])
-      room_members << params["user_id"]
-    end
-
+    room_members = Rails.cache.read(params["channel_name"]) || []
+    room_members << user_identifier if room_members.exclude?(user_identifier)
     stream_from params["channel_name"]
-
     Rails.cache.write(params["channel_name"], room_members, expires_in: 60.minutes)
-
     Collaborators::BroadcastCollabWorker.perform_async(params["channel_name"], room_members)
   end
 
   def unsubscribed
-    room_members = Rails.cache.read(params["channel_name"])
-
-    room_members.delete(params["user_id"])
-
+    room_members = Rails.cache.read(params["channel_name"]) || []
+    room_members.delete(user_identifier)
     Rails.cache.write(params["channel_name"], room_members, expires_in: 60.minutes)
-
     Collaborators::BroadcastCollabWorker.perform_async(params["channel_name"], room_members)
+  end
+
+  private
+
+  def user_identifier
+    "#{params["user_id"]}-#{params["current_tab"]}"
   end
 end
--- spec/channels/general_room_channel_spec.rb
+++ spec/channels/general_room_channel_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
-RSpec.describe GeneralRoomChannel, type: :channel do
+describe GeneralRoomChannel, type: :channel do
   let(:channel_name) { "presence-chat-development-1-general" }
   let(:user) { create(:user) }
   let(:user_two) { create(:user) }
+  let(:current_tab) { "anything" }
 
   before do
     stub_connection
@@ -14,33 +15,35 @@ RSpec.describe GeneralRoomChannel, type: :channel do
   describe "#subscribed" do
     context "when the room is empty" do
       it "adds the subscribing user ID to the redis cache" do
-        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s })
+        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s, "current_tab" => current_tab })
 
-        expect(Rails.cache.read(channel_name)).to eq([user.id.to_s])
+        expect(Rails.cache.read(channel_name)).to eq(["#{user.id}-#{current_tab}"])
       end
 
       it "broadcasts the new room member" do
-        expect(Collaborators::BroadcastCollabWorker).to receive(:perform_async).with(channel_name, [user.id.to_s])
+        expect(Collaborators::BroadcastCollabWorker).to receive(:perform_async).with(channel_name, ["#{user.id}-#{current_tab}"])
 
-        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s })
+        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s, "current_tab" => current_tab })
       end
     end
 
     context "when the room already has a member" do
       before do
-        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s })
+        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s, "current_tab" => current_tab })
       end
 
-      it "adds the subscribing user ID to the redis cache" do
-        subscribe({ "channel_name" => channel_name, "user_id" => user_two.id.to_s })
+      let(:user_two_tab) { "anything-else" }
 
-        expect(Rails.cache.read(channel_name)).to eq([user.id.to_s, user_two.id.to_s])
+      it "adds the subscribing user ID to the redis cache" do
+        subscribe({ "channel_name" => channel_name, "user_id" => user_two.id.to_s, "current_tab" => user_two_tab })
+
+        expect(Rails.cache.read(channel_name)).to eq(["#{user.id}-#{current_tab}", "#{user_two.id}-#{user_two_tab}"])
       end
 
       it "broadcasts the new room member" do
-        expect(Collaborators::BroadcastCollabWorker).to receive(:perform_async).with(channel_name, [user.id.to_s, user_two.id.to_s])
+        expect(Collaborators::BroadcastCollabWorker).to receive(:perform_async).with(channel_name, ["#{user.id}-#{current_tab}", "#{user_two.id}-#{user_two_tab}"])
 
-        subscribe({ "channel_name" => channel_name, "user_id" => user_two.id.to_s })
+        subscribe({ "channel_name" => channel_name, "user_id" => user_two.id.to_s, "current_tab" => user_two_tab })
       end
     end
   end
@@ -48,7 +51,7 @@ RSpec.describe GeneralRoomChannel, type: :channel do
   describe "#unsubscribed" do
     context "when there is one room member" do
       it "clears the room" do
-        sub_one = subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s })
+        sub_one = subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s, "current_tab" => current_tab })
 
         sub_one.unsubscribe_from_channel
 
@@ -56,7 +59,7 @@ RSpec.describe GeneralRoomChannel, type: :channel do
       end
 
       it "broadcasts the new (empty) room members" do
-        sub_one = subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s })
+        sub_one = subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s, "current_tab" => current_tab })
 
         expect(Collaborators::BroadcastCollabWorker).to receive(:perform_async).with(channel_name, [])
         sub_one.unsubscribe_from_channel
@@ -64,17 +67,19 @@ RSpec.describe GeneralRoomChannel, type: :channel do
     end
 
     context "when there are multiple room members" do
+      let(:user_two_tab) { "anything-else" }
+
       it "removes only the unsubscribing room member" do
-        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s })
-        sub_two = subscribe({ "channel_name" => channel_name, "user_id" => user_two.id.to_s })
+        subscribe({ "channel_name" => channel_name, "user_id" => user.id.to_s, "current_tab" => current_tab })
+        sub_two = subscribe({ "channel_name" => channel_name, "user_id" => user_two.id.to_s, "current_tab" => user_two_tab })
 
-        expect(Rails.cache.read(channel_name)).to eq([user.id.to_s, user_two.id.to_s])
+        expect(Rails.cache.read(channel_name)).to eq(["#{user.id}-#{current_tab}", "#{user_two.id}-#{user_two_tab}"])
 
-        expect(Collaborators::BroadcastCollabWorker).to receive(:perform_async).with(channel_name, [user.id.to_s])
+        expect(Collaborators::BroadcastCollabWorker).to receive(:perform_async).with(channel_name, ["#{user.id}-#{current_tab}"])
 
         sub_two.unsubscribe_from_channel
 
-        expect(Rails.cache.read(channel_name)).to eq([user.id.to_s])
+        expect(Rails.cache.read(channel_name)).to eq(["#{user.id}-#{current_tab}"])
       end
     end
   end
